081562a0024d112988a0fb809defcc964a0b5491
liaralabs <liara@swizzin.ltd>
2017-06-20 13:18:38 -0700

    Add ssl-cert to nginx depends

--- scripts/install/nginx.sh
+++ scripts/install/nginx.sh
@@ -14,7 +14,7 @@ fi
 
 
 
-APT='nginx-full php7.0 php7.0-cli php7.0-fpm php7.0-dev php7.0-curl php7.0-xmlrpc php7.0-json php7.0-mcrypt php7.0-opcache php-geoip'
+APT='nginx-full ssl-cert php7.0 php7.0-cli php7.0-fpm php7.0-dev php7.0-curl php7.0-xmlrpc php7.0-json php7.0-mcrypt php7.0-opcache php-geoip'
 for depends in $APT; do
 apt-get -qq -y --yes --force-yes install "$depends" >/dev/null 2>&1 || (echo "APT-GET could not find all the required sources. Script Ending." && echo "${warning}" && exit 1)
 done
