42894fba2460ffea383ab13e3d60d932728ccb4b
liaralabs <liara@swizzin.ltd>
2017-06-23 12:05:11 -0700

    Only install panel if nginx is detected

--- scripts/install/panel.sh
+++ scripts/install/panel.sh
@@ -3,21 +3,25 @@
 #Author: swizzin | lizaSB
 IFACE=$(ip link show|grep -i broadcast|grep -m1 UP|cut -d: -f 2|cut -d@ -f 1|sed -e 's/ //g');
 user=$(cat /etc/.master.info | cut -d: -f2)
-cd /srv/
-git clone https://gitlab.swizzin.ltd/quickbox_dashbord panel
 
-chown -R www-data: /srv/panel
+if [[ ! -f /install/.nginx.lock ]]; then
+  echo "ERROR: Web server not detected. Please install nginx and restart panel install."
+else
+  cd /srv/
+  git clone https://gitlab.swizzin.ltd/quickbox_dashbord panel
 
-touch /srv/panel/db/output.log
-printf "${IFACE}" > /srv/panel/db/interface.txt
-printf "${user}" > /srv/panel/db/master.txt
-LOCALE=en_GB.UTF-8
-LANG=lang_en
-sed -i "s/LOCALE/${LOCALE}/g" /srv/panel/inc/localize.php
-sed -i "s/LANG/${LANG}/g" /srv/panel/inc/localize.php
-echo "*/1 * * * * root bash /usr/local/bin/swizzin/set_interface" > /etc/cron.d/set_interface
+  chown -R www-data: /srv/panel
 
-cat > /etc/nginx/apps/panel.conf <<PAN
+  touch /srv/panel/db/output.log
+  printf "${IFACE}" > /srv/panel/db/interface.txt
+  printf "${user}" > /srv/panel/db/master.txt
+  LOCALE=en_GB.UTF-8
+  LANG=lang_en
+  sed -i "s/LOCALE/${LOCALE}/g" /srv/panel/inc/localize.php
+  sed -i "s/LANG/${LANG}/g" /srv/panel/inc/localize.php
+  echo "*/1 * * * * root bash /usr/local/bin/swizzin/set_interface" > /etc/cron.d/set_interface
+
+  cat > /etc/nginx/apps/panel.conf <<PAN
 location / {
 alias /srv/panel;
 auth_basic "What's the password?";
@@ -25,5 +29,6 @@ auth_basic_user_file /etc/htpasswd;
 }
 PAN
 
-service nginx force-reload
-touch /install/.panel.lock
+  service nginx force-reload
+  touch /install/.panel.lock
+fi
