9e6ad2558988c4f7fbf6bc694f98e0c7ef5c988a
liaralabs <liara@swizzin.ltd>
2017-06-23 20:30:33 -0700

    Update syncthing

--- scripts/install/syncthing.sh
+++ scripts/install/syncthing.sh
@@ -18,8 +18,14 @@
 #   under the GPL along with build & install instructions.
 
 local_setup=/etc/QuickBox/setup/
-OUTTO=/srv/rutorrent/home/db/output.log
-MASTER=$(cat /srv/rutorrent/home/db/master.txt)
+if [[ -f /tmp/.install.lock ]]; then
+  OUTTO="/root/logs/install.log"
+elif [[ -f /install/.panel.lock ]]; then
+  OUTTO="/srv/panel/db/output.log"
+else
+  OUTTO="/dev/null"
+fi
+MASTER=$(cat /root/.master.info | cut -d: -f1)
 
 echo "Adding Syncthing Repository ... " >>"${OUTTO}" 2>&1;
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add - > /dev/null 2>&1
@@ -30,22 +36,43 @@ sudo apt-get -q update > /dev/null 2>&1
 sudo apt-get -qy install syncthing > /dev/null 2>&1
 
 echo "Configuring Syncthing & Starting ... " >>"${OUTTO}" 2>&1;
-cp ${local_setup}templates/sysd/syncthing.template /etc/systemd/system/syncthing@.service
+cat > /etc/systemd/system/syncthing@.service <<SYNC
+[Unit]
+Description=Syncthing - Open Source Continuous File Synchronization for %I
+Documentation=man:syncthing(1)
+After=network.target
+Wants=syncthing-inotify@.service
+
+[Service]
+User=%i
+ExecStart=/usr/bin/syncthing -no-browser -no-restart -logflags=0
+Restart=on-failure
+SuccessExitStatus=3 4
+RestartForceExitStatus=3 4
+
+[Install]
+WantedBy=multi-user.target
+SYNC
 systemctl enable syncthing@${MASTER} > /dev/null 2>&1
 systemctl start syncthing@${MASTER} > /dev/null 2>&1
 
-cat > /etc/apache2/sites-enabled/syncthing.conf <<EOF
-<Location /syncthing>
-ProxyPass http://localhost:8384
-ProxyPassReverse http://localhost:8384
-AuthType Digest
-AuthName "rutorrent"
-AuthUserFile '/etc/htpasswd'
-Require user ${MASTER}
-</Location>
+cat > /etc/nginx/apps/syncthing.conf <<EOF
+location /syncthing/ {
+  proxy_set_header        Host \$host;
+  proxy_set_header        X-Real-IP \$remote_addr;
+  proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
+  proxy_set_header        X-Forwarded-Proto \$scheme;
+
+  proxy_pass              http://localhost:8384/;
+
+  proxy_read_timeout      600s;
+  proxy_send_timeout      600s;
+
+  auth_basic "What's the password?";
+  auth_basic_user_file /etc/htpasswd.d/htpasswd.${MASTER};
+}
 EOF
-chown www-data: /etc/apache2/sites-enabled/syncthing.conf
-service apache2 reload
+service nginx reload
 
 touch /install/.syncthing.lock
 echo "Syncthing installation complete!" >>"${OUTTO}" 2>&1;
