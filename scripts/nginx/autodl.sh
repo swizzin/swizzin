93837a956d640f281478d3e14abf183378ebe9a7
liaralabs <liara@swizzin.ltd>
2017-06-27 13:01:36 -0700

    Fix autodl logic

--- scripts/nginx/autodl.sh
+++ scripts/nginx/autodl.sh
@@ -4,12 +4,12 @@ users=($(cat /etc/htpasswd | cut -d ":" -f 1))
     git clone https://github.com/autodl-community/autodl-rutorrent.git autodl-irssi >/dev/null 2>&1 || (echo "git of autodl plugin to main plugins seems to have failed ... ")
     chown -R www-data:www-data autodl-irssi/
 for u in "${users[@]}"; do
-if [[ -f /install/.autodl.lock ]]; then
-    IRSSI_PORT=$(cat /home/${u}/.autodl2.cfg | grep port | cut -d= -f2 | sed 's/ //g' )
-    IRSSI_PASS=$(cat /home/${u}/.autodl2.cfg | grep password | cut -d= -f2 | sed 's/ //g' )
+    IRSSI_PORT=$(cat /home/${u}/.autodl/autodl2.cfg | grep port | cut -d= -f2 | sed 's/ //g' )
+    IRSSI_PASS=$(cat /home/${u}/.autodl/autodl2.cfg | grep password | cut -d= -f2 | sed 's/ //g' )
     sed -i '/?>/d' /srv/rutorrent/conf/users/${u}/config.php
+    sed -i '/autodl/d' /srv/rutorrent/conf/users/${u}/config.php
     echo "\$autodlPort = \"$IRSSI_PORT\";" >> /srv/rutorrent/conf/users/${u}/config.php
     echo "\$autodlPassword = $IRSSI_PASS;" >> /srv/rutorrent/conf/users/${u}/config.php
     echo "?>" >> /srv/rutorrent/conf/users/${u}/config.php
-  fi
+
 done
