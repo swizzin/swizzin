29b0203b4482c6714885f34670e5bd73420ca0c9
liaralabs <liara@swizzin.ltd>
2017-07-10 17:33:21 -0400

    Fix proxy host issues

--- scripts/install/nginx.sh
+++ scripts/install/nginx.sh
@@ -111,7 +111,7 @@ proxy_send_timeout 240;
 proxy_connect_timeout 240;
 
 # Basic Proxy Config
-proxy_set_header Host \$host;
+proxy_set_header Host \$proxy_host;
 proxy_set_header X-Real-IP \$remote_addr;
 proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 proxy_set_header X-Forwarded-Proto https;
