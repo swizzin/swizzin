2d188cf5a24a00118f80412c2a8d0bd5ce0e0471
liaralabs <liara@swizzin.ltd>
2017-08-09 18:54:13 -0700

    Check if autodl exists before clone

--- scripts/nginx/autodl.sh
+++ scripts/nginx/autodl.sh
@@ -10,8 +10,10 @@
 #   under the GPL along with build & install instructions.
 users=($(cat /etc/htpasswd | cut -d ":" -f 1))
     cd /srv/rutorrent/plugins/
-    git clone https://github.com/autodl-community/autodl-rutorrent.git autodl-irssi >/dev/null 2>&1 || (echo "git of autodl plugin to main plugins seems to have failed ... ")
-    chown -R www-data:www-data autodl-irssi/
+    if [[ ! -d /srv/rutorrent/plugins/autodl-irssi ]]; then
+        git clone https://github.com/autodl-community/autodl-rutorrent.git autodl-irssi >/dev/null 2>&1 || (echo "git of autodl plugin to main plugins seems to have failed ... ")
+        chown -R www-data:www-data autodl-irssi/
+    fi
 for u in "${users[@]}"; do
     IRSSI_PORT=$(cat /home/${u}/.autodl/autodl2.cfg | grep port | cut -d= -f2 | sed 's/ //g' )
     IRSSI_PASS=$(cat /home/${u}/.autodl/autodl2.cfg | grep password | cut -d= -f2 | sed 's/ //g' )
