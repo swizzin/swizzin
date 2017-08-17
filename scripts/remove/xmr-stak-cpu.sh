657574f8c3b66d3990a27e7268fd6f11cce1de71
liaralabs <liara@swizzin.ltd>
2017-08-17 15:29:37 -0700

    xmr-stak-cpu removal

---
+++ scripts/remove/xmr-stak-cpu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# xmr-stak-cpu removal
+
+user=$(cat /root/.master.info | cut -d: -f1)
+systemctl disable xmr > /dev/null 2>&1
+systemctl stop xmr
+rm -rf /home/${user}/.xmr
+rm -rf /etc/systemd/system/xmr.service
+rm -rf /usr/local/bin/xmr-stak-cpu
