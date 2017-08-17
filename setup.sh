53d3d05628b7eeda4aa5afe662071f83668b1c39
liaralabs <liara@swizzin.ltd>
2017-08-17 16:12:23 -0700

    Properly set secure_path environmental variable

--- setup.sh
+++ setup.sh
@@ -229,6 +229,7 @@ function _post {
 	ip=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
 	echo "export PATH=$PATH:/usr/local/bin/swizzin" >> /root/.bashrc
 	echo "export PATH=$PATH:/usr/local/bin/swizzin" >> /home/$user/.bashrc
+	echo "Defaults    secure_path = /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin/swizzin" > /etc/sudoers.d/secure_path
 	echo "Installation complete!"
 	echo ""
 	echo "You may now login with the following info: ${user}:${pass}"
