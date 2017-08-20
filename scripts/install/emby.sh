abf22313a1a9ad81249d8fecb991caebc8824b35
liaralabs <liara@swizzin.ltd>
2017-08-20 11:28:57 -0700

    Fix emby install issues

--- scripts/install/emby.sh
+++ scripts/install/emby.sh
@@ -32,11 +32,23 @@ fi
 
 echo "Installing emby keys and sources ... " >>"${OUTTO}" 2>&1;
   if [[ $DISTRO == Debian ]]; then
-    echo "deb http://download.opensuse.org/repositories/home:/emby/$(lsb_release -is)_$(lsb_release -rs)/ /" > /etc/apt/sources.list.d/emby-server.list
-    wget --quiet http://download.opensuse.org/repositories/home:emby/$(lsb_release -is)_$(lsb_release -rs)/Release.key -O - | apt-key add - > /dev/null 2>&1
+    if [[ $CODENAME == stretch ]]; then
+      version=9.0
+    elif [[ $CODENAME == jessie ]]; then
+      version=8.0
+    fi
+    echo "deb http://download.opensuse.org/repositories/home:/emby/$(lsb_release -is)_${version}/ /" > /etc/apt/sources.list.d/emby-server.list
+    wget --quiet http://download.opensuse.org/repositories/home:emby/$(lsb_release -is)_${version}/Release.key -O - | apt-key add - > /dev/null 2>&1
   elif [[ $DISTRO == Ubuntu ]]; then
-    sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/emby/x$(lsb_release -is)_$(lsb_release -rs)/ /' > /etc/apt/sources.list.d/emby-server.list"
-    wget --quiet http://download.opensuse.org/repositories/home:emby/x$(lsb_release -is)_$(lsb_release -rs)/Release.key -O - | apt-key add - > /dev/null 2>&1
+      if [[ $CODENAME == xenial ]]; then
+        version=16.04
+      elif [[ $CODENAME == yakkety ]]; then
+        version=16.10
+      elif [[ $CODENAME == zesty ]]; then
+        version=17.04
+      fi
+    sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/emby/x$(lsb_release -is)_${version}/ /' > /etc/apt/sources.list.d/emby-server.list"
+    wget --quiet http://download.opensuse.org/repositories/home:emby/x$(lsb_release -is)_${version}/Release.key -O - | apt-key add - > /dev/null 2>&1
   fi
 
 echo "Updating system & installing emby server ... " >>"${OUTTO}" 2>&1;
--- scripts/nginx/emby.sh
+++ scripts/nginx/emby.sh
@@ -12,7 +12,7 @@ MASTER=$(cat /root/.master.info | cut -d: -f1)
 if [[ ! -f /etc/nginx/apps/emby.conf ]]; then
 cat > /etc/nginx/apps/emby.conf <<EMB
 location /emby/ {
-  rewrite /emby/(.*) /$1 break
+  rewrite /emby/(.*) /$1 break;
   include /etc/nginx/conf.d/proxy.conf;
   proxy_pass        http://127.0.0.1:8096/;
   auth_basic "What's the password?";
