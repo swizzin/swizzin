3af310d4d73301a0c896610a412cbcc2adf5ad4c
liaralabs <liara@swizzin.ltd>
2017-08-25 10:17:26 -0700

    Push a default vsftpd conf and enable ftp(s) by default

--- scripts/install/vsftpd.sh
+++ scripts/install/vsftpd.sh
@@ -18,6 +18,44 @@ else
 fi
 
 apt-get -y update >> $log 2>&1
-apt-get -y install vsftpd >> $log 2>&1
+apt-get -y install vsftpd ssl-cert>> $log 2>&1
 
-touch /install/.vsftpd.conf
+cat > /etc/vsftpd.conf <<VSC
+listen=NO
+listen_ipv6=YES
+anonymous_enable=NO
+local_enable=YES
+write_enable=YES
+force_dot_files=YES
+local_umask=022
+dirmessage_enable=YES
+use_localtime=YES
+xferlog_enable=YES
+connect_from_port_20=YES
+utf8_filesystem=YES
+rsa_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem
+rsa_private_key_file=/etc/ssl/private/ssl-cert-snakeoil.key
+ssl_enable=YES
+pam_service_name=vsftpd
+secure_chroot_dir=/var/run/vsftpd/empty
+
+#ascii_upload_enable=YES
+#ascii_download_enable=YES
+#ftpd_banner=Welcome to blah FTP service.
+
+#############################################
+#Uncomment these lines to enable FXP support#
+#############################################
+#pasv_promiscuous=YES
+#port_promiscuous=YES
+
+###################
+#Set a custom port#
+###################
+#listen_port=
+VSC
+
+systemctl restart vsftpd
+
+
+touch /install/.vsftpd.lock
