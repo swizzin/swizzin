e880081d698d8763d0f4889a5d0e0ec31d82354d
liaralabs <liara@swizzin.ltd>
2017-09-02 17:20:24 -0700

    Escape nginx variables in netdata config

--- scripts/nginx/netdata.sh
+++ scripts/nginx/netdata.sh
@@ -11,23 +11,23 @@
 user=$(cat /root/.master.info | cut -d: -f1)
 
 if [[ ! -f /etc/nginx/apps/netdata.conf ]]; then
-  cat > /etc/nginx/apps/netdata.conf <<'NET'
+  cat > /etc/nginx/apps/netdata.conf <<NET
 location /netdata {
   return 301 /netdata/;
 }
 
 location ~ /netdata/(?<ndpath>.*) {
   proxy_redirect off;
-  proxy_set_header Host $host;
+  proxy_set_header Host \$host;
 
-  proxy_set_header X-Forwarded-Host $host;
-  proxy_set_header X-Forwarded-Server $host;
-  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+  proxy_set_header X-Forwarded-Host \$host;
+  proxy_set_header X-Forwarded-Server \$host;
+  proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
   proxy_http_version 1.1;
   proxy_pass_request_headers on;
   proxy_set_header Connection "keep-alive";
   proxy_store off;
-  proxy_pass http://127.0.0.1:19999/$ndpath$is_args$args;
+  proxy_pass http://127.0.0.1:19999/\$ndpath\$is_args\$args;
   auth_basic "What's the password?";
   auth_basic_user_file /etc/htpasswd.d/htpasswd.${user};
 
