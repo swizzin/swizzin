7d733011dc24f033271481657f34fe1cbcb6b02e
liaralabs <liara@swizzin.ltd>
2017-09-07 19:43:23 -0700

    Avoid permissions issues

--- setup.sh
+++ setup.sh
@@ -227,6 +227,7 @@ function _post {
   ip=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
   echo "export PATH=$PATH:/usr/local/bin/swizzin" >> /root/.bashrc
   echo "export PATH=$PATH:/usr/local/bin/swizzin" >> /home/$user/.bashrc
+  chown ${user}: /home/$user/.bashrc
   echo "Defaults    secure_path = /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin/swizzin" > /etc/sudoers.d/secure_path
   echo "Installation complete!"
   echo ""
