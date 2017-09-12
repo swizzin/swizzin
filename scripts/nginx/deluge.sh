8e54866cbf0c9adbb18d55641895161775a661d9
liaralabs <liara@swizzin.ltd>
2017-09-11 20:04:54 -0700

    Fix web.conf location

--- scripts/nginx/deluge.sh
+++ scripts/nginx/deluge.sh
@@ -25,8 +25,8 @@ for u in "${users[@]}"; do
   if [[ -f /etc/nginx/apps/${u}.dindex.conf ]]; then rm -f /etc/nginx/apps/${u}.dindex.conf; fi
 
 systemctl stop deluge-web@$u
-sed -i 's/"interface": "0.0.0.0"/"interface": "127.0.0.1"/g' /home/$u/.config/web.conf
-sed -i 's/"https": true/"https": false/g' /home/$u/.config/web.conf
+sed -i 's/"interface": "0.0.0.0"/"interface": "127.0.0.1"/g' /home/$u/.config/deluge/web.conf
+sed -i 's/"https": true/"https": false/g' /home/$u/.config/deluge/web.conf
 
   if [[ ! -f /etc/nginx/conf.d/${u}.deluge.conf ]]; then
     DWPORT=$(cat /home/$u/.config/deluge/web.conf | grep port | cut -d: -f2| sed 's/ //g' | sed 's/,//g')
