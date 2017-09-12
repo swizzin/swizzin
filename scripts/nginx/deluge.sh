d19bcedba7e5308d2bb2554520c8e23ff9363356
liaralabs <liara@swizzin.ltd>
2017-09-11 19:23:43 -0700

    New! Deluge reverse proxies (and updated download directory -- /deluge.downloads) run "bash /etc/swizzin/scripts/nginx/deluge.sh" to update

--- scripts/nginx/deluge.sh
+++ scripts/nginx/deluge.sh
@@ -10,15 +10,48 @@
 #   under the GPL along with build & install instructions.
 users=($(cat /etc/htpasswd | cut -d ":" -f 1))
 
-for u in "${users[@]}"; do
-  if [[ ! -f /etc/nginx/apps/${u}.dindex.conf ]]; then
-  cat > /etc/nginx/apps/${u}.dindex.conf <<DIN
-location /${u}.deluge.downloads {
-  alias /home/${u}/torrents/deluge;
+if [[ ! -f /etc/nginx/apps/dindex.conf ]]; then
+  cat > /etc/nginx/apps/dindex.conf <<DIN
+location /deluge.downloads {
+  alias /home/\$remote_user/torrents/deluge;
   include /etc/nginx/snippets/fancyindex.conf;
   auth_basic "What's the password?";
-  auth_basic_user_file /etc/htpasswd.d/htpasswd.${u};
+  auth_basic_user_file /etc/htpasswd;
 }
 DIN
+fi
+
+for u in "${users[@]}"; do
+  if [[ -f /etc/nginx/apps/${u}.dindex.conf ]]; then rm -f /etc/nginx/apps/${u}.dindex.conf; fi
+
+systemctl stop deluge-web@$u
+sed -i 's/"interface": "0.0.0.0"/"interface": "127.0.0.1"/g' /home/$u/.config/web.conf
+sed -i 's/"https": true/"https": false/g' /home/$u/.config/web.conf
+
+  if [[ ! -f /etc/nginx/conf.d/${u}.deluge.conf ]]; then
+    DWPORT=$(cat /home/$u/.config/deluge/web.conf | grep port | cut -d: -f2| sed 's/ //g' | sed 's/,//g')
+    cat > /etc/nginx/conf.d/${u}.deluge.conf <<DUPS
+upstream $u.deluge {
+  server 127.0.0.1:$DWPORT;
+}
+DUPS
+  fi
+
+  if [[ ! -f /etc/nginx/apps/deluge.conf ]]; then
+    cat > /etc/nginx/apps/deluge.conf <<'DRP'
+location /deluge {
+  return 301 /deluge/;
+}
+
+location /deluge/ {
+  include /etc/nginx/snippets/proxy.conf;
+  auth_basic "What's the password?";
+  auth_basic_user_file /etc/htpasswd;
+  proxy_set_header X-Deluge-Base "/deluge/";
+  rewrite ^/deluge/(.*) /$1 break;
+  proxy_pass http://$remote_user.deluge;
+}
+DRP
   fi
 done
+systemctl reload nginx
