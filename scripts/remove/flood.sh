c6570106ee67fcd99a3405add63a5efb259baf6d
liaralabs <liara@swizzin.ltd>
2017-09-21 11:19:36 -0700

    Add removal scripts for ruTorrent and Flood

---
+++ scripts/remove/flood.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Flood uninstaller
+# Author: liara
+
+users=($(cat /etc/htpasswd | cut -d ":" -f 1))
+for u in "${users[@]}"; do
+  systemctl disable flood@$u
+  systemctl stop flood@$u
+  rm -rf /home/$u/.flood
+  rm -rf /etc/nginx/conf.d/$u.flood.conf
+done
+rm -rf /etc/nginx/apps/flood.conf
+if [[ ! -f /install/.rutorrent.lock ]]; then
+  rm -rf /etc/nginx/apps/rindex.conf
+fi
+rm -rf /install/.flood.lock
