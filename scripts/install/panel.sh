434c61adab09bc5fceb7421b587a9ae8d98eb044
liaralabs <liara@swizzin.ltd>
2017-09-24 09:27:12 -0700

    Move bulk of panel install to nginx and leave wrapper script in its place

--- scripts/install/panel.sh
+++ scripts/install/panel.sh
@@ -9,67 +9,10 @@
 #   including (via compiler) GPL-licensed code must also be made available
 #   under the GPL along with build & install instructions.
 #
-IFACE=$(ip link show|grep -i broadcast|grep -m1 UP|cut -d: -f 2|cut -d@ -f 1|sed -e 's/ //g');
-user=$(cat /root/.master.info | cut -d: -f1)
-if [[ -f /tmp/.install.lock ]]; then
-  log="/root/logs/install.log"
-else
-  log="/dev/null"
-fi
-
 if [[ ! -f /install/.nginx.lock ]]; then
   echo "ERROR: Web server not detected. Please install nginx and restart panel install."
-else
-  cd /srv/
-  git clone https://github.com/liaralabs/quickbox_dashboard.git panel >>$log 2>&1
-
-  chown -R www-data: /srv/panel
-
-  touch /srv/panel/db/output.log
-  printf "${IFACE}" > /srv/panel/db/interface.txt
-  printf "${user}" > /srv/panel/db/master.txt
-  LOCALE=en_GB.UTF-8
-  LANG=lang_en
-  sed -i "s/LOCALE/${LOCALE}/g" /srv/panel/inc/localize.php
-  sed -i "s/LANG/${LANG}/g" /srv/panel/inc/localize.php
-  echo "*/1 * * * * root bash /usr/local/bin/swizzin/panel/set_interface" > /etc/cron.d/set_interface
-
-  cat > /etc/nginx/apps/panel.conf <<PAN
-location / {
-alias /srv/panel/ ;
-auth_basic "What's the password?";
-auth_basic_user_file /etc/htpasswd;
-try_files \$uri \$uri/ /index.php?q=\$uri&\$args;
-index index.php;
-allow all;
-location ~ \.php$
-  {
-    include snippets/fastcgi-php.conf;
-    fastcgi_pass unix:/run/php/php7.0-fpm.sock;
-    #fastcgi_index index.php;
-    fastcgi_param SCRIPT_FILENAME /srv/panel\$fastcgi_script_name;
-  }
-}
-
-PAN
-
-cat > /etc/sudoers.d/panel <<SUD
-#secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin/swizzin:/usr/local/bin/swizzin/scripts:/usr/local/bin/swizzin/scripts/install:/usr/local/bin/swizzin/scripts/remove:/usr/local/bin/swizzin/panel"
-#Defaults  env_keep -="HOME"
-
-# Host alias specification
-
-# User alias specification
-
-# Cmnd alias specification
-Cmnd_Alias   CLEANMEM = /usr/local/bin/swizzin/panel/clean_mem, /proc/sys/vm/drop_caches
-Cmnd_Alias   SYSCMNDS = /usr/local/bin/swizzin/panel/clean_log, /usr/local/bin/swizzin/panel/set_interface, /usr/local/bin/swizzin/panel/lang/langSelect-*, /usr/local/bin/swizzin/panel/theme/themeSelect-*, /usr/local/bin/swizzin/box
-Cmnd_Alias   PACKAGECMNDS = /usr/local/bin/swizzin/install/*, /usr/local/bin/swizzin/remove/*
-Cmnd_Alias   GENERALCMNDS = /usr/bin/ifstat, /usr/bin/vnstat, /usr/sbin/repquota, /bin/grep, /usr/bin/awk, /usr/bin/reload, /bin/systemctl force-reload nginx, /usr/bin/pkill, /usr/bin/killall, /bin/sed, /bin/systemctl
-
-www-data     ALL = (ALL) NOPASSWD: CLEANMEM, SYSCMNDS, PACKAGECMNDS, GENERALCMNDS
-
-SUD
-  service nginx force-reload
-  touch /install/.panel.lock
+  exit 1
 fi
+
+bash /usr/local/bin/swizzin/nginx/panel.sh
+touch /install/.panel.lock
