25914c595847b0121373daeeab225f697b2ad426
liara <liara@swizzin.ltd>
2018-02-16 20:27:34 -0800

    Ensure rtorrent nginx scgi configs are removed upon ruTorrent uninstall

--- scripts/remove/rutorrent.sh
+++ scripts/remove/rutorrent.sh
@@ -2,8 +2,15 @@
 # ruTorrent removal
 # Author: liara
 
+users=($(cat /etc/htpasswd | cut -d ":" -f 1))
+
 rm -rf /srv/rutorrent
 rm -rf /etc/nginx/apps/rutorrent.conf
+
+for u in "${users[@]}"; do
+  rm -f /etc/nginx/apps/${u}.scgi.conf
+done
+
 if [[ ! -f /install/.flood.lock ]]; then
   rm -rf /etc/nginx/apps/rindex.conf
 fi
