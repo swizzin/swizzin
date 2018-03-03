3433a34ec5c40455657e8fe92f4bbaf8ce4a1b36
liara <liara@swizzin.ltd>
2018-03-03 14:20:14 -0800

    Add further support for DarkBetter

--- scripts/update/rutorrent.sh
+++ scripts/update/rutorrent.sh
@@ -6,3 +6,10 @@ if [[ -d /srv/rutorrent/plugins/theme/themes/club-QuickBox ]]; then
     git reset HEAD --hard
     git pull
 fi
+
+if [[ -d /srv/rutorrent/plugins/theme/themes/DarkBetter ]]; then
+    if [[ -z "$(ls -A /srv/rutorrent/plugins/theme/themes/DarkBetter/)" ]]; then
+        cd /srv/rutorrent
+        git submodule update --init --recursive > /dev/null 2>&1
+    fi
+fi
