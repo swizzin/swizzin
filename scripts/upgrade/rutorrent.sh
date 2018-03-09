4a8738125f2ad929b55561987f22188cf3008ebe
liara <liara@swizzin.ltd>
2018-03-09 13:14:22 -0800

    EXPERIMENTAL: ruTorrent upgrade script -- upgrade at your own risk (will create a backup before the upgrade)

--- scripts/upgrade/rutorrent.sh
+++ scripts/upgrade/rutorrent.sh
@@ -11,6 +11,30 @@ if [[ ! -f /install/.rutorrent.lock ]]; then
   echo "ruTorrent doesn't appear to be installed. Script exiting."
   exit 1
 fi
+PID=$$
+cd /srv/rutorrent/plugins
+plugs=($(echo */|sed 's/\///g'))
+cp -a /srv/rutorrent /srv/rutorrent.${PID}
+cd /srv/rutorrent
+git reset > /dev/null 2>&1
+git checkout -- php/settings.php > /dev/null 2>&1
+git pull
+if grep -q network.port php/settings.php; then
+  :
+else
+  git apply /etc/swizzin/sources/network.port.patch > /dev/null 2>&1
+fi
+find . -user root -exec chown www-data: {} \;
+cd /srv/rutorrent/plugins
+newplugs=($(echo */|sed 's/\///g'))
+for i in ${newplugs[@]}; do
+  if [[ ! ${plugs[@]} =~ $i ]]; then
+    echo "Removing git pull cruft of $i"
+    rm -rf $i
+  fi
+done
 
-bash /usr/local/bin/swizzin/nginx/rutorrent.sh
+echo "Previous ruTorrent directory has been backed up to /srv/rutorrent.${PID}. Please ensure ruTorrent is behaving as expected before you remove this backup!"
+
+#bash /usr/local/bin/swizzin/nginx/rutorrent.sh
 systemctl force-reload nginx
