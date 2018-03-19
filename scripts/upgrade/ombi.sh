4ecbc540e1d09bf7f1208958d22eff179a982d77
liara <liara@swizzin.ltd>
2018-03-19 08:42:06 -0700

    Update ombi upgrade script for new zip structure

--- scripts/upgrade/ombi.sh
+++ scripts/upgrade/ombi.sh
@@ -6,12 +6,12 @@ echo "Upgrading Ombi. Please wait ... "
 user=$(cat /root/.master.info | cut -d: -f1)
 systemctl stop ombi
 cd /opt
-cp ombi/Ombi.sqlite .
-rm -rf ombi
 curl -sL https://git.io/vKEJz | grep release | grep zip | cut -d "\"" -f 2 | sed -e 's/\/tidusjar/https:\/\/github.com\/tidusjar/g' | xargs wget --quiet -O Ombi.zip >/dev/null 2>&1
+mkdir ombi
+mv Ombi.zip ombi
+cd ombi
 unzip Ombi.zip >/dev/null 2>&1
-mv Release ombi
-mv Ombi.sqlite ombi
-rm -rf Ombi.zip
+rm Ombi.zip
+cd /opt
 chown -R ${user}: ombi
 systemctl start ombi
