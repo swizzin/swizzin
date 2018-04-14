3d149ded6b02ae40dfa97f12a6ae5d7f7d443e61
liara <liara@swizzin.ltd>
2018-04-13 18:24:09 -0700

    Fix typo

--- setup.sh
+++ setup.sh
@@ -293,7 +293,7 @@ function _post {
   #echo "export PATH=\$PATH:/usr/local/bin/swizzin" >> /home/$user/.bashrc
   #chown ${user}: /home/$user/.profile
   echo "Defaults    secure_path = /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin/swizzin" > /etc/sudoers.d/secure_path
-  if [[ $disitrbution = "Ubuntu" ]]; then
+  if [[ $distribution = "Ubuntu" ]]; then
     echo 'Defaults  env_keep -="HOME"' > /etc/sudoers.d/env_keep
   fi
   echo "Installation complete!"
