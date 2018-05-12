a30f6638b8d692906f8e009222774983c21780e6
liara <liara@swizzin.ltd>
2018-05-12 13:24:24 -0700

    Fix ambiguous redirect (Fixes #105)

--- scripts/upgrade/emby.sh
+++ scripts/upgrade/emby.sh
@@ -4,5 +4,5 @@
 current=$(curl -L -s -H 'Accept: application/json' https://github.com/MediaBrowser/Emby/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 cd /tmp
 wget -q -O emby.dpkg https://github.com/MediaBrowser/Emby/releases/download/${current}/emby-server-deb_${current}_amd64.deb
-dpkg -i emby.dpkg >> $OUTTO 2>&1
+dpkg -i emby.dpkg >> /dev/null 2>&1
 rm emby.dpkg
