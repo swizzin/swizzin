e39f3ddb9530a58978bc5af16de04e6277e19397
Kenny Deckers <deckers.kenny@gmail.com>
2018-08-25 10:24:37 +0200

    Updated the url structure of the emby packages

--- scripts/install/emby.sh
+++ scripts/install/emby.sh
@@ -39,7 +39,7 @@ echo "Installing emby keys and sources ... " >>"${OUTTO}" 2>&1;
     if [[ $CODENAME =~ ("artful"|"bionic") ]]; then
       current=$(curl -L -s -H 'Accept: application/json' https://github.com/MediaBrowser/Emby/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
       cd /tmp
-      wget -q -O emby.dpkg https://github.com/MediaBrowser/Emby/releases/download/${current}/emby-server-deb_${current}_amd64.deb
+      wget -q -O emby.dpkg https://github.com/MediaBrowser/Emby.Releases/releases/download/${current}/emby-server-deb_${current}_amd64.deb
       dpkg -i emby.dpkg >> $OUTTO 2>&1
       rm emby.dpkg
     else
