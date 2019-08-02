9cfa91c6aa5a1f5b8d7cd82ee9480f90c754bb03
liaralabs <liara@swizzin.ltd>
2019-08-02 10:52:27 -0700

    Refactor and fix some bugs

--- scripts/install/x2go.sh
+++ scripts/install/x2go.sh
@@ -2,9 +2,9 @@
 #
 # x2go installer
 #
-# Originally written for QuickBox.io by liara
+# Author: liara
 #
-# QuickBox Copyright (C) 2016
+# swizzin Copyright (C) 2019
 # Licensed under GNU General Public License v3.0 GPL-3 (in short)
 #
 #   You may copy, distribute and modify the software as long as you track
@@ -13,11 +13,28 @@
 #   under the GPL along with build & install instructions.
 #
 #################################################################################
-function _x2gorepo() {
+
+if [[ -f /tmp/.install.lock ]]; then
+  log="/root/logs/install.log"
+elif [[ -f /install/.panel.lock ]]; then
+  log="/srv/panel/db/output.log"
+else
+  log="/dev/null"
+fi
+
+distribution=$(lsb_release -is)
+release=$(lsb_release -cs)
+echo -n "Installing Xfce4 (this may take a bit) ... "
+apt-get install -y xfce4 >> ${log} 2>&1
+#disable lightdm because it causes suspend issues on Ubuntu
+systemctl disable --now lightdm >> ${log} 2>&1
+
+echo -n "Installing x2go repositories ... "
+
 if [[ $distribution == Ubuntu ]]; then
-	apt-get install -q -y software-properties-common > /dev/null 2>&1
-	apt-add-repository ppa:x2go/stable -y >/dev/null 2>&1
-	apt-get -y update >/dev/null 2>&1
+	apt-get install -q -y software-properties-common >> ${log} 2>&1
+	apt-add-repository ppa:x2go/stable -y >> ${log} 2>&1
+	apt-get -y update >> ${log} 2>&1
 else
 
 cat >/etc/apt/sources.list.d/x2go.list<<EOF
@@ -32,45 +49,21 @@ deb-src http://packages.x2go.org/debian ${release} main
 #deb-src http://packages.x2go.org/debian ${release} heuler
 EOF
 
-apt-get -y update >/dev/null 2>&1
+apt-get -y update>> ${log} 2>&1
 if [[ $release == "jessie" ]]; then
-	gpg --keyserver keys.gnupg.net --recv E1F958385BFE2B6E >/dev/null 2>&1
+	gpg --keyserver keys.gnupg.net --recv E1F958385BFE2B6E >> ${log} 2>&1
 	gpg --export E1F958385BFE2B6E > /etc/apt/trusted.gpg.d/x2go.gpg
 else
-  apt-key --keyring /etc/apt/trusted.gpg.d/x2go.gpg adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E1F958385BFE2B6E > /dev/null 2>&1
+  apt-key --keyring /etc/apt/trusted.gpg.d/x2go.gpg adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E1F958385BFE2B6E >> ${log} 2>&1
 fi
 
-apt-get -y update >/dev/null 2>&1
-apt-get -y install x2go-keyring >/dev/null 2>&1 && apt-get update >/dev/null 2>&1
+apt-get -y update >> ${log} 2>&1
+apt-get -y install x2go-keyring >> ${log} 2>&1 && apt-get update >> ${log} 2>&1
 fi
-}
-function _x2go() {
-		_x2gorepo
-			apt-get -y install x2goserver x2goserver-xsession >/dev/null 2>&1
-			apt-get -y install pulseaudio >/dev/null 2>&1
-			apt-get -y install iceweasel >/dev/null 2>&1
-		echo ${ok}
-}
 
-if [[ -f /tmp/.install.lock ]]; then
-  OUTTO="/root/logs/install.log"
-elif [[ -f /install/.panel.lock ]]; then
-  OUTTO="/srv/panel/db/output.log"
-else
-  OUTTO="/dev/null"
-fi
-distribution=$(lsb_release -is)
-release=$(lsb_release -cs)
-ok=$(echo -e "[ \e[0;32mDONE\e[00m ]")
-echo -n "Installing Xfce4 (this may take a bit) ... " >>"${OUTTO}" 2>&1;apt-get install -y xfce4 >/dev/null 2>&1
-echo >>"${OUTTO}" 2>&1;
-echo -n "Installing x2go repositories ... " >>"${OUTTO}" 2>&1; _x2gorepo
-echo >>"${OUTTO}" 2>&1;
-echo -n "Installing X2go (this may take a bit) ... " >>"${OUTTO}" 2>&1; _x2go
+
+echo -n "Installing X2go (this may take a bit) ... "
+apt-get -y install x2goserver x2goserver-xsession >> ${log} 2>&1
+apt-get -y install pulseaudio >> ${log} 2>&1
+
 touch /install/.x2go.lock
-echo >>"${OUTTO}" 2>&1;
-echo "X2go Install Complete!" >>"${OUTTO}" 2>&1;
-sleep 5
-echo >>"${OUTTO}" 2>&1;
-echo >>"${OUTTO}" 2>&1;
-echo "Close this dialog box to refresh your browser" >>"${OUTTO}" 2>&1;
