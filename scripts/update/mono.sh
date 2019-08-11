5218e7bce306816f08d2b995594beca4a5e1231e
liaralabs <liara@swizzin.ltd>
2019-08-11 09:09:16 -0700

    Fix sonarr apt repository signing key

--- scripts/update/mono.sh
+++ scripts/update/mono.sh
@@ -19,3 +19,19 @@ if [[ -f /install/.sonarr.lock ]] || [[ -f /install/.radarr.lock ]] || [[ -f /in
   fi
   done
 fi
+
+if [[ -f /install/.sonarr.lock ]]; then
+if ! apt-key adv --list-public-keys | grep A236C58F409091A18ACA53CBEBFF6B99D9B78493; then
+  version=$(lsb_release -cs)
+  distribution=$(lsb_release -is)
+  if [[ $distribution == "Ubuntu" ]]; then
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493 >/dev/null 2>&1
+  elif [[ $distribution == "Debian" ]]; then
+    if [[ $version == "jessie" ]]; then
+      apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493 >/dev/null 2>&1
+    else
+      #buster friendly
+      apt-key --keyring /etc/apt/trusted.gpg.d/nzbdrone.gpg adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493
+    fi
+  fi
+fi
