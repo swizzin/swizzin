17a98812fb91813dd6bad81c2b11447aa45e2cd1
liaralabs <liara@swizzin.ltd>
2019-08-12 12:02:22 -0700

    fix syntax

--- scripts/update/mono.sh
+++ scripts/update/mono.sh
@@ -21,17 +21,18 @@ if [[ -f /install/.sonarr.lock ]] || [[ -f /install/.radarr.lock ]] || [[ -f /in
 fi
 
 if [[ -f /install/.sonarr.lock ]]; then
-if ! apt-key adv --list-public-keys | grep A236C58F409091A18ACA53CBEBFF6B99D9B78493; then
-  version=$(lsb_release -cs)
-  distribution=$(lsb_release -is)
-  if [[ $distribution == "Ubuntu" ]]; then
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493 >/dev/null 2>&1
-  elif [[ $distribution == "Debian" ]]; then
-    if [[ $version == "jessie" ]]; then
-      apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493 >/dev/null 2>&1
-    else
-      #buster friendly
-      apt-key --keyring /etc/apt/trusted.gpg.d/nzbdrone.gpg adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493
+  if ! apt-key adv --list-public-keys | grep A236C58F409091A18ACA53CBEBFF6B99D9B78493; then
+    version=$(lsb_release -cs)
+    distribution=$(lsb_release -is)
+    if [[ $distribution == "Ubuntu" ]]; then
+      apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493 >/dev/null 2>&1
+    elif [[ $distribution == "Debian" ]]; then
+      if [[ $version == "jessie" ]]; then
+        apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493 >/dev/null 2>&1
+      else
+        #buster friendly
+        apt-key --keyring /etc/apt/trusted.gpg.d/nzbdrone.gpg adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493
+      fi
     fi
   fi
 fi
