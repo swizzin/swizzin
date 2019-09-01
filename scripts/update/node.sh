357121c747ea08dfe92982d2c5d1976caab5fa26
liaralabs <liara@swizzin.ltd>
2019-08-31 21:36:26 -0700

    Fix sed command

--- scripts/update/node.sh
+++ scripts/update/node.sh
@@ -2,10 +2,12 @@
 # Node update check
 # author: liara
 
+if [[ -f /etc/apt/sources.list.d/nodesource.listE ]]; then mv /etc/apt/sources.list.d/nodesource.listE /etc/apt/sources.list.d/nodesource.list; fi
+
 if [[ -f /etc/apt/sources.list.d/nodesource.list ]]; then
   if ! grep -q 10 /etc/apt/sources.list.d/nodesource.list; then
     echo "Upgrading nodejs to version 10 LTS"
-    sed -iE 's/[0-9]+/10/g' /etc/apt/sources.list.d/nodesource.list
+    sed -i -E 's/[0-9]+/10/g' /etc/apt/sources.list.d/nodesource.list
     apt -y -q  update > /dev/null 2>&1
     apt -y -q upgrade > /dev/null 2>&1
   fi
