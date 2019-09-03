a69fbd6ff052c5aff6c50f7d4b81b47756254447
liaralabs <liara@swizzin.ltd>
2019-09-03 10:10:18 -0700

    Refactor emby scripts to utilize github in all cases (Fixes #234)

--- scripts/remove/emby.sh
+++ scripts/remove/emby.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
-#
-# [Quick Box :: Remove emby-server package]
-#
-# GITHUB REPOS
-# GitHub _ packages  :   https://github.com/QuickBox/quickbox_packages
-# LOCAL REPOS
-# Local _ packages   :   /etc/QuickBox/packages
-# Author             :   QuickBox.IO | JMSolo
-# URL                :   https://quickbox.io
-#
-# QuickBox Copyright (C) 2017 QuickBox.io
-# Licensed under GNU General Public License v3.0 GPL-3 (in short)
-#
-#   You may copy, distribute and modify the software as long as you track
-#   changes/dates in source files. Any modifications to our software
-#   including (via compiler) GPL-licensed code must also be made available
-#   under the GPL along with build & install instructions.
-#
+# Removal script for emby
 
-function _removeEmby() {
-  dpkg -r emby-server >/dev/null 2>&1
-  sudo apt-get purge -y emby-server >/dev/null 2>&1
-  rm -rf /etc/apt/sources.list.d/emby-server.list
-  rm -rf /etc/nginx/apps/emby.conf
-  rm -rf /install/.emby.lock
-  pkill -f emby-server
-}
+systemctl disable --now emby-server > /dev/null 2>&1
+dpkg -r emby-server >/dev/null 2>&1
+if [[ -f /etc/apt/sources.list.d/emby-server.list ]]; then
+  rm /etc/apt/sources.list.d/emby-server.list
+fi
+rm -rf /etc/nginx/apps/emby.conf
+rm -rf /install/.emby.lock
 
-_removeEmby
