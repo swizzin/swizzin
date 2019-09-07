6e4c9a755f63b9fe3a3619bb7996c77f78bded3b
liaralabs <liara@swizzin.ltd>
2019-09-07 11:58:01 -0700

    Rework node installation and do not specify npm (it should absolutely be getting installed with 'nodejs' under the nodesource repo)

--- scripts/update/node.sh
+++ scripts/update/node.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 # Node update check
 # author: liara
-
+. /etc/swizzin/sources/functions/npm
 if [[ -f /etc/apt/sources.list.d/nodesource.listE ]]; then mv /etc/apt/sources.list.d/nodesource.listE /etc/apt/sources.list.d/nodesource.list; fi
-
-if [[ -f /etc/apt/sources.list.d/nodesource.list ]]; then
-  if ! grep -q 10 /etc/apt/sources.list.d/nodesource.list; then
-    echo "Upgrading nodejs to version 10 LTS"
-    sed -i -E 's/[0-9]+/10/g' /etc/apt/sources.list.d/nodesource.list
-    apt -y -q  update > /dev/null 2>&1
-    apt -y -q upgrade > /dev/null 2>&1
-  fi
-fi
+npm_update
