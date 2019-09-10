5ac8f6142cdeafa0075215f63d9be6ebc51c83db
liaralabs <liara@swizzin.ltd>
2019-09-10 09:47:34 -0700

    Remove duplicate proxy headers (#240)

--- scripts/nginx/jackett.sh
+++ scripts/nginx/jackett.sh
@@ -23,9 +23,6 @@ location /jackett {
 location /jackett/ {
   include /etc/nginx/snippets/proxy.conf;
   proxy_pass http://127.0.0.1:9117/jackett/;
-  proxy_set_header X-Real-IP \$remote_addr;
-  proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-  proxy_set_header X-Forwarded-Proto https;
   auth_basic "What's the password?";
   auth_basic_user_file /etc/htpasswd.d/htpasswd.${MASTER};
 }
