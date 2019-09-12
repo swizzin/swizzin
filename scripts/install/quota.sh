78b400b8a65bf444905025c61264dcc46f1d7f48
liaralabs <liara@swizzin.ltd>
2019-09-12 09:17:23 -0700

    Ensure quota script also installs the correct plugin for ruTorrent (#239)

--- scripts/install/quota.sh
+++ scripts/install/quota.sh
@@ -113,9 +113,10 @@ function _installquota(){
 #################################################################################
   require_once( '../../php/util.php' );
   if (isset($quotaUser) && file_exists('/install/.quota.lock')) {
-      $total = shell_exec("/usr/bin/sudo /usr/sbin/repquota -u MOUNT | /bin/grep ^".$quotaUser." | /usr/bin/awk '{printf $4*1024}'");
-      $free = shell_exec("/usr/bin/sudo /usr/sbin/repquota -u MOUNT | /bin/grep ^".$quotaUser." | /usr/bin/awk '{printf ($4-$3)*1024}'");
-      cachedEcho('{ "total": '.$total.', "free": '.$free.' }',"application/json");
+    $total = shell_exec("sudo /usr/bin/quota -u ".$quotaUser."| tail -n 1 | sed -e 's|^[ \t]*||' | awk '{print $3*1024}'");
+    $used = shell_exec("sudo /usr/bin/quota -u ".$quotaUser."| tail -n 1 | sed -e 's|^[ \t]*||' | awk '{print $2*1024}'");
+    $free = sprintf($total - $used);
+    cachedEcho('{ "total": '.$total.', "free": '.$free.' }',"application/json");
   } else {
       cachedEcho('{ "total": '.disk_total_space($topDirectory).', "free": '.disk_free_space($topDirectory).' }',"application/json");
   }
