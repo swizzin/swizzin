1a90f4423ab3ed570bf408d6d8733b17e91453b9
liaralabs <liara@swizzin.ltd>
2019-10-31 10:54:42 -0700

    Update nextcloud nginx configuration for CVE-2019-11043

--- scripts/update/nextcloud.sh
+++ scripts/update/nextcloud.sh
@@ -1,112 +1,18 @@
 #!/bin/bash
-#
-# [Install nextcloud package]
-#
-# Author:   liara for QuickBox.io
-# Ported to swizzin by liara
-#
-# QuickBox Copyright (C) 2017 QuickBox.io
-# Licensed under GNU General Public License v3.0 GPL-3 (in short)
-#
-#   You may copy, distribute and modify the software as long as you track
-#   changes/dates in source files. Any modifications to our software
-#   including (via compiler) GPL-licensed code must also be made available
-#   under the GPL along with build & install instructions.
+# Nextcloud updates
 
-inst=$(which mysql)
-ip=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
-if [[ ! -f /install/.nginx.lock ]]; then
-  echo "ERROR: Web server not detected. Please install nginx and restart panel install."
-  exit 1
-else
-echo "Please choose a password for the nextcloud mysql user."
-read -s -p "Password: " 'nextpass'
-#Check for existing mysql and install if not found
-if [[ -n $inst ]]; then
-  echo -n -e "Existing mysql server detected!\n"
-  echo -n -e "Please enter mysql root password so that installation may continue:\n"
-  read -s -p "Password: " 'password'
-  echo -e "Please wait while nextcloud is installed ... "
-
-else
-  echo -n -e "No mysql server found! Setup will install. \n"
-  echo -n -e "Please enter a mysql root password \n"
-  while [ -z "$password" ]; do
-    read -s -p "Password: " 'pass1'
-    echo
-    read -s -p "Re-enter password to verify: " 'pass2'
-    if [ $pass1 = $pass2 ]; then
-       password=$pass1
+if [[ -f /install/.nextcloud.lock ]]; then
+  if ! grep -q 'set $path_info' /etc/nginx/apps/nextcloud.conf; then
+    if [[ -f /lib/systemd/system/php7.3-fpm.service ]]; then
+      sock=php7.3-fpm
+    elif [[ -f /lib/systemd/system/php7.2-fpm.service ]]; then
+      sock=php7.2-fpm
+    elif [[ -f /lib/systemd/system/php7.1-fpm.service ]]; then
+      sock=php7.1-fpm
     else
-       echo
-       echo "Passwords do not match"
+      sock=php7.0-fpm
     fi
-  done
-  echo -e "Please wait while nextcloud is installed ... "
-  DEBIAN_FRONTEND=non‌​interactive apt-get -y install mariadb-server > /dev/null 2>&1
-  if [[ $(systemctl is-active mysql) != "active" ]]; then
-    systemctl start mysql
-  fi
-  mysqladmin -u root password ${password}
-fi
-#Depends
-apt-get install -y -q unzip php-mysql libxml2-dev php-common php-gd php-json php-curl  php-zip php-xml php-mbstring > /dev/null 2>&1
-#a2enmod rewrite > /dev/null 2>&1
-cd /tmp
-
-#Nextcloud 16 no longer supports php7.0, so 15 is the last supported release for Debian 9
-codename=$(lsb_release -cs)
-if [[ $codename =~ ("stretch"|"jessie"|"xenial") ]]; then
-  version="nextcloud-$(curl -s https://nextcloud.com/changelog/ | grep -A5 '"latest15"' | grep 'id=' | cut -d'"' -f2 | sed 's/-/./g')"
-else
-  version=latest
-fi
-wget -q https://download.nextcloud.com/server/releases/${version}.zip > /dev/null 2>&1
-unzip ${version}.zip > /dev/null 2>&1
-mv nextcloud /srv
-rm -rf /tmp/${version}.zip
-
-#Set permissions as per nextcloud
-ocpath='/srv/nextcloud'
-htuser='www-data'
-htgroup='www-data'
-rootuser='root'
-
-mkdir -p $ocpath/data
-mkdir -p $ocpath/assets
-mkdir -p $ocpath/updater
-find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
-find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
-chown -R ${rootuser}:${htgroup} ${ocpath}/
-chown -R ${htuser}:${htgroup} ${ocpath}/apps/
-chown -R ${htuser}:${htgroup} ${ocpath}/assets/
-chown -R ${htuser}:${htgroup} ${ocpath}/config/
-chown -R ${htuser}:${htgroup} ${ocpath}/data/
-chown -R ${htuser}:${htgroup} ${ocpath}/themes/
-chown -R ${htuser}:${htgroup} ${ocpath}/updater/
-chmod +x ${ocpath}/occ
-if [ -f ${ocpath}/.htaccess ]
-then
- chmod 0644 ${ocpath}/.htaccess
- chown ${rootuser}:${htgroup} ${ocpath}/.htaccess
-fi
-if [ -f ${ocpath}/data/.htaccess ]
-then
- chmod 0644 ${ocpath}/data/.htaccess
- chown ${rootuser}:${htgroup} ${ocpath}/data/.htaccess
-fi
-
-if [[ -f /lib/systemd/system/php7.3-fpm.service ]]; then
-  sock=php7.3-fpm
-elif [[ -f /lib/systemd/system/php7.2-fpm.service ]]; then
-  sock=php7.2-fpm
-elif [[ -f /lib/systemd/system/php7.1-fpm.service ]]; then
-  sock=php7.1-fpm
-else
-  sock=php7.0-fpm
-fi
-
-cat > /etc/nginx/apps/nextcloud.conf <<EOF
+    cat > /etc/nginx/apps/nextcloud.conf <<EOF
 # The following 2 rules are only needed for the user_webfinger app.
 # Uncomment it if you're planning to use this app.
 #rewrite ^/.well-known/host-meta /nextcloud/public.php?service=host-meta last;
@@ -211,17 +117,6 @@ location ^~ /nextcloud {
     }
 }
 EOF
-
-mysql --user="root" --password="$password" --execute="CREATE DATABASE nextcloud;"
-mysql --user="root" --password="$password" --execute="CREATE USER nextcloud@localhost IDENTIFIED BY '$nextpass';"
-mysql --user="root" --password="$password" --execute="GRANT ALL PRIVILEGES ON nextcloud.* TO nextcloud@localhost;"
-mysql --user="root" --password="$password" --execute="FLUSH PRIVILEGES;"
-
-service nginx reload
-touch /install/.nextcloud.lock
-
-echo -e "Visit https://${ip}/nextcloud to finish installation."
-echo -e "Database user: nextcloud"
-echo -e "Database password: ${nextpass}"
-echo -e "Database name: nextcloud"
+    systemctl reload nginx
+  fi
 fi
