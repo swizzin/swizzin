3ce7bf73587122789bb37bf225bb60bbd3f3e85e
liaralabs <liara@swizzin.ltd>
2020-03-30 17:53:03 -0700

    Hide output of rm for files that are unlikely to exist anymore

--- scripts/remove/panel.sh
+++ scripts/remove/panel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 systemctl disable --now panel
-rm -rf /srv/panel
+rm -rf /srv/panel > /dev/null 2>&1
 rm -rf /opt/swizzin
 rm -f /etc/nginx/apps/panel.conf
 rm -f /etc/sudoers.d/panel
-rm /etc/cron.d/set_interface
+rm /etc/cron.d/set_interface > /dev/null 2>&1
 rm /install/.panel.lock
