7623a21aa1d1c2f11e8aa53536f172d7f5e4ff5f
liaralabs <liara@swizzin.ltd>
2020-04-05 18:52:54 -0700

    Use ACLs to ensure the panel group can read into user directories if it needs to

--- scripts/upgrade/panel.sh
+++ scripts/upgrade/panel.sh
@@ -2,7 +2,14 @@
 # Temporary upgrade script for the panel until panel becomes more self-sufficient
 
 if [[ -f /install/.panel.lock ]]; then
+  if ! dpkg -s acl > /dev/null 2>&1; then
+    echo "Modifying ACLs for swizzin group to prevent panel issues"
+    apt-get -y -q install acl
+    setfacl -m g:swizzin:rx /home/*
+  fi 
   cd /opt/swizzin/swizzin
+  echo "Resetting git repo"
+  git reset HEAD --hard
   echo "Pulling new commits"
   git pull
   echo "Restarting Panel"
