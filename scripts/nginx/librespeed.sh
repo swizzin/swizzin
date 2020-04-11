a7045f01cad31f7c1df490fbdcfc396a40e47c8f
hwcltjn <32576978+hwcltjn@users.noreply.github.com>
2020-04-11 04:27:30 +0300

    new application: LibreSpeed (#280)
    * new application: LibreSpeed
    
    This commit adds 3 new files for the purpose of including the LibreSpeed open source speed test to Swizzin
    
    * Added fallback if no domain has been configured in nginx

---
+++ scripts/nginx/librespeed.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Nginx configuration for LibreSpeed
+# Author - hwcltjn
+
+if [[ ! -f /etc/nginx/apps/librespeed.conf ]]; then
+	cat > /etc/nginx/apps/librespeed.conf <<RAP
+location /librespeed/ {
+	alias /srv/librespeed/;
+
+	client_max_body_size 50M;
+	client_body_buffer_size 128k;
+}
+RAP
+fi
