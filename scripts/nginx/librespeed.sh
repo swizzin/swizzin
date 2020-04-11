277b62bc7e6f1613fee46a6604af32466ecf5ec3
liaralabs <liara@swizzin.ltd>
2020-04-10 18:36:53 -0700

    Remove trailing slash

--- scripts/nginx/librespeed.sh
+++ scripts/nginx/librespeed.sh
@@ -5,8 +5,8 @@
 
 if [[ ! -f /etc/nginx/apps/librespeed.conf ]]; then
 	cat > /etc/nginx/apps/librespeed.conf <<RAP
-location /librespeed/ {
-	alias /srv/librespeed/;
+location /librespeed {
+	alias /srv/librespeed;
 
 	client_max_body_size 50M;
 	client_body_buffer_size 128k;
