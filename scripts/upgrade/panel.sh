516d0d10b943e9774118e71ce5172338ca216743
liaralabs <liara@swizzin.ltd>
2020-04-14 18:56:15 -0700

    Fix issue that was resetting custom profiles

--- scripts/upgrade/panel.sh
+++ scripts/upgrade/panel.sh
@@ -8,10 +8,18 @@ if [[ -f /install/.panel.lock ]]; then
     setfacl -m g:swizzin:rx /home/*
   fi 
   cd /opt/swizzin/swizzin
-  echo "Resetting git repo"
-  git reset HEAD --hard
+  #git reset HEAD --hard
   echo "Pulling new commits"
-  git pull
+  git pull 2> /dev/null || { PANELRESET=1; }
+  if [[ $PANELRESET == 1 ]]; then
+    echo "Working around unclean git repo"
+    git fetch origin master
+    cp -a core/custom core/custom.tmp
+    echo "Resetting git repo"
+    git reset --hard origin/master
+    mv core/custom.tmp/* core/custom/
+    rm -r core/custom.tmp
+  fi
   echo "Restarting Panel"
   systemctl restart panel
   echo "Done!"
