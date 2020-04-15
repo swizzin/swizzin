efc3c6f6608b575bfe0427e9f7fad3875a8c2818
liaralabs <30909703+liaralabs@users.noreply.github.com>
2020-04-15 16:17:34 -0700

    Update bazarr with python version management (#282)
    * Add python version management with pyenv
    
    * Update bazarr for pyenv
    
    * Ensure logging does not create issues in update
    
    * Ensure shell has variables loaded
    
    * Try another method
    
    * Fix directory to check

--- scripts/remove/bazarr.sh
+++ scripts/remove/bazarr.sh
@@ -3,6 +3,10 @@ user=$(cut -d: -f1 < /root/.master.info)
 systemctl disable --now bazarr
 
 rm -rf /home/$user/bazarr
+rm -rf /home/$user/.venv/bazarr
+if [ -z "$(ls -A /home/$user/.venv)" ]; then
+   rm -rf  /home/$user/.venv
+fi
 rm -rf /etc/nginx/apps/bazarr.conf
 rm -rf /install/.bazarr.lock
 rm -rf /etc/systemd/system/bazarr.service
