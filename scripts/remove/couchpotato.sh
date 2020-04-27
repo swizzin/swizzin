635fc01adc18f6d6f8e62b337a265df6c7233eb1
liaralabs <liara@swizzin.ltd>
2020-04-27 16:27:35 -0700

    Ensure data dir is removed

--- scripts/remove/couchpotato.sh
+++ scripts/remove/couchpotato.sh
@@ -5,6 +5,7 @@ user=$(cut -d: -f1 < /root/.master.info)
 systemctl disable --now couchpotato > /dev/null 2>&1
 rm /etc/systemd/system/couchpotato.service
 rm -rf /home/${user}/couchpotato
+rm -rf /home/${user}/.config/couchpotato
 rm -rf /home/${user}/.venv/couchpotato
 if [ -z "$(ls -A /home/${user}/.venv)" ]; then
    rm -rf  /home/${user}/.venv
