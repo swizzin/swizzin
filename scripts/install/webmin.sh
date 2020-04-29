76cfaa553c864d75df14059570e985647048ea68
Flying-sausages <flying_sausages@protonmail.com>
2020-04-29 16:59:31 +0000

    Add hook into letsencrypt to get domain if it exits, and guess from nginx config

--- scripts/install/webmin.sh
+++ scripts/install/webmin.sh
@@ -22,42 +22,45 @@ _install_webmin () {
 
 _get_domain_for_webmin () {
     if [[ -z $webmin_referers ]]; then
-        webmin_referers=$(whiptail --inputbox "Enter your host's domain or IP address, which you expect to find webmind under.\ne.g. \"sub.domain.com\", \"123.234.32.21\", etc." 10 50 3>&1 1>&2 2>&3); exitstatus=$?; if [ "$exitstatus" = 1 ]; then return 1; fi
+        if [[ -f /install/.letsencrypt.lock ]]; then 
+            webmin_referers=$(grep "server_name" /etc/nginx/sites-enabled/default |head -1|sed 's/^\s*//g'|cut -d ' ' -f2 | cut -d\; -f1)
+        else
+            guess=$(grep "server_name" /etc/nginx/sites-enabled/default |head -1|sed 's/^\s*//g'|cut -d ' ' -f2 | cut -d\; -f1)
+            if [[ $guess = '_' ]]; then 
+                guess=''
+            fi
+
+            if [[ -n $guess ]]; then
+                guesstext="\nBelow is a possible match from your nginx configuration."
+            fi
+            webmin_referers=$(whiptail --inputbox "Enter your host's domain or IP address, which you expect to find webmind under.\ne.g. \"sub.domain.com\", \"123.234.32.21\", etc.${guesstext}" 10 50 "${guess}" 3>&1 1>&2 2>&3); exitstatus=$?; if [ "$exitstatus" = 1 ]; then return 1; fi
+        fi
     fi
     echo $webmin_referers
 }
 
 _webmin_conf () {
-    #TODO figure out if there's a cleaner way to get this from nginx or something
-    referers=$(_get_domain_for_webmin)
-    cat >> /etc/webmin/config << EOF
+    if [[ -f /install/.nginx.lock ]]; then
+        
+        bash /etc/swizzin/scripts/nginx/webmin.sh
+        #TODO figure out if there's a cleaner way to get this from nginx or something
+        referers=$(_get_domain_for_webmin)
+        echo "If you change domain/IP in the future, please edit /etc/webmin/config"
+        cat >> /etc/webmin/config << EOF
 webprefix=/webmin
 webprefixnoredir=1
 referers=${referers}
-noremember=
-realname=
-
 EOF
-    cat >> /etc/webmin/miniserv.conf << EOF
+        cat >> /etc/webmin/miniserv.conf << EOF
 bind=127.0.0.1
 sockets=
-utmp=
-no_pam=0
-blockuser_time=
-logouttime=
-pam_conv=
-pam_end=
-blocklock=
-blockuser_failures=
-session_ip=
 EOF
-    echo "If you change domain/IP in the future, please edit /etc/webmin/config"
+    fi
     systemctl reload webmin
 }
 
 
 
-bash /etc/swizzin/scripts/nginx/webmin.sh
 
 _install_webmin
 _webmin_conf
