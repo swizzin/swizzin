3826338ae97e225436cf3994233ef1b2e362bb0e
Flying-sausages <flying_sausages@protonmail.com>
2020-04-29 18:19:50 +0000

    Move nginx stuff to the nginx function

--- scripts/install/webmin.sh
+++ scripts/install/webmin.sh
@@ -20,56 +20,8 @@ _install_webmin () {
     apt-get install webmin -yq >> $log 2>&1
 }
 
-_get_domain_for_webmin () {
-    if [[ -z $webmin_referers ]]; then
-
-        if [[ -f /install/.letsencrypt.lock ]]; then 
-            webmin_referers=$(grep -m1 "server_name" /etc/nginx/sites-enabled/default | awk '{print $2}' | sed 's/;//g')
-        else
-            guess=$(grep -m1 "server_name" /etc/nginx/sites-enabled/default | awk '{print $2}' | sed 's/;//g')
-            if [[ $guess = '_' ]]; then 
-                guess=''
-            fi
-            if [[ -n $guess ]]; then
-                guesstext="\nBelow is a possible match from your nginx configuration."
-            fi
-            webmin_referers=$(whiptail --inputbox "Enter your host's domain or IP address.\ne.g. \"sub.domain.com\", \"123.234.32.21\", etc.${guesstext}\nLeave empty to configure manually later" 10 50 "${guess}" 3>&1 1>&2 2>&3)
-        fi
-    fi
-    echo $webmin_referers
-}
-
-_webmin_conf () {
-    if [[ -f /install/.nginx.lock ]]; then
-        bash /etc/swizzin/scripts/nginx/webmin.sh
-
-        #TODO figure out if there's a cleaner way to get this from nginx or something
-        referers=$(_get_domain_for_webmin)
-        if [[ -z $referers ]]; then 
-            echo "You can set the IP/fqdn manually in /etc/webmin/conf"
-        else
-            echo "If you change domain/IP in the future, please edit /etc/webmin/config"
-        fi
-
-        cat >> /etc/webmin/config << EOF
-webprefix=/webmin
-webprefixnoredir=1
-referers=${referers}
-EOF
-        cat >> /etc/webmin/miniserv.conf << EOF
-bind=127.0.0.1
-sockets=
-EOF
-    systemctl reload webmin
-    fi
-    
-}
-
-
-
-
 _install_webmin
-_webmin_conf
+bash /etc/swizzin/scripts/nginx/webmin.sh
 
 echo 
 echo "Webmin has been installed, please use any account with sudo permissions to log in"
--- scripts/nginx/webmin.sh
+++ scripts/nginx/webmin.sh
@@ -23,4 +23,43 @@ location /webmin/ {
 EMB
 fi
 
+_get_domain_for_webmin () {
+    if [[ -z $webmin_referers ]]; then
+
+        if [[ -f /install/.letsencrypt.lock ]]; then 
+            webmin_referers=$(grep -m1 "server_name" /etc/nginx/sites-enabled/default | awk '{print $2}' | sed 's/;//g')
+        else
+            guess=$(grep -m1 "server_name" /etc/nginx/sites-enabled/default | awk '{print $2}' | sed 's/;//g')
+            if [[ $guess = '_' ]]; then 
+                guess=''
+            fi
+            if [[ -n $guess ]]; then
+                guesstext="\nBelow is a possible match from your nginx configuration."
+            fi
+            webmin_referers=$(whiptail --inputbox "Enter your host's domain or IP address.\ne.g. \"sub.domain.com\", \"123.234.32.21\", etc.${guesstext}\nLeave empty to configure manually later" 10 50 "${guess}" 3>&1 1>&2 2>&3)
+        fi
+    fi
+    echo $webmin_referers
+}
+
+#TODO figure out if there's a cleaner way to get this from nginx or something
+referers=$(_get_domain_for_webmin)
+if [[ -z $referers ]]; then 
+    echo "You can set the IP/fqdn manually in /etc/webmin/conf"
+else
+    echo "If you change domain/IP in the future, please edit /etc/webmin/config"
+fi
+
+cat >> /etc/webmin/config << EOF
+webprefix=/webmin
+webprefixnoredir=1
+referers=${referers}
+EOF
+cat >> /etc/webmin/miniserv.conf << EOF
+bind=127.0.0.1
+sockets=
+EOF
+
+systemctl reload webmin
+
 systemctl reload nginx
