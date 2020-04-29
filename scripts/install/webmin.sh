a54a59a15c85e5c8ddc83f236338f42d63304930
Flying-sausages <flying_sausages@protonmail.com>
2020-04-29 17:28:14 +0000

    Handling more domain stuff

--- scripts/install/webmin.sh
+++ scripts/install/webmin.sh
@@ -22,6 +22,7 @@ _install_webmin () {
 
 _get_domain_for_webmin () {
     if [[ -z $webmin_referers ]]; then
+
         if [[ -f /install/.letsencrypt.lock ]]; then 
             webmin_referers=$(grep -m1 "server_name" /etc/nginx/sites-enabled/default | awk '{print $2}' | sed 's/;//g')
         else
@@ -29,11 +30,11 @@ _get_domain_for_webmin () {
             if [[ $guess = '_' ]]; then 
                 guess=''
             fi
-
+            echo "guess = $guess"
             if [[ -n $guess ]]; then
                 guesstext="\nBelow is a possible match from your nginx configuration."
             fi
-            webmin_referers=$(whiptail --inputbox "Enter your host's domain or IP address, which you expect to find webmind under.\ne.g. \"sub.domain.com\", \"123.234.32.21\", etc.${guesstext}" 10 50 "${guess}" 3>&1 1>&2 2>&3); exitstatus=$?; if [ "$exitstatus" = 1 ]; then return 1; fi
+            webmin_referers=$(whiptail --inputbox "Enter your host's domain or IP address.\ne.g. \"sub.domain.com\", \"123.234.32.21\", etc.${guesstext}\nLeave empty to configure manually later" 10 50 "${guess}" 3>&1 1>&2 2>&3)
         fi
     fi
     echo $webmin_referers
@@ -41,11 +42,16 @@ _get_domain_for_webmin () {
 
 _webmin_conf () {
     if [[ -f /install/.nginx.lock ]]; then
-        
         bash /etc/swizzin/scripts/nginx/webmin.sh
+
         #TODO figure out if there's a cleaner way to get this from nginx or something
         referers=$(_get_domain_for_webmin)
-        echo "If you change domain/IP in the future, please edit /etc/webmin/config"
+        if [[ -z $referers ]]; then 
+            echo "You can set the IP/fqdn manually in /etc/webmin/conf"
+        else
+            echo "If you change domain/IP in the future, please edit /etc/webmin/config"
+        fi
+
         cat >> /etc/webmin/config << EOF
 webprefix=/webmin
 webprefixnoredir=1
@@ -55,8 +61,9 @@ EOF
 bind=127.0.0.1
 sockets=
 EOF
-    fi
     systemctl reload webmin
+    fi
+    
 }
 
 
