f8b156eb5aac0c5a28658e2ba37ca7d9aa88f49e
liaralabs <liara@swizzin.ltd>
2020-04-28 21:26:58 -0700

    Ensure pyload updates this directory

--- scripts/update/00-venv.sh
+++ scripts/update/00-venv.sh
@@ -7,6 +7,9 @@ if [[ -d /home/${user}/.venv ]]; then
         mv /home/${user}/${app} /opt
         sed -i "s|/home/${user}|/opt|g" /etc/systemd/system/${app}.service
         sed -i "s|/opt/.config|/home/${user}/.config|g" /etc/systemd/system/${app}.service
+        if [[ $app == "pyload" ]]; then
+            echo "/opt/pyload" > /opt/pyload/module/config/configdir
+        fi
         systemctl daemon-reload
         systemctl try-restart $app
     done
