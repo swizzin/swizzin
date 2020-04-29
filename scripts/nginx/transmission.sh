0ef96b64e9aa3bed1cbdf09bd72067ff877bf69a
Flying-sausages <flying_sausages@protonmail.com>
2020-04-29 20:30:43 +0000

    escape dollars in nginx heredoc

--- scripts/nginx/transmission.sh
+++ scripts/nginx/transmission.sh
@@ -12,9 +12,9 @@ location /transmission {
 }
 
 location /transmission/ {
-    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Real-IP \$remote_addr;
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-    proxy_set_header Host $http_host;
+    proxy_set_header Host \$http_host;
     proxy_set_header X-NginX-Proxy true;
     proxy_http_version 1.1;
     proxy_set_header Connection "";
