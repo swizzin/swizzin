8fff14d50cb2ce257ad66fbc4416fb45216601c0
Flying-sausages <flying_sausages@protonmail.com>
2020-05-03 04:45:49 +0000

    Try deal with the baseurl for an app that doesn't support it

--- scripts/nginx/wg-dashboard.sh
+++ scripts/nginx/wg-dashboard.sh
@@ -4,12 +4,13 @@
 MASTER=$(cut -d: -f1 < /root/.master.info)
 cat > /etc/nginx/apps/wg-dashboard.conf <<EOF
 location /wg-dashboard/ {
-proxy_pass http://127.0.0.1:3000/;
-proxy_http_version 1.1;
-proxy_set_header Connection "upgrade";
-proxy_set_header Upgrade \$http_upgrade;
-auth_basic "What's the password?";
-auth_basic_user_file /etc/htpasswd.d/htpasswd.${MASTER};
+  rewrite /wg-dashboard(.*) /$1 break;
+  proxy_pass http://127.0.0.1:3000/;
+  proxy_http_version 1.1;
+  proxy_set_header Connection "upgrade";
+  proxy_set_header Upgrade \$http_upgrade;
+  auth_basic "What's the password?";
+  auth_basic_user_file /etc/htpasswd.d/htpasswd.${MASTER};
 }
 EOF
 
