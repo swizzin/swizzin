188268dd929c0f04ed0b46d374e47a6f191524d0
Flying-sausages <flying_sausages@protonmail.com>
2020-05-03 03:46:46 +0000

    Added remove CoreDNS

--- scripts/remove/wg-dashboard.sh
+++ scripts/remove/wg-dashboard.sh
@@ -6,3 +6,6 @@ systemctl disable --now wg-dashboard
 rm -rf /opt/wg-dashboard
 rm /etc/systemd/system/wg-dashboard.service
 systemctl daemon-reload
+# shellcheck source=sources/functions/coredns
+. /etc/swizzin/sources/functions/coredns
+_remove_coredns
