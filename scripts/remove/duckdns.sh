b451887bb078b13820e00bef8bde11a134095943
Flying-sausages <flying_sausages@protonmail.com>
2020-05-11 00:38:50 +0000

    Retrieve installed values and unset domain as part of removal.

--- scripts/remove/duckdns.sh
+++ scripts/remove/duckdns.sh
@@ -2,10 +2,14 @@
 #duckdns yeeter
 #flying_sausages 2020 swizzin gplv3 respect mah autooritah
 
+eval "$(grep subdomain /opt/duckdns/duck.sh | head -1)"
+eval "$(grep token /opt/duckdns/duck.sh | head -1)"
+
+echo -n "Clearing set IP for $subdomain.duckdns.org... "
+echo url="https://www.duckdns.org/update?domains=$subdomain&token=$token&clear=true" | curl -k -K -
+echo
 rm -rf /opt/duckdns
 crontab -l | grep -v '/opt/duckdns'  | crontab -
 echo "DuckDNS Removed"
-echo
-echo "DuckDNS will keep pointing the domain to the IP until it is set to something else or is deleted"
 
 rm /install/.duckdns.lock
