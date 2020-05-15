8758e4049415ca0dc735dceffb894c4eafffb85b
Flying-sausages <flying_sausages@protonmail.com>
2020-05-15 02:36:27 +0000

    Switch to newinesep arr in remove

--- scripts/remove/wireguard.sh
+++ scripts/remove/wireguard.sh
@@ -4,8 +4,8 @@ codename=$(lsb_release -cs)
 
 #shellcheck source=sources/functions/utils
 . /etc/swizzin/sources/functions/utils
-users=("$(_get_user_list)")
-for u in "${users[@]}"; do
+users=($(_get_user_list))
+for u in ${users[@]}; do
     systemctl disable --now wg-quick@wg$(id -u $u)
     rm -rf /home/$u/.wireguard
 done
