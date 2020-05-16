bc73ba8973269f9e2e7462e82650570a67d5b593
Flying-sausages <flying_sausages@protonmail.com>
2020-05-16 00:37:30 +0000

    remove unused echo and read

--- scripts/remove/nextcloud.sh
+++ scripts/remove/nextcloud.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # nextcloud uninstaller
-echo -n -e "Please enter mysql root password so that nextcloud database and user can be dropped.\n"
-read -r -s -p "Password: " 'password'
-echo
 host=$(mysql -u root --password="$password" --execute="select host from mysql.user where user = 'nextcloud';" | grep -E "localhost|127.0.0.1")
 mysql --execute="DROP DATABASE nextcloud;"
 mysql --execute="DROP USER nextcloud@$host;"
