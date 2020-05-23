6253a4787142c959bb9d542f6d5c7c5e03270caa
Flying-sausages <flying_sausages@protonmail.com>
2020-05-23 08:56:06 +0000

    i give up

--- scripts/nginx/mango.sh
+++ scripts/nginx/mango.sh
@@ -3,17 +3,10 @@
 
 
 cat > /etc/nginx/apps/mango.conf <<EOF
-location /mango {
-  # TODO this does not work
-  # rewrite /mango(.*) /\$1 break;
-  # rewrite ^/mango/(.*)\$  /\$1 break;
-  proxy_pass http://127.0.0.1:9003/;
-  proxy_redirect https://\$host /mango;
-  proxy_set_header X-Real-IP \$remote_addr;
-  proxy_set_header X-Forwarded-For \$remote_addr;
-  proxy_set_header X-Request-URI \$request; #** This adds the original path as a header **
-  proxy_set_header Host \$host:\$server_port;
-
+location /mango/ {
+  # TODO This needs a doing
+  # rewrite ^/mango(.*) /\$1 break;
+  proxy_pass http://localhost:9003;
 }
 EOF
 
