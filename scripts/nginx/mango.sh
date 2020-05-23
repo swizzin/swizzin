677a8251b7dd2b3cb4544166ab06fcb6975b9dfb
Flying-sausages <flying_sausages@protonmail.com>
2020-05-23 05:42:08 +0000

    A stab hat doesn't work right now as here is no Baseurl support currently

---
+++ scripts/nginx/mango.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Nginx configuration for Mango
+
+
+cat > /etc/nginx/apps/mango.conf <<EOF
+location /mango {
+  # TODO this does not work
+  # rewrite /mango(.*) /\$1 break;
+  # rewrite ^/mango/(.*)\$  /\$1 break;
+  proxy_pass http://127.0.0.1:9003/;
+  proxy_redirect https://\$host /mango;
+  proxy_set_header X-Real-IP \$remote_addr;
+  proxy_set_header X-Forwarded-For \$remote_addr;
+  proxy_set_header X-Request-URI \$request; #** This adds the original path as a header **
+  proxy_set_header Host \$host:\$server_port;
+
+}
+EOF
+
+systemctl reload nginx
+
+
+  # proxy_http_version 1.1;
+  # proxy_set_header Connection "upgrade";
+  # proxy_set_header Upgrade \$http_upgrade;
+  # proxy_set_header X-Forwarded-For \$remote_addr;
