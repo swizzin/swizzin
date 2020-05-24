bf85c1849154a70c05415666437bdc926b08e13a
Flying-sausages <flying_sausages@protonmail.com>
2020-05-24 07:49:18 +0000

    Added configuration for BaseUrl

--- scripts/nginx/mango.sh
+++ scripts/nginx/mango.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # Nginx configuration for Mango
 
-
 cat > /etc/nginx/apps/mango.conf <<EOF
 location /mango/ {
-  # TODO This needs a doing
-  # rewrite ^/mango(.*) /\$1 break;
   proxy_pass http://localhost:9003;
 }
 EOF
