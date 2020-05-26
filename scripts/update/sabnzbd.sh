816ebf1514712aa5ba63581c11d7b917a4c0d6d0
liaralabs <liara@swizzin.ltd>
2020-05-26 12:17:13 -0700

    Add sabyenc to pip modules

--- scripts/update/sabnzbd.sh
+++ scripts/update/sabnzbd.sh
@@ -25,7 +25,7 @@ if [[ -f /install/.sabnzbd.lock ]]; then
 
         python2_venv ${user} sabnzbd
 
-        PIP='wheel setuptools dbus-python configobj feedparser pgi lxml utidylib yenc cheetah pyOpenSSL'
+        PIP='wheel setuptools dbus-python configobj feedparser pgi lxml utidylib yenc sabyenc cheetah pyOpenSSL'
         /opt/.venv/sabnzbd/bin/pip install $PIP >>"${log}" 2>&1
         chown -R ${user}: /opt/.venv/sabnzbd
 
