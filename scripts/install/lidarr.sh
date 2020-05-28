fdc3960f9ddcf8e655bffafb3710fa5c9a2f4109
Flying-sausages <flying_sausages@protonmail.com>
2020-05-28 15:23:57 +0000

    Add logging, fix linter issues

--- scripts/install/lidarr.sh
+++ scripts/install/lidarr.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#shellcheck disable=SC2129
 # Lidarr installer for swizzin
 # Author: liara
 # Copyright (C) 2019 Swizzin
@@ -8,19 +9,25 @@
 #   changes/dates in source files. Any modifications to our software
 #   including (via compiler) GPL-licensed code must also be made available
 #   under the GPL along with build & install instructions.
+if [[ -f /tmp/.install.lock ]]; then
+  log="/root/logs/install.log"
+else
+  log="/root/logs/swizzin.log"
+fi
+
 user=$(cut -d: -f1 < /root/.master.info )
 distribution=$(lsb_release -is)
 #shellcheck source=sources/functions/mono
 . /etc/swizzin/sources/functions/mono
 mono_repo_setup
-apt-get install -y libmono-cil-dev >/dev/null 2>&1
+apt-get install -y libmono-cil-dev >> $log 2>&1
 
-wget -O /tmp/lidarr.tar.gz -q $( curl -s https://api.github.com/repos/Lidarr/Lidarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
-tar xf /tmp/lidarr.tar.gz --directory /opt/
+wget -O /tmp/lidarr.tar.gz "$( curl -s https://api.github.com/repos/Lidarr/Lidarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )" >> $log 2>&1
+tar xfv /tmp/lidarr.tar.gz --directory /opt/ >> $log 2>&1
 rm -rf /tmp/lidarr.tar.gz
-chown -R ${user}: /opt/Lidarr
-if [[ ! -d /home/${user}/.config/Lidarr/ ]]; then mkdir -p /home/${user}/.config/Lidarr/; fi
-cat > /home/${user}/.config/Lidarr/config.xml <<LID
+chown -R "${user}": /opt/Lidarr
+if [[ ! -d /home/${user}/.config/Lidarr/ ]]; then mkdir -p "/home/${user}/.config/Lidarr/"; fi
+cat > "/home/${user}/.config/Lidarr/config.xml" <<LID
 <Config>
   <Port>8686</Port>
   <UrlBase>lidarr</UrlBase>
@@ -30,7 +37,7 @@ cat > /home/${user}/.config/Lidarr/config.xml <<LID
   <LaunchBrowser>False</LaunchBrowser>
 </Config>
 LID
-chown -R ${user}: /home/${user}/.config
+chown -R "${user}": "/home/${user}/.config"
 cat > /etc/systemd/system/lidarr.service <<LID
 [Unit]
 Description=lidarr for ${user}
