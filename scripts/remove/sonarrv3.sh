d9740b45a0fe145032f5a8a1823605a3a79ac4e0
Flying-sausages <flying_sausages@protonmail.com>
2020-06-29 23:30:25 +0100

    Add logs and lockfile removal

--- scripts/remove/sonarrv3.sh
+++ scripts/remove/sonarrv3.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-apt-get remove -yq sonarr --purge
+log="/root/logs/swizzin.log"
+
+apt-get remove -yq sonarr --purge >> $log 2>&1
 rm -rf /var/lib/sonarr
 if [[ -f /install/.nginx.lock ]]; then 
     rm /etc/nginx/apps/sonarrv3.conf
-    systemctl reload nginx
+    systemctl reload nginx >> $log 2>&1
 fi
+
+rm /install/.sonarrv3.lock
