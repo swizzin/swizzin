27a5226a738d87cb598712b6754c0d76f1be34d4
Flying-sausages <flying_sausages@protonmail.com>
2020-07-03 00:47:31 +0100

    change OUTTO to log

--- setup.sh
+++ setup.sh
@@ -90,17 +90,17 @@ function _nukeovh() {
       esac
       if [[ $kernel == yes ]]; then
         if [[ $DISTRO == Ubuntu ]]; then
-          apt-get install -q -y linux-image-generic >>"${OUTTO}" 2>&1
+          apt-get install -q -y linux-image-generic >>"${log}" 2>&1
         elif [[ $DISTRO == Debian ]]; then
           arch=$(uname -m)
           if [[ $arch =~ ("i686"|"i386") ]]; then
-            apt-get install -q -y linux-image-686 >>"${OUTTO}" 2>&1
+            apt-get install -q -y linux-image-686 >>"${log}" 2>&1
           elif [[ $arch == x86_64 ]]; then
-            apt-get install -q -y linux-image-amd64 >>"${OUTTO}" 2>&1
+            apt-get install -q -y linux-image-amd64 >>"${log}" 2>&1
           fi
         fi
         mv /etc/grub.d/06_OVHkernel /etc/grub.d/25_OVHkernel
-        update-grub >>"${OUTTO}" 2>&1
+        update-grub >>"${log}" 2>&1
       fi
   fi
 }
