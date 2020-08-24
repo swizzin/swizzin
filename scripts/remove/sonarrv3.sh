a680df19b4a5c86d166cee3ef1974c0e88c20b2c
Flying-sausages <flying_sausages@protonmail.com>
2020-08-24 15:46:09 +0200

    Remove sonarr user

--- scripts/remove/sonarrv3.sh
+++ scripts/remove/sonarrv3.sh
@@ -2,8 +2,11 @@
 log="/root/logs/swizzin.log"
 
 apt-get remove -yq sonarr --purge >> $log 2>&1
+
+deluser sonarr
 rm -rf /var/lib/sonarr
 rm -rf /usr/lib/sonarr
+
 if [[ -f /install/.nginx.lock ]]; then 
     rm /etc/nginx/apps/sonarrv3.conf
     systemctl reload nginx >> $log 2>&1
