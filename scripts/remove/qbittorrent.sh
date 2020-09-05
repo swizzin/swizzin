03b29f1040175a35db6a91683dacf213093723bf
liaralabs <liara@swizzin.ltd>
2020-09-05 15:06:22 -0700

    Refine removal scripts

--- scripts/remove/qbittorrent.sh
+++ scripts/remove/qbittorrent.sh
@@ -9,7 +9,11 @@ done
 rm /etc/nginx/apps/qbittorrent.conf
 rm /etc/nginx/conf.d/*.qbittorrent.conf
 rm /etc/systemd/system/qbittorrent@.service
-dpkg -i qbittorrent-nox
+dpkg -r qbittorrent-nox > /dev/null 2>&1
 
 systemctl reload nginx
 rm /install/.qbittorrent.lock
+
+if [[ ! -f /install/.deluge.lock ]]; then
+    bash /etc/swizzin/scripts/remove/libtorrent.sh
+fi
