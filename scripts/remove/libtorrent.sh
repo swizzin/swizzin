f9a8b01f80c17d3637705e241f6d1a76989bc267
Flying Sausages <flying_sausages@protonmail.com>
2020-09-12 01:16:52 +0200

    implement apt functions in more files from master

--- scripts/remove/libtorrent.sh
+++ scripts/remove/libtorrent.sh
@@ -6,9 +6,11 @@ if [[ -f /install/.deluge.lock ]] || [[ -f /install/.qbittorrent.lock ]]; then
     exit 1
 fi
 
-apt-get purge -y libtorrent-rasterbar* > /dev/null 2>&1
+apt_remove --purge libtorrent-rasterbar*
 dpkg -r libtorrent > /dev/null 2>&1
 dpkg -r libtorrent-rasterbar > /dev/null 2>&1
 dpkg -r python-libtorrent > /dev/null 2>&1
 dpkg -r python3-libtorrent > /dev/null 2>&1
 dpkg -r deluge-common > /dev/null 2>&1
+
+rm /install/.libtorrent.lock
