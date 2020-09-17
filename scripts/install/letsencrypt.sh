f81db91d5eb3461c2894b3205124fb938202e0a1
Flying Sausages <flying_sausages@protonmail.com>
2020-09-17 22:01:21 +0200

    Clean up and beautify letsencrypt

--- scripts/install/letsencrypt.sh
+++ scripts/install/letsencrypt.sh
@@ -11,17 +11,18 @@
 #
 
 if [[ ! -f /install/.nginx.lock ]]; then
-    echo "This script is meant to be used in conjunction with nginx and it has not been installed. Please install nginx first and restart this installer."
+    echo_error "This script is meant to be used in conjunction with nginx and it has not been installed. Please install nginx first and restart this installer."
     exit 1
 fi    
 
 . /etc/swizzin/sources/functions/letsencrypt
 ip=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
 
-echo -e "Enter domain name to secure with LE"
+echo_query "Enter domain name to secure with LE"
 read -e hostname
 
-read -p "Do you want to apply this certificate to your swizzin default conf? (y/n) " yn
+echo_query "Do you want to apply this certificate to your swizzin default conf? (y/n)"
+read yn
 case $yn in
   [Yy] )
       main=yes
@@ -29,14 +30,15 @@ case $yn in
   [Nn] )
       main=no
       ;;
-  * ) echo "Please answer (y)es or (n)o.";;
+  * ) echo_warn "Please answer (y)es or (n)o.";;
 esac
 
 if [[ $main == yes ]]; then
   sed -i "s/server_name .*;/server_name $hostname;/g" /etc/nginx/sites-enabled/default
 fi
 
-read -p "Is your DNS managed by CloudFlare? (y/n) " yn
+echo_query "Is your DNS managed by CloudFlare? (y/n) " 
+read yn
 case $yn in
   [Yy] )
       cf=yes
@@ -44,18 +46,18 @@ case $yn in
   [Nn] )
       cf=no
       ;;
-  * ) echo "Please answer (y)es or (n)o.";;
+  * ) echo_warn "Please answer (y)es or (n)o.";;
 esac
-
 
 if [[ ${cf} == yes ]]; then
 
   if [[ $hostname =~ (\.cf$|\.ga$|\.gq$|\.ml$|\.tk$) ]]; then
-    echo "ERROR Cloudflare does not support API calls for the following TLDs: cf, .ga, .gq, .ml, or .tk"
+    echo_error "Cloudflare does not support API calls for the following TLDs: cf, .ga, .gq, .ml, or .tk"
     exit 1
   fi
 
-  read -p "Does the record for this subdomain already exist? (y/n) " yn
+  echo_query "Does the record for this subdomain already exist? (y/n) "
+  read yn
   case $yn in
       [Yy] )
       record=yes
@@ -64,15 +66,15 @@ if [[ ${cf} == yes ]]; then
       record=no
       ;;
       * )
-      echo "Please answer (y)es or (n)o."
+      echo_warn "Please answer (y)es or (n)o."
       ;;
   esac
   
 
-  echo -e "Enter CF API key"
+  echo_query "Enter CF API key"
   read -e api
 
-  echo -e "CF Email"
+  echo_query "CF Email"
   read -e email
 
   export CF_Key="${api}"
@@ -86,7 +88,7 @@ if [[ ${cf} == yes ]]; then
   fi
 
   if [[ ${record} == no ]]; then
-    echo -e "Zone Name (example.com)"
+    echo_query "Zone Name (example.com)"
     read -e zone
     zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone" -H "X-Auth-Email: $email" -H "X-Auth-Key: $api" -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
     addrecord=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records" -H "X-Auth-Email: $email" -H "X-Auth-Key: $api" -H "Content-Type: application/json" --data "{\"id\":\"$zoneid\",\"type\":\"A\",\"name\":\"$hostname\",\"content\":\"$ip\",\"proxied\":true}")
@@ -96,7 +98,7 @@ if [[ ${cf} == yes ]]; then
       exit 1
     else
       message="DNS record added for $hostname at $ip"
-      echo "$message"
+      echo_info "$message"
     fi
   fi
 fi
@@ -104,30 +106,36 @@ fi
 apt_install socat
 
 if [[ ! -f /root/.acme.sh/acme.sh ]]; then
-  curl https://get.acme.sh | sh
+  echo_progress_start "Installing ACME script"
+  curl https://get.acme.sh | sh >> $log 2>&1
+  echo_progress_done
 fi
 
 mkdir -p /etc/nginx/ssl/${hostname}
 chmod 700 /etc/nginx/ssl
 
+echo_progress_start "Registering certificates"
 if [[ ${cf} == yes ]]; then
-  /root/.acme.sh/acme.sh --force --issue --dns dns_cf -d ${hostname} || { echo "ERROR: Certificate could not be issued. Please check your info and try again"; exit 1; }
+  /root/.acme.sh/acme.sh --force --issue --dns dns_cf -d ${hostname}  >> $log 2>&1 || { echo_error "Certificate could not be issued."; exit 1; }
 else
   if [[ $main = yes ]]; then
-    /root/.acme.sh/acme.sh --force --issue --nginx -d ${hostname} || { echo "ERROR: Certificate could not be issued. Please check your info and try again"; exit 1; }
+    /root/.acme.sh/acme.sh --force --issue --nginx -d ${hostname} >> $log 2>&1 || { echo_error "Certificate could not be issued."; exit 1; }
   else
     systemctl stop nginx
-    /root/.acme.sh/acme.sh --force --issue --standalone -d ${hostname} --pre-hook "systemctl stop nginx" --post-hook "systemctl start nginx" || { echo "ERROR: Certificate could not be issued. Please check your info and try again"; exit 1; }
+    /root/.acme.sh/acme.sh --force --issue --standalone -d ${hostname} --pre-hook "systemctl stop nginx" --post-hook "systemctl start nginx" >> $log 2>&1 || { echo_error "Certificate could not be issued. Please check your info and try again"; exit 1; }
     sleep 1
     systemctl start nginx
   fi
 fi
+echo_progress_done "Certificate acquired"
 
+echo_progress_start "Installing certificate"
 /root/.acme.sh/acme.sh --force --install-cert -d ${hostname} --key-file /etc/nginx/ssl/${hostname}/key.pem --fullchain-file /etc/nginx/ssl/${hostname}/fullchain.pem --ca-file /etc/nginx/ssl/${hostname}/chain.pem --reloadcmd "systemctl reload nginx"
 if [[ $main == yes ]]; then
   sed -i "s/ssl_certificate .*/ssl_certificate \/etc\/nginx\/ssl\/${hostname}\/fullchain.pem;/g" /etc/nginx/sites-enabled/default
   sed -i "s/ssl_certificate_key .*/ssl_certificate_key \/etc\/nginx\/ssl\/${hostname}\/key.pem;/g" /etc/nginx/sites-enabled/default
 fi
+echo_progress_done "Certificate installed"
 
 # Add LE certs to ZNC, if installed.
 if [[ -f /install/.znc.lock ]]; then
@@ -141,3 +149,5 @@ if [[ -f /install/.vsftpd.lock ]]; then
 fi
 
 systemctl reload nginx
+
+echo_success "Letsencrypt installed"
