c497eb23f34eef1e9b310b6d138dd625e1012643
Flying Sausages <flying_sausages@protonmail.com>
2020-09-17 22:55:35 +0200

    netdata echo

--- scripts/install/netdata.sh
+++ scripts/install/netdata.sh
@@ -2,17 +2,16 @@
 # Netdata installer for swizzin
 # Author: liara
 
-if [[ -f /tmp/.install.lock ]]; then
-  log="/root/logs/install.log"
-else
-  log="/root/logs/swizzin.log"
-fi
-
+echo_progress_start "Running netdata install script"
 bash <(curl -Ss https://my-netdata.io/kickstart.sh) --non-interactive >> $log 2>&1
+echo_progress_done
 
 if [[ -f /install/.nginx.lock ]]; then
+  echo_progress_start "Configuring nginx"
   bash /usr/local/bin/swizzin/nginx/netdata.sh
   systemctl reload nginx
+  echo_progress_done
 fi
 
+echo_success "Netdata installed"
 touch /install/.netdata.lock
