7f6e571e7f9fb275cc1d9afd73e248dff46316bb
Flying Sausages <flying_sausages@protonmail.com>
2020-09-18 22:36:10 +0200

    syncthing

--- scripts/install/syncthing.sh
+++ scripts/install/syncthing.sh
@@ -17,22 +17,17 @@
 #   including (via compiler) GPL-licensed code must also be made available
 #   under the GPL along with build & install instructions.
 
-if [[ -f /tmp/.install.lock ]]; then
-  OUTTO="/root/logs/install.log"
-else
-  OUTTO="/root/logs/swizzin.log"
-fi
 MASTER=$(cut -d: -f1 < /root/.master.info)
 
-echo "Adding Syncthing Repository ... " >>"${OUTTO}" 2>&1;
-curl -s https://syncthing.net/release-key.txt | sudo apt-key add - > /dev/null 2>&1
+echo_progress_start "Adding Syncthing Repository"
+curl -s https://syncthing.net/release-key.txt | apt-key add - >> $log 2>&1
 echo "deb http://apt.syncthing.net/ syncthing release" > /etc/apt/sources.list.d/syncthing.list
+echo_progress_done "Repo added"
 apt_update
 
-echo "Installing Syncthing ... " >>"${OUTTO}" 2>&1;
 apt_install syncthing
 
-echo "Configuring Syncthing & Starting ... " >>"${OUTTO}" 2>&1;
+echo_progress_start "Configuring Syncthing & Starting"
 cat > /etc/systemd/system/syncthing@.service <<SYNC
 [Unit]
 Description=Syncthing - Open Source Continuous File Synchronization for %i
@@ -50,16 +45,16 @@ RestartForceExitStatus=3 4
 [Install]
 WantedBy=multi-user.target
 SYNC
-systemctl enable syncthing@${MASTER} > /dev/null 2>&1
-systemctl start syncthing@${MASTER} > /dev/null 2>&1
+systemctl enable syncthing@${MASTER} >> $log 2>&1
+systemctl start syncthing@${MASTER} >> $log 2>&1
+echo_progress_done
 
 if [[ -f /install/.nginx.lock ]]; then
+  echo_progress_start "Configuring nginx"
   bash /usr/local/bin/swizzin/nginx/syncthing.sh
   systemctl reload nginx
+  echo_progress_done
 fi
 
 touch /install/.syncthing.lock
-echo "Syncthing installation complete!" >>"${OUTTO}" 2>&1
-echo >>"${OUTTO}" 2>&1
-echo >>"${OUTTO}" 2>&1
-echo "Close this dialog box to refresh your browser" >>"${OUTTO}" 2>&1
+echo_success "Syncthing installed"
