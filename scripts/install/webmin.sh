ec9b1f088b15e91a17c81c9d41ca0851408939b2
Flying Sausages <flying_sausages@protonmail.com>
2020-09-18 23:07:13 +0200

    bebbmin

--- scripts/install/webmin.sh
+++ scripts/install/webmin.sh
@@ -10,23 +10,24 @@ else
 fi
 
 _install_webmin () {
-    echo "Installing Webmin repo"
+    echo_progress_start "Installing Webmin repo"
     echo "deb https://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list
     wget http://www.webmin.com/jcameron-key.asc >> $log 2>&1
-    sudo apt-key add jcameron-key.asc >> $log 2>&1
+    apt-key add jcameron-key.asc >> $log 2>&1
     rm jcameron-key.asc
-    echo "Fetching updates"
+    echo_progress_done "Repo added"
     apt_update
-    echo "Installing Webmin from apt"
     apt_install webmin
 }
 
 _install_webmin
 if [[ -f /install/.nginx.lock ]]; then
+  echo_progress_start "Configuring nginx"
   bash /etc/swizzin/scripts/nginx/webmin.sh
+  echo_progress_done
 fi
 
-echo 
-echo "Webmin has been installed, please use any account with sudo permissions to log in"
+echo_success "Webmin installed"
+echo_info "Please use any account with sudo permissions to log in"
 
 touch /install/.webmin.lock
