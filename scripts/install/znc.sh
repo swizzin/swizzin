600e4b0a8da20548d9dbcc32b9ee791995e74274
Flying Sausages <flying_sausages@protonmail.com>
2020-09-19 00:02:15 +0200

    znc and backports

--- scripts/install/znc.sh
+++ scripts/install/znc.sh
@@ -14,30 +14,28 @@
 
 DISTRO=$(lsb_release -is)
 CODENAME=$(lsb_release -cs)
-if [[ -f /tmp/.install.lock ]]; then
-  OUTTO="/root/logs/install.log"
-else
-  OUTTO="/root/logs/swizzin.log"
-fi
-
+#shellcheck source=sources/functions/letsencrypt
 . /etc/swizzin/sources/functions/letsencrypt
 
-echo "Installing ZNC. Please wait ... " >> ${OUTTO} 2>&1
-echo "" >> ${OUTTO} 2>&1
-echo "" >> ${OUTTO} 2>&1
+echo_progress_start "Creating system user for znc"
 useradd znc -m -s /bin/bash
-passwd znc -l >> ${OUTTO} 2>&1
+passwd znc -l >> ${log} 2>&1
+echo_progress_done
 
 if [[ $DISTRO == Debian ]]; then
+  #shellcheck source=sources/functions/backports
   . /etc/swizzin/sources/functions/backports
   check_debian_backports
   set_packages_to_backports znc
 elif [[ $DISTRO == Ubuntu ]]; then
-  add-apt-repository --yes ppa:teward/znc >> ${OUTTO} 2>&1
+  echo_progress_start "Adding ZNC PPA"
+  add-apt-repository --yes ppa:teward/znc >> ${log} 2>&1
+  echo_progress_done
 fi
   apt_update
   apt_install znc
   #sudo -u znc crontab -l | echo -e "*/10 * * * * /usr/bin/znc >/dev/null 2>&1\n@reboot /usr/bin/znc >/dev/null 2>&1" | crontab -u znc - > /dev/null 2>&1
+  echo_progress_start "Installing systemd service"
   cat > /etc/systemd/system/znc.service <<ZNC
 [Unit]
 Description=ZNC, an advanced IRC bouncer
@@ -52,7 +50,9 @@ Restart=always
 WantedBy=multi-user.target
 ZNC
 systemctl enable znc
-echo "#### ZNC configuration will now run. Please answer the following prompts ####"
+echo_progress_done
+
+echo_warn "ZNC configuration will now run. Please answer the following prompts"
 sleep 5
 sudo -H -u znc znc --makeconf
 killall -u znc znc > /dev/null 2>&1
@@ -64,4 +64,4 @@ le_znc_hook
 systemctl start znc
 echo "$(grep Port /home/znc/.znc/configs/znc.conf | sed -e 's/^[ \t]*//')" > /install/.znc.lock
 echo "$(grep SSL /home/znc/.znc/configs/znc.conf | sed -e 's/^[ \t]*//')" >> /install/.znc.lock
-echo "#### ZNC now installed! ####"
+echo_success "ZNC installed"
