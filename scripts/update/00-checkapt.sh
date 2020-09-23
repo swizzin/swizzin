efcb34073fe9fd37760cc0063704140ef421822d
liaralabs <liara@swizzin.ltd>
2020-09-23 12:14:26 -0700

    Add blank echo for readability

--- scripts/update/00-checkapt.sh
+++ scripts/update/00-checkapt.sh
@@ -3,6 +3,7 @@
 # Function should be loaded on the next run (by `box`) without further need for interaction.
 
 if ! command -v apt_install > /dev/null 2>&1 ; then
+    echo
     echo "Due to internal restructuing please restart \`box update\`. You should only have to do this once."
     kill -13 `ps --pid $$ -oppid=`
     exit 1
