c9d39540e97e49f401b4d0b04f85f18937c804ff
liaralabs <liara@swizzin.ltd>
2020-09-23 12:11:27 -0700

    Add apt function check to prevent confusion on updates

---
+++ scripts/update/00-checkapt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Checks if required functions are loaded before continuing with the update scripts.
+# Function should be loaded on the next run (by `box`) without further need for interaction.
+
+if ! command -v apt_install > /dev/null 2>&1 ; then
+    echo "Due to internal restructuing please restart \`box update\`. You should only have to do this once."
+    kill -13 `ps --pid $$ -oppid=`
+    exit 1
+fi
