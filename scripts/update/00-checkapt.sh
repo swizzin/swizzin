7f50fe27b88a0b117435b4a3a542fdd5b3f2e484
liaralabs <liara@swizzin.ltd>
2020-09-24 09:00:41 -0700

    fix typo in echo

--- scripts/update/00-checkapt.sh
+++ scripts/update/00-checkapt.sh
@@ -4,7 +4,7 @@
 
 if ! command -v apt_install > /dev/null 2>&1 ; then
     echo
-    echo "Due to internal restructuing please restart \`box update\`. You should only have to do this once."
+    echo "Due to internal restructuring please restart \`box update\`. You should only have to do this once."
     kill -13 `ps --pid $$ -oppid=`
     exit 1
 fi
