6cc065f8c33450739f7fc0c024255e017a82cea0
Flying Sausages <flying_sausages@protonmail.com>
2020-10-07 15:38:31 +0200

    stfu linter

--- scripts/remove/scrutiny.sh
+++ scripts/remove/scrutiny.sh
@@ -2,8 +2,6 @@
 # Scrutiny installer by flying_sausages for Swizzin 2020
 # GPLv3 applies
 
-scrutinydir="/opt/scrutiny"
-
 systemctl disable -q --now scrutiny-web
 systemctl disable -q --now scrutiny-collector.timer
 systemctl disable -q --now scrutiny-collector.service
