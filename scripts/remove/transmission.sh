452cdbbf2461aadf9a748d8d0a0987d571a80127
flying-sausages <flying_sausages@protonmail.com>
2020-10-08 22:57:11 +0200

    HOTFIX: Transmission watchdirs and function fixes (#362)
    * Change setenv behaviour, add watchdir, fixed nginx bash script exec
    
    * Fix settings not being removed due to script running, fixed log references
    
    * Ensure values are re-set for port checking function
    
    * Add while loop for script to shut down, add `jq`-based json manipulation. !!! REQUIRES #354 !!!!
    
    * Added timeout to prevent infinite loops
    
    * HOTFIX vsftpd remover post-apt

--- scripts/remove/transmission.sh
+++ scripts/remove/transmission.sh
@@ -9,17 +9,19 @@ else
 fi
 
 users=($(cut -d: -f1 < /etc/htpasswd))
-for u in ${users}; do
-    systemctl disable --now transmission@$u > /dev/null 2>&1
-    #TODO decide if other stuff should be deleted too
+for u in ${users[@]}; do
+    echo "Shutting down transmission@$u"
+    systemctl stop transmission@"$u" > $log 2>&1
+    systemctl disable transmission@"$u" > $log 2>&1
     rm -f /home/${u}/.config/transmission-daemon/settings.json
 done
+
 add-apt-repository --remove ppa:transmissionbt/ppa -y >> $log 2>&1
 apt_remove --purge transmission-common transmission-cli transmission-daemon
 rm /etc/systemd/system/transmission@.service
-rm /etc/nginx/apps/transmission.conf > /dev/null 2>&1
-rm /etc/nginx/conf.d/*.transmission.conf > /dev/null 2>&1
-systemctl reload nginx > /dev/null 2>&1
+rm /etc/nginx/apps/transmission.conf > $log 2>&1
+rm /etc/nginx/conf.d/*.transmission.conf > $log 2>&1
+systemctl reload nginx > $log 2>&1
 systemctl daemon-reload
 
 rm /install/.transmission.lock
