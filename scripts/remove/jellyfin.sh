047720456b404ad93e8c0173ea5239e136a007a4
userdocs <16525024+userdocs@users.noreply.github.com>
2020-10-21 23:20:11 +0100

    jellyfin remove: requested changes

--- scripts/remove/jellyfin.sh
+++ scripts/remove/jellyfin.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 #
-systemctl -q stop "jellyfin.service"
+systemctl -q stop jellyfin.service
 #
 apt_remove --purge jellyfin jellyfin-ffmpeg
 #
-[[ -d "/var/lib/jellyfin" ]] && rm -rf "/var/lib/jellyfin" || :
-[[ -d "/var/log/jellyfin" ]] && rm -rf "/var/log/jellyfin" || :
-[[ -d "/var/cache/jellyfin" ]] && rm -rf "/var/cache/jellyfin" || :
-[[ -d "/usr/share/jellyfin/web" ]] && rm -rf "/usr/share/jellyfin/web" || :
+rm_if_exists /var/lib/jellyfin
+rm_if_exists /var/log/jellyfin
+rm_if_exists /var/cache/jellyfin
+rm_if_exists /usr/share/jellyfin/web
 #
 # Remove the nginx conf and reload nginx.
-if [[ -f "/install/.nginx.lock" ]]; then 
-    rm -f "/etc/nginx/apps/jellyfin.conf"
-    systemctl -q reload "nginx.service"
+if [[ -f /install/.nginx.lock ]]; then 
+    rm -f /etc/nginx/apps/jellyfin.conf
+    systemctl -q reload nginx.service
 fi
 #
-[[ -d "/install/.jellyfin.lock" ]] && rm -f "/install/.jellyfin.lock" || :
+rm_if_exists /install/.jellyfin.lock
 #
 exit
