5d5564defbadeb3c6dab17106b88186c3f76ff40
liaralabs <liara@swizzin.ltd>
2020-10-25 10:58:12 -0700

    Fix usermod command

--- scripts/install/deluge.sh
+++ scripts/install/deluge.sh
@@ -200,7 +200,7 @@ DHL
   mkdir -p /home/${u}/torrents/deluge
   chown ${u}: /home/${u}/torrents
   chown ${u}: /home/${u}/torrents/deluge
-  usermod -a -G ${user} www-data 2>> $log
+  usermod -a -G ${u} www-data 2>> $log
 done
 }
 function _dservice {
