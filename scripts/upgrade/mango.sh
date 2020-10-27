a5e777cd82565ce8df5c8c378fc5c67e42bf7347
Flying Sausages <flying_sausages@protonmail.com>
2020-10-27 19:38:02 +0100

    echo down mango

--- scripts/upgrade/mango.sh
+++ scripts/upgrade/mango.sh
@@ -3,7 +3,7 @@
 # flying_sausages for swizzin 2020
 
 if [[ ! -f /install/.mango.lock ]]; then
-    echo "Mango not installed "
+    echo_error "Mango not installed "
     exit 1
 fi
 
@@ -11,26 +11,30 @@ mangodir="/opt/mango"
 
 if [[ $(systemctl is-active mango) == "active" ]]; then
     wasActive="true"
-    echo "Shutting down Mango ($($mangodir/mango --version))"
+    echo_progress_start "Shutting down Mango ($($mangodir/mango --version))"
     systemctl stop mango
+    echo_progress_done
 fi
 
 rm -rf /tmp/mangobak
 mkdir /tmp/mangobak
 cp -rt /tmp/mangobak $mangodir/mango $mangodir/.config/
 
-echo "Downloading binary" | tee -a $log
+echo_progress_start "Downloading binary" | tee -a $log
 dlurl=$(curl -s https://api.github.com/repos/hkalexling/Mango/releases/latest | grep "browser_download_url" | head -1 | cut -d\" -f 4)
 # shellcheck disable=SC2181
 if [[ $? != 0 ]]; then
-    echo "Failed to query github"
+    echo_error "Failed to query github"
     exit 1
 fi
-
 wget "${dlurl}" -O $mangodir/mango >> $log 2>&1
 chmod +x "$mangodir"/mango
+echo_progress_done
 
 if [[ $wasActive = "true" ]]; then
-    echo "Restarting Mango ($($mangodir/mango --version))"
+    echo_progress_start "Restarting Mango ($($mangodir/mango --version))"
     systemctl start mango
+    echo_progress_done
 fi
+
+echo_success "Mango upgraded"
