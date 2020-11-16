463b23e81941df106cd721b5faffda13fca82148
Flying Sausages <flying_sausages@protonmail.com>
2020-11-16 19:30:57 +0100

    Add version question before triggering upgrade

--- scripts/upgrade/airsonic.sh
+++ scripts/upgrade/airsonic.sh
@@ -3,6 +3,12 @@ if [[ ! -f /install/.airsonic.lock ]]; then
 	echo_error "Airsonic not installed"
 	exit 1
 fi
+current_v="$(unzip -p /opt/airsonic/airsonic.war META-INF/MANIFEST.MF | grep -i Implementation-Version | cut -d' ' -f 2)"
+release_v="$(curl -s https://api.github.com/repos/airsonic/airsonic/releases/latest | jq -r .'tag_name')"
+echo_info "Installed = $current_v\nAvailable = $release_v"
+if ! ask "Upgrade Airsonic package?"; then
+	exit 0
+fi
 
 echo_progress_start "Downloading airsonic binary"
 dlurl=$(curl -s https://api.github.com/repos/airsonic/airsonic/releases/latest | grep "browser_download_url" | grep "airsonic.war" | head -1 | cut -d\" -f 4)
