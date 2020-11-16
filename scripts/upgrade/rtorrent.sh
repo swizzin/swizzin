e20d262936fbbf0c43ad73d0685715a299c5918b
Flying Sausages <flying_sausages@protonmail.com>
2020-11-16 15:15:17 +0100

    Revert "for ([a-z]*) in "\$\{users\[@\]\}"; --> for $1 in $(_get_user_list);"
    This reverts commit 5e295c04cdaef96a6d00c10fc748fcc1e650bfae.

--- scripts/upgrade/rtorrent.sh
+++ scripts/upgrade/rtorrent.sh
@@ -3,8 +3,8 @@
 # Author: liara
 
 if [[ ! -f /install/.rtorrent.lock ]]; then
-	echo_error "rTorrent doesn't appear to be installed. What do you hope to accomplish by running this script?"
-	exit 1
+  echo_error "rTorrent doesn't appear to be installed. What do you hope to accomplish by running this script?"
+  exit 1
 fi
 
 export DEBIAN_FRONTEND=noninteractive
@@ -26,34 +26,24 @@ echo_progress_start "Removing old rTorrent binaries and libraries ... "
 if [[ -z $isdeb ]]; then
 	remove_rtorrent_legacy
 else
-	remove_rtorrent
+  remove_rtorrent
 fi
 echo_progress_done
 
-echo_progress_start "Checking rTorrent Dependencies ... "
-depends_rtorrent
-echo_progress_done
+echo_progress_start "Checking rTorrent Dependencies ... ";depends_rtorrent ; echo_progress_done
 if [[ ! $rtorrentver == repo ]]; then
-	echo_progress_start "Building xmlrpc-c from source ... "
-	build_xmlrpc-c
-	echo_progress_done
-	echo_progress_start "Building libtorrent from source ... "
-	build_libtorrent_rakshasa
-	echo_progress_done
-	echo_progress_start "Building rtorrent from source ... "
-	build_rtorrent
-	echo_progress_done
+  echo_progress_start "Building xmlrpc-c from source ... ";build_xmlrpc-c; echo_progress_done
+  echo_progress_start "Building libtorrent from source ... ";build_libtorrent_rakshasa; echo_progress_done
+  echo_progress_start "Building rtorrent from source ... ";build_rtorrent; echo_progress_done
 else
-	echo_progress_start "Installing rtorrent with apt-get ... "
-	rtorrent_apt
-	echo_progress_done
+  echo_progress_start "Installing rtorrent with apt-get ... ";rtorrent_apt; echo_progress_done
 fi
 
 if [[ -n $noexec ]]; then
 	mount -o remount,noexec /tmp
 fi
 
-for u in $(_get_user_list); do
+for u in "${users[@]}"; do
 	if grep -q localhost /home/$u/.rtorrent.rc; then sed -i 's/localhost/127.0.0.1/g' /home/$u/.rtorrent.rc; fi
-	systemctl try-restart rtorrent@${u}
+  systemctl try-restart rtorrent@${u}
 done
