6f83f4e8f99123b29f670e773b206c0436b39f91
userdocs <16525024+userdocs@users.noreply.github.com>
2020-11-16 18:09:32 +0000

    Update jellyfin.sh
    fix cache and metadata paths if set in the system.xml

--- scripts/update/jellyfin.sh
+++ scripts/update/jellyfin.sh
@@ -41,6 +41,8 @@ if [[ -f /install/.jellyfin.lock ]]; then
         sed -r 's#<HttpServerPortNumber>(.*)</HttpServerPortNumber>#<HttpServerPortNumber>8096</HttpServerPortNumber>#g' -i /etc/jellyfin/system.xml
         sed -r 's#<HttpsPortNumber>(.*)</HttpsPortNumber>#<HttpsPortNumber>8920</HttpsPortNumber>#g' -i /etc/jellyfin/system.xml
         sed -r 's#<RequireHttps>false</RequireHttps>#<RequireHttps>true</RequireHttps>#g' -i /etc/jellyfin/system.xml
+        sed -r 's#<CachePath>(.*)</CachePath>#<CachePath>/var/cache/jellyfin</CachePath>#g' -i /etc/jellyfin/system.xml
+        sed -r 's#<MetadataPath>(.*)</MetadataPath>#<MetadataPath>/var/lib/jellyfin/metadata</MetadataPath>#g' -i /etc/jellyfin/system.xml
         echo_warn "The Jellyfin ports have been moved to 8920 (SSL). Please update any applications that rely on the old random port"
         #
         if [[ -f /etc/nginx/apps/jellyfin.conf ]]; then
