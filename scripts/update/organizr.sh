bd2582b334512a6031924a95cc42ceaf05fec072
Flying Sausages <flying_sausages@protonmail.com>
2020-11-25 19:06:26 +0100

    Force organizr update to prevent the upstream bug with non-baseurl

--- scripts/update/organizr.sh
+++ scripts/update/organizr.sh
@@ -4,8 +4,14 @@ if [[ -f /install/organizr.sh ]]; then
 	# If there is no mention of the v2 api, re-run nginx config
 	if ! grep -q api/v2 /etc/nginx/apps/organizr.conf; then
 		#shellcheck source=scripts/nginx/organizr.sh
-		echo_progress_start "Updating organizr nginx config to v2.1"
+		echo_progress_start "Updating organizr to v2.1"
 		bash /etc/swizzin/scripts/nginx/organizr.sh
 		echo_progress_done "Organizr nginx config updated"
+
+		# TODO update /srv/organizr past the commit where baseurls are broken
+		echo_progress_start "Pulling down new organizr source code"
+		git -C "/srv/organizr" pull 
+		echo_progress_done "Retrieved new organizr source code"
 	fi
+
 fi
