b7afb1553fb10e16da46af53146a778e12424d91
Flying Sausages <flying_sausages@protonmail.com>
2020-11-29 16:43:32 +0100

    Refactor v3 variables

--- scripts/install/radarr.sh
+++ scripts/install/radarr.sh
@@ -5,31 +5,31 @@
 #shellcheck source=sources/functions/utils
 . /etc/swizzin/sources/functions/utils
 
-if [[ -z $radarrv3owner ]]; then
-	radarrv3owner=$(_get_master_username)
+if [[ -z $radarrOwner ]]; then
+	radarrOwner=$(_get_master_username)
 fi
 
 [[ -z $radarrv02owner ]] && radarrv02owner=$(_get_master_username)
 
-_install_radarrv3() {
+_install_radarr() {
 	apt_install curl mediainfo sqlite3
 
-	radarrv3confdir="/home/$radarrv3owner/.config/Radarr"
-	mkdir -p "$radarrv3confdir"
-	chown -R "$radarrv3owner":"$radarrv3owner" "$radarrv3confdir"
+	radarrConfDir="/home/$radarrOwner/.config/Radarr"
+	mkdir -p "$radarrConfDir"
+	chown -R "$radarrOwner":"$radarrOwner" "$radarrConfDir"
 
 	echo_progress_start "Downloading source files"
-	if ! wget "https://radarr.servarr.com/v1/update/nightly/updatefile?os=linux&runtime=netcore&arch=x64" -O /tmp/Radarrv3.tar.gz >> "$log" 2>&1; then
+	if ! wget "https://radarr.servarr.com/v1/update/nightly/updatefile?os=linux&runtime=netcore&arch=x64" -O /tmp/Radarr.tar.gz >> "$log" 2>&1; then
 		echo_error "Download failed, exiting"
 		exit 1
 	fi
 	echo_progress_done "Source downloaded"
 
 	echo_progress_start "Extracting archive"
-	tar -xvf /tmp/Radarrv3.tar.gz -C /opt >> "$log" 2>&1
+	tar -xvf /tmp/Radarr.tar.gz -C /opt >> "$log" 2>&1
 	echo_progress_done "Archive extracted"
 
-	touch /install/.radarrv3.lock
+	touch /install/.radarr.lock
 
 	echo_progress_start "Installing Systemd service"
 	cat > /etc/systemd/system/radarr.service << EOF
@@ -39,8 +39,8 @@ After=syslog.target network.target
 
 [Service]
 # Change the user and group variables here.
-User=${radarrv3owner}
-Group=${radarrv3owner}
+User=${radarrOwner}
+Group=${radarrOwner}
 
 Type=simple
 
@@ -60,17 +60,17 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.target
 EOF
-	chown -R "$radarrv3owner":"$radarrv3owner" /opt/Radarr
+	chown -R "$radarrOwner":"$radarrOwner" /opt/Radarr
 	systemctl -q daemon-reload
 	systemctl enable --now -q radarr
 	sleep 1
 	echo_progress_done "Radarr service installed and enabled"
 
-	if [[ -f $radarrv3confdir/update_required ]]; then
+	if [[ -f $radarrConfDir/update_required ]]; then
 		echo_progress_start "Radarr is installing an internal upgrade..."
 		# echo "You can track the update by running \`systemctl status Radarr\`0. in another shell."
 		# echo "In case of errors, please press CTRL+C and run \`box remove sonarrv3\` in this shell and check in with us in the Discord"
-		while [[ -f $radarrv3confdir/update_required ]]; do
+		while [[ -f $radarrConfDir/update_required ]]; do
 			sleep 1
 			echo_log_only "Upgrade file is still here"
 		done
@@ -81,24 +81,24 @@ EOF
 	#Ensures that local clients running over HTTPS don't need valid certs
 	sleep 5
 	systemctl -q stop radarr
-	sqlite3 "$radarrv3confdir"/radarr.db "INSERT or REPLACE INTO Config VALUES('6', 'certificatevalidation', 'DisabledForLocalAddresses');"
+	sqlite3 "$radarrConfDir"/radarr.db "INSERT or REPLACE INTO Config VALUES('6', 'certificatevalidation', 'DisabledForLocalAddresses');"
 	systemctl -q start radarr
 	echo_progress_done "Policy configured"
 }
 
-_nginx_radarrv3() {
+_nginx_radarr() {
 	if [[ -f /install/.nginx.lock ]]; then
 		echo_progress_start "Installing nginx configuration"
 		#TODO what is this sleep here for? See if this can be fixed by doing a check for whatever it needs to
 		sleep 10
-		bash /usr/local/bin/swizzin/nginx/radarrv3.sh
+		bash /usr/local/bin/swizzin/nginx/radarr.sh
 		systemctl -q reload nginx
 		echo_progress_done "Nginx configured"
 	fi
 }
 
-_install_radarrv3
-_nginx_radarrv3
+_install_radarr
+_nginx_radarr
 
 if [[ -f /install/.ombi.lock ]]; then
 	echo_info "Please adjust your Ombi setup accordingly"
--- scripts/update/radarr.sh
+++ scripts/update/radarr.sh
@@ -25,14 +25,14 @@ if [[ -f /install/.radarr.lock ]]; then
 			systemctl stop radarr -q
 
 			echo_progress_start "Downloading source files"
-			if ! wget "https://radarr.servarr.com/v1/update/nightly/updatefile?os=linux&runtime=netcore&arch=x64" -O /tmp/Radarrv3.tar.gz >> "$log" 2>&1; then
+			if ! wget "https://radarr.servarr.com/v1/update/nightly/updatefile?os=linux&runtime=netcore&arch=x64" -O /tmp/Radarr.tar.gz >> "$log" 2>&1; then
 				echo_error "Download failed, exiting"
 				exit 1
 			fi
 			echo_progress_done "Source downloaded"
 
 			echo_progress_start "Extracting archive"
-			tar -xvf /tmp/Radarrv3.tar.gz -C /opt >> "$log" 2>&1
+			tar -xvf /tmp/Radarr.tar.gz -C /opt >> "$log" 2>&1
 			chown -R "$radarruser":"$radarruser" /opt/Radarr
 			echo_progress_done "Archive extracted"
 
