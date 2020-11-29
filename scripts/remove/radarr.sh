0b9c737f78e019b17cbd087c8d5b0adf88cfab20
Flying Sausages <flying_sausages@protonmail.com>
2020-11-29 15:54:38 +0100

    Fix remover

--- scripts/remove/radarr.sh
+++ scripts/remove/radarr.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 systemctl disable --now -q radarr
-rm -rf /etc/systemd/system/radarr.service
+rm /etc/systemd/system/radarr.service
 systemctl daemon-reload -q
 rm -rf /opt/Radarr
 
 if [[ -f /install/.nginx.lock ]]; then
-	rm -rf /etc/nginx/apps/radarrv3.conf
+	rm /etc/nginx/apps/radarr.conf
 	systemctl reload nginx
 fi
 
-rm -rf /install/.radarrv3.lock
+rm /install/.radarrv3.lock
