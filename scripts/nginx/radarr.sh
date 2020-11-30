2388361fa8fa67ff88f70ca644765fdb7209809b
flying-sausages <flying_sausages@protonmail.com>
2020-11-30 01:24:34 +0100

    Radarr v3 (#482)
    * paste files into v3 names, minor name subs
    
    * Working install
    
    * Added v3 check to v 0.2 installer
    
    * v2 and v0.2 changing, more v.02 install checks, move migration logic, remove sonarr things that are useless
    
    * Formatting
    
    * Prevent backups from being overwritten
    
    * fix some logic from radarr knowledge (don't overwrite an existing v3 config with an old v2 backup)
    
    * formatting
    
    * add right proxy headers to radarrv3 config
    
    * add sqlite to let radarr connect to rutorrent behind self-signed cert
    
    * Move sqlite command to nginx scripts so it triggers in less scenarios
    
    * Fix db paths oops
    
    * Updating to new standards and formatting
    
    * formatting on sonarrv3 file pre-merge
    
    * remove v02 flow from v3 installer
    
    * replace old scripts with new
    
    * remove upgrader as that is now done internally so
    
    * Add chown just in case lol
    
    * WIP updater
    
    * Untrack old files
    
    * Fix nginx path to fix
    
    * Fix remover
    
    * Handle cases where radarr .net migration cannot continue properly
    
    * Add legacy installer back for testing
    
    * shellcheck changes
    
    * remove v3 from remover
    
    * Refactor v3 variables
    
    * Switch releases to master from ngihtly
    
    * friendship ended with wget, curl is now my best friend
    
    * Don't stop radarr if the dl fails, remove binary as docs say
    
    * formatting
    
    * Change docs link, add echo for port on non-nginx
    
    * Move security policy to nginx config file and convert to API use
    
    * Fix nginx updater to wait for radarr api availability
    
    * Making updater more stable with different conditions
    
    * prevent fuckery with ipv6
    
    * Add sleeps so that nginx and radarr can spin up so that we can make queries
    
    * make slkeep conditional on nginx, add nicer handling of invalid response
    
    * quiet curls
    
    * reword update message
    
    * make sleep nginx-ignorant

--- scripts/nginx/radarr.sh
+++ scripts/nginx/radarr.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-# Nginx Configuration for Radarr
-# Author: liara
-# Copyright (C) 2017 Swizzin
-# Licensed under GNU General Public License v3.0 GPL-3 (in short)
-#
-#   You may copy, distribute and modify the software as long as you track
-#   changes/dates in source files. Any modifications to our software
-#   including (via compiler) GPL-licensed code must also be made available
-#   under the GPL along with build & install instructions.
-MASTER=$(cut -d: -f1 < /root/.master.info)
-isactive=$(systemctl is-active radarr)
+# Nginx conf for Radarr v3
+# Flying sausages 2020
+master=$(cut -d: -f1 < /root/.master.info)
 
-if [[ $isactive == "active" ]]; then
-	systemctl stop radarr
-fi
-if [[ ! -f /etc/nginx/apps/radarr.conf ]]; then
-	cat > /etc/nginx/apps/radarr.conf << RAD
+cat > /etc/nginx/apps/radarr.conf << RADARR
 location /radarr {
   proxy_pass        http://127.0.0.1:7878/radarr;
   proxy_set_header Host \$proxy_host;
@@ -23,26 +11,60 @@ location /radarr {
   proxy_set_header X-Forwarded-Proto \$scheme;
   proxy_redirect off;
   auth_basic "What's the password?";
-  auth_basic_user_file /etc/htpasswd.d/htpasswd.${MASTER};
+  auth_basic_user_file /etc/htpasswd.d/htpasswd.${master};
+
+  proxy_http_version 1.1;
+  proxy_set_header Upgrade \$http_upgrade;
+  proxy_set_header Connection \$http_connection;
 }
-RAD
-fi
-if [[ ! -d /home/${MASTER}/.config/Radarr/ ]]; then mkdir -p /home/${MASTER}/.config/Radarr/; fi
-cat > /home/${MASTER}/.config/Radarr/config.xml << RAD
-<Config>
-  <Port>7878</Port>
-  <UrlBase>radarr</UrlBase>
-  <BindAddress>127.0.0.1</BindAddress>
-  <SslPort>9898</SslPort>
-  <EnableSsl>False</EnableSsl>
-  <LogLevel>Info</LogLevel>
-  <Branch>master</Branch>
-  <LaunchBrowser>False</LaunchBrowser>
-  <UpdateMechanism>BuiltIn</UpdateMechanism>
-  <AnalyticsEnabled>False</AnalyticsEnabled>
-</Config>
-RAD
-chown -R ${MASTER}: /home/${MASTER}/.config/Radarr
+RADARR
+
+isactive=$(systemctl is-active radarr)
+
 if [[ $isactive == "active" ]]; then
-	systemctl start radarr
+	echo_log_only "Stopping radarr"
+	systemctl stop radarr
+fi
+user=$(grep User /etc/systemd/system/radarr.service | cut -d= -f2)
+echo_log_only "Radarr user detected as $user"
+apikey=$(awk -F '[<>]' '/ApiKey/{print $3}' /home/"$user"/.config/Radarr/config.xml)
+echo_log_only "API Key  = $apikey" >> "$log"
+#TODO cahnge Branch whenever that becomes relevant
+cat > /home/"$user"/.config/Radarr/config.xml << SONN
+<Config>
+  <LogLevel>info</LogLevel>
+  <UpdateMechanism>BuiltIn</UpdateMechanism>
+  <BindAddress>127.0.0.1</BindAddress>
+  <Port>7878</Port>
+  <SslPort>8787</SslPort>
+  <EnableSsl>False</EnableSsl>
+  <LaunchBrowser>False</LaunchBrowser>
+  <ApiKey>${apikey}</ApiKey>
+  <AuthenticationMethod>None</AuthenticationMethod>
+  <UrlBase>radarr</UrlBase>
+</Config>
+SONN
+
+chown -R "$user":"$user" /home/"$user"/.config/Radarr
+
+#shellcheck source=sources/functions/utils
+. /etc/swizzin/sources/functions/utils
+systemctl start radarr -q # Switch radarr on regardless whether it was off before or not as we need to have it online to trigger this cahnge
+
+sleep 5 # TODO replace with a loop until the API is available
+payload="$(curl -s "https://127.0.0.1/radarr/api/v3/config/host?apiKey=${apikey}" \
+	--user "${user}:$(_get_user_password "${user}")" --insecure \
+	-s | \
+	jq '.certificateValidation = "disabledForLocalAddresses"')"
+echo_log_only "Payload = \n${payload}"
+echo_log_only "Return from radarr after PUT ="
+curl "https://127.0.0.1/radarr/api/v3/config/host?apiKey=${apikey}" -X PUT --insecure \
+	-H 'Accept: application/json, text/javascript, */*; q=0.01' \
+	--compressed -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' \
+	--user "${user}:$(_get_user_password "${user}")" \
+	--data-raw "$payload" -s >> "$log"
+
+# Switch radarr back off if it was dead before
+if [[ $isactive != "active" ]]; then
+	systemctl stop radarr
 fi
---
+++ scripts/update/radarr.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+if [[ -f /install/.radarr.lock ]]; then
+
+	#Move v3mono installs to v3.net
+	if grep -q "ExecStart=/usr/bin/mono" /etc/systemd/system/radarr.service; then
+		echo_info "Moving Radarr from mono to .Net"
+		sleep 5 # TODO change this to something that would check that the Rqadarr API is query-able, as without this you will see nginx 502s
+		echo_log_only "Found radarr service pointing to mono"
+		#shellcheck source=sources/functions/utils
+		. /etc/swizzin/sources/functions/utils
+		[[ -z $radarrOwner ]] && radarrOwner=$(_get_master_username)
+		apikey=$(grep -oPm1 "(?<=<ApiKey>)[^<]+" /home/"${radarrOwner}"/.config/Radarr/config.xml)
+		echo_log_only "Apikey = $apikey"
+		# basicauth=$(echo "${radarrOwner}:$(_get_user_password ${radarrOwner})" | base64)
+		if [[ -f /install/.nginx.lock ]]; then
+			ret=$(curl -sS -L --insecure --user "${radarrOwner}":"$(_get_user_password "${radarrOwner}")" "http://127.0.0.1/radarr/api/v3/system/status?apiKey=${apikey}")
+		else
+			ret=$(curl -sS -L --insecure "http://127.0.0.1:7878/api/v3/system/status?apiKey=${apikey}")
+		fi
+		echo_log_only "Content of ret =\n ${ret}"
+		if echo "$ret " | jq . >> "$log" 2>&1; then
+			isnetcore=$(jq '.isNetCore' <<< "$ret")
+		else
+			echo_log_only "jq decided ret wasn't valid"
+		fi
+
+		##TODO find a different way to check this seeing as we need to query Radarr API, would ben nicer to do from FS
+		if [[ $isnetcore = "false" ]]; then # This case confirms we are running on v3 without .net core, i.e. the case we want to update
+
+			echo_progress_start "Downloading source files"
+			if ! curl "https://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64" -L -o /tmp/Radarr.tar.gz >> "$log" 2>&1; then
+				echo_error "Download failed, exiting"
+				exit 1
+			fi
+			echo_progress_done "Source downloaded"
+
+			echo_progress_start "Extracting archive"
+			systemctl stop radarr -q
+			rm /opt/Radarr/Radarr.exe
+			tar -xvf /tmp/Radarr.tar.gz -C /opt >> "$log" 2>&1
+			chown -R "$radarrOwner":"$radarrOwner" /opt/Radarr
+			echo_progress_done "Archive extracted"
+
+			# Watch out!
+			# If this sed runs, the updater will not trigger anymore. keep this at the bottom.
+			sed -i "s|ExecStart=/usr/bin/mono /opt/Radarr/Radarr.exe|ExecStart=/opt/Radarr/Radarr|g" /etc/systemd/system/radarr.service
+			#
+
+			systemctl daemon-reload
+			systemctl start radarr -q
+			echo_success "Radarr upgraded to .Net"
+
+		else #	This case triggers if the v3 API did not return correctly, which would indicate a switched off v3 or a v02
+			echo_warn "Could not reach v3 API.
+Please upgrade your radarr to v3 and ensure it is running to continue.
+The next time you will run 'box update', the instance will be migrated to .Net core"
+			echo_docs "application/radarr#Migrating-to-v3-on-.Net-Core "
+		fi
+	fi
+
+	#If nginx config is missing the attributes to have radarrv3 refresh UI right, then trigger the nginx script and reload
+	if [[ -f /install/.nginx.lock ]]; then
+		if ! grep "proxy_http_version 1.1" /etc/nginx/apps/radarr.conf -q; then
+			echo_progress_start "Upgrading nginx config for Radarr"
+			bash /etc/swizzin/scripts/nginx/radarr.sh
+			systemctl reload nginx -q
+			echo_progress_done "Nginx conf for Radarr upgraded"
+		fi
+	fi
+fi
