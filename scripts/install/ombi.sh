c03143c5a9c31a73f94594a09b90877523711986
Flying Sausages <flying_sausages@protonmail.com>
2020-12-01 01:32:24 +0100

    Remove opld service, remove comments, fix echos to terminal

--- scripts/install/ombi.sh
+++ scripts/install/ombi.sh
@@ -14,70 +14,33 @@
 #   under the GPL along with build & install instructions.
 #
 
-function _depends() {
+function _sources() {
 	echo_progress_start "Installing ombi apt sources"
 	if [[ ! -f /etc/apt/sources.list.d/ombi.list ]]; then
-		echo "deb http://repo.ombi.turd.me/stable/ jessie main" > /etc/apt/sources.list.d/ombi.list
-		wget -qO - https://repo.ombi.turd.me/pubkey.txt | sudo apt-key add -
+		echo "deb http://repo.ombi.turd.me/stable/ jessie main" > /etc/apt/sources.list.d/ombi.list >> "$log" 2>&1
+		wget -qO - https://repo.ombi.turd.me/pubkey.txt | sudo apt-key add - >> "$log" 2>&1
 	fi
+	echo_progress_done "Sources installed"
 	apt_update
-	echo_progress_done "Sources installed and refreshed"
 }
 
 function _install() {
-	#cd /opt
-	#curl -sL https://git.io/vKEJz | grep release | grep linux.tar.gz | cut -d "\"" -f 2 | sed -e 's/\/tidusjar/https:\/\/github.com\/tidusjar/g' | xargs wget --quiet -O Ombi.zip >/dev/null 2>&1
-	#mkdir ombi
-	#mv Ombi.zip ombi
-	#cd ombi
-	#unzip Ombi.zip >/dev/null 2>&1
-	#rm Ombi.zip
-	#cd /opt
-	#chown -R ${user}: ombi
 	apt_install ombi
 }
 
-function _services() {
-	echo_progress_start "Installing systemd service"
-	cat > /etc/systemd/system/ombi.service << OMB
-[Unit]
-Description=Ombi - PMS Requests System
-After=network-online.target
+function _nginx() {
 
-[Service]
-User=ombi
-Group=nogroup
-WorkingDirectory=/opt/Ombi/
-ExecStart=/opt/Ombi/Ombi --baseurl /ombi --host http://0.0.0.0:3000 --storage /etc/Ombi
-Type=simple
-TimeoutStopSec=30
-Restart=on-failure
-RestartSec=5
-
-[Install]
-WantedBy=multi-user.target
-OMB
-
-	touch /install/.ombi.lock
-	echo_progress_done "Service installed"
 	if [[ -f /install/.nginx.lock ]]; then
 		echo_progress_start "Configuring nginx"
 		bash /usr/local/bin/swizzin/nginx/ombi.sh
 		systemctl reload nginx
 		echo_progress_done "Nginx configured"
 	fi
-	echo_progress_start "Enabling and starting ombi"
-	systemctl enable -q ombi 2>&1 | tee -a $log
-	systemctl restart ombi
-	echo_progress_done "Ombi started"
+
 }
 
-distribution=$(lsb_release -is)
-user=$(cut -d: -f1 < /root/.master.info)
-
-# echo -ne "Initializing plex ... $i\033[0K\r"
-
-_depends
+_sources
 _install
-_services
+_nginx
+touch /install/.ombi.lock
 echo_success "Ombi installed"
