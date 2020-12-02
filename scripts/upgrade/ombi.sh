2f3ecd3ce070a0be2b5404bcdf42c94bd6039c26
Flying Sausages <flying_sausages@protonmail.com>
2020-12-02 23:56:51 +0100

    Add confirmation to undowngradeable scenario

--- scripts/upgrade/ombi.sh
+++ scripts/upgrade/ombi.sh
@@ -5,6 +5,12 @@ if [[ ! -f /install/.ombi.lock ]]; then
 fi
 
 if ! grep roxedus.github.io /etc/apt/sources.list.d/ombi.list; then
+	echo_warn "Please make sure to make sufficients database backups in case you'd like to downgrade from v4 to v3."
+	echo_docs "applications/ombi#upgrading-to-v4"
+	if ! ask "Do you want to continue?" N; then
+		exit 0
+	fi
+
 	echo_info "Upgrading ombi to v4 sources"
 	curl -sSL https://roxedus.github.io/apt-test/pub.key | sudo apt-key add -
 	echo "deb https://roxedus.github.io/apt-test/develop jessie main" | tee /etc/apt/sources.list.d/ombi.list
