f530c0b9d3b37a6cbe94e98a55e2890f776b43e9
Flying Sausages <flying_sausages@protonmail.com>
2020-12-04 18:56:01 +0100

    Actually perform a database backup

--- scripts/upgrade/ombi.sh
+++ scripts/upgrade/ombi.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 if [[ ! -f /install/.ombi.lock ]]; then
-	echo_error "Ombi not isntalled"
+	echo_error "Ombi not installed"
 	exit 1
 fi
 
 if ! grep -q roxedus.github.io /etc/apt/sources.list.d/ombi.list; then
-	echo_warn "Please make sure to make sufficients database backups in case you'd like to downgrade from v4 to v3."
-	echo_docs "applications/ombi#upgrading-to-v4"
-	if ! ask "Do you want to continue?" N; then
-		exit 0
-	fi
 
 	echo_info "Upgrading ombi to v4 sources"
 	curl -sSL https://roxedus.github.io/apt-test/pub.key | apt-key add - >> "$log" 2>&1
 	echo "deb https://roxedus.github.io/apt-test/develop jessie main" > /etc/apt/sources.list.d/ombi.list
+
+	echo_progress_start "Backing up Ombi v3 config and database"
+	mkdir -p /root/swizzin/backups/ombiv3
+	cp -R /etc/Ombi /root/swizzin/backups/ombiv3
+	echo_progress_done "Backed up to /root/swizzin/backups/ombiv3"
 
 	apt_update
 	apt_install ombi
 	if [[ -f /install/.nginx.lock ]]; then
 		bash /etc/swizzin/scripts/nginx/ombi.sh
 		systemctl reload nginx
-
+	else
+		echo_info "Ombi will be running on port 5000"
 	fi
 	echo_success "Ombi upgraded to v4"
 else
