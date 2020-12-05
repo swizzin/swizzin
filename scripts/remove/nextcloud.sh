7194be33f08f6f9d52ee434e0c47a5ea57ed802b
Flying Sausages <flying_sausages@protonmail.com>
2020-12-05 02:09:39 +0100

    formatting

--- scripts/remove/nextcloud.sh
+++ scripts/remove/nextcloud.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # nextcloud uninstaller
 host=$(mysql --execute="select host from mysql.user where user = 'nextcloud';" | grep -E "localhost|127.0.0.1")
-echo_info "Removing all Nextcloud data"
 mysql --execute="DROP DATABASE nextcloud;" && mysql --execute="DROP USER nextcloud@$host;"
-if [[ $? != "0" ]]; then 
-    echo_error "MySQL Drop failed. Please try again or investigate"
-    exit 1
+if [[ $? != "0" ]]; then
+	echo_error "MySQL Drop failed. Please try again or investigate"
+	exit 1
 fi
 crontab -l -u www-data | grep -v nextcloud > /tmp/.lol
 crontab -u www-data /tmp/.lol
