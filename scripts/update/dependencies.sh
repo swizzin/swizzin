7d677ea2c25d6dcd6a5c42ef0d70de7cc926da48
Flying Sausages <flying_sausages@protonmail.com>
2020-12-05 19:49:41 +0100

    fix dependency issue where we were checking for 2 dependencies at minimum instead of whether there _is_ one

--- scripts/update/dependencies.sh
+++ scripts/update/dependencies.sh
@@ -2,16 +2,17 @@
 # Ensures that dependencies are installed and corrects them if that is not the case.
 
 #space-separated list of required GLOBAL SWIZZIN dependencies (NOT application specific ones)
-dependencies="jq uuid-runtime net-tools fortune"
+dependencies="jq uuid-runtime net-tools fortune-mod cowsay"
 
 missing=()
 for dep in $dependencies; do
 	if ! check_installed "$dep"; then
+		echo_log_only "$dep is missing"
 		missing+=("$dep")
 	fi
 done
 
-if [[ ${missing[1]} != "" ]]; then
+if [[ ${missing[0]} != "" ]]; then
 	echo_info "Installing the following dependencies: ${missing[*]}"
 	apt_install "${missing[@]}"
 else
