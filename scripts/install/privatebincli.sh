5f608a8e2d934133782aaade713400ca0e5e0ba7
Flying Sausages <flying_sausages@protonmail.com>
2020-12-09 13:10:11 +0100

    add source to npm functions, fix some echoes

--- scripts/install/privatebincli.sh
+++ scripts/install/privatebincli.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Privatebin CLI installer, dependency for dumpinfo
 
+#shellcheck source=sources/functions/npm
+. /etc/swizzin/sources/functions/npm
 npm_install
-npm install -g @pixelfactory/privatebin
+
+echo_progress_start "Installing Privatebin CLI"
+npm install -g @pixelfactory/privatebin >> "$log"
+echo_progress_done "Privatebin CLI installed"
