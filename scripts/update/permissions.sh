91f2d9c33c2769a16dc21c3623e224d9fabe6de1
Flying Sausages <flying_sausages@protonmail.com>
2020-12-11 15:21:37 +0100

    shellcheck permissions

--- scripts/update/permissions.sh
+++ scripts/update/permissions.sh
@@ -4,15 +4,18 @@ find /home -mindepth 1 -maxdepth 1 -type d -exec chmod 750 {} \;
 
 if [[ -f /install/.plex.lock ]]; then
     master=$(cut -d: -f1 < /root/.master.info)
-    if [[ -z $(groups plex | grep ${master}) ]]; then
-        usermod -a -G ${master} plex
+    if ! groups plex | grep -q "${master}"; then
+        echo_log_only "Adding plex to master group"
+        usermod -a -G "${master}" plex
     fi
 fi
 
+#shellcheck disable=SC2016
 if grep -q 'export PATH=$PATH:/usr/local/bin/swizzin' /root/.profile; then
     sed -i '/export PATH=$PATH:\/usr\/local\/bin\/swizzin/d' /root/.profile
 fi
 
+#shellcheck disable=SC2016
 if grep -q 'export PATH=$PATH:/usr/local/bin/swizzin' /root/.bashrc; then
     :
 else
