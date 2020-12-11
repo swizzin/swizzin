931832bbe127badea5deb5c1c5505a3152a872a3
Flying Sausages <flying_sausages@protonmail.com>
2020-12-11 13:34:45 +0100

    fix small formatting and sourcing

--- scripts/update/node.sh
+++ scripts/update/node.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Node update check
 # author: liara
+#shellcheck source=sources/functions/npm
 . /etc/swizzin/sources/functions/npm
-if [[ -f /etc/apt/sources.list.d/nodesource.listE ]]; then mv /etc/apt/sources.list.d/nodesource.listE /etc/apt/sources.list.d/nodesource.list; fi
+if [[ -f /etc/apt/sources.list.d/nodesource.listE ]]; then
+    mv /etc/apt/sources.list.d/nodesource.listE /etc/apt/sources.list.d/nodesource.list
+fi
 npm_update
