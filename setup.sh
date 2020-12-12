801924ab4e06a9123060c9b64b31c20319e16b88
userdocs <16525024+userdocs@users.noreply.github.com>
2020-12-12 18:43:51 +0000

    improve: make setup completion more helpful and detect if a reboot is… (#517)
    * improve: make setup completion more helpful and detect if a reboot is actually required.
    
    * improve: update read me in-line with setup.sh changes
    
    * fix: only echo path to bashrc if it does not exist to prevent duplicate entries
    
    * fix: remove extra forward slash in doc url generation
    
    * Apply suggestions from code review
    
    * some echos
    
    * Revert "some echos" as that's all handled in PR #438
    
    This reverts commit fa5003e7f3eb95d38462bf31570d4a6e6abf9482.
    
    Co-authored-by: flying-sausages <flying_sausages@protonmail.com>

--- setup.sh
+++ setup.sh
@@ -319,7 +319,7 @@ function _install() {
 
 function _post() {
     ip=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
-    echo "export PATH=\$PATH:/usr/local/bin/swizzin" >> /root/.bashrc
+    if grep -ow '^export PATH=$PATH:/usr/local/bin/swizzin$' ~/.bashrc &> /dev/null; then true; else echo "export PATH=\$PATH:/usr/local/bin/swizzin" >> /root/.bashrc; fi
     #echo "export PATH=\$PATH:/usr/local/bin/swizzin" >> /home/$user/.bashrc
     #chown ${user}: /home/$user/.profile
     echo "Defaults    secure_path = /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin/swizzin" > /etc/sudoers.d/secure_path
@@ -339,7 +339,19 @@ function _post() {
         echo "Your deluge web port is$(grep port /home/${user}/.config/deluge/web.conf | cut -d: -f2 | cut -d"," -f1)"
         echo ""
     fi
-    echo -e "\e[1m\e[31mPlease note, certain functions may not be fully functional until your server is rebooted or you log out and back in. However you may issue the command 'source /root/.bashrc' to begin using box and related functions now\e[0m"
+    #
+    if [[ -f /var/run/reboot-required ]]; then
+        echo_warn "The server requires a reboot to finalise this installation. Please reboot now."
+        echo
+    else
+        echo_success "You can now use the box command to manage swizzin features"
+        echo
+        echo_info "box install nginx panel"
+        echo
+        echo_docs getting-started/box-basics
+        echo
+    fi
+
 }
 
 _os
