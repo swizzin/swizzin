a02d6de7e3724ac1fec1153c12bae45e211936c2
flying-sausages <flying_sausages@protonmail.com>
2020-12-12 03:06:08 +0100

    Fix organizr v21 (#450)
    * Add updater for organizr nginx config
    
    * the "should work" config
    
    * formatting
    
    * This config works?????
    
    * revert back to alias for nginx block
    
    * switch this back seeing as otherwise API won't work??
    
    * disable organizr db setup, user adding block and f2b to troubleshoot
    
    * remove old noninteractive block
    
    * Enable master user setup, small prep for creating other users via API
    
    * remove unnecessary FS check, re-enable f2b
    
    * remove organizr lockfile when nginx is removed
    
    * properly comment adduser WIP
    
    * remove db fs check entirely
    
    * Fix formkey use oops
    
    * fix shellcheck
    
    * Force organizr update to prevent the upstream bug with non-baseurl
    
    * fixup! Force organizr update to prevent the upstream bug with non-baseurl
    
    * Undo master squashmerge because github silly
    
    * mastr formatting
    
    * formatting

---
+++ scripts/update/organizr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ -f /install/organizr.sh ]]; then
+    # If there is no mention of the v2 api, re-run nginx config
+    if ! grep -q api/v2 /etc/nginx/apps/organizr.conf; then
+        #shellcheck source=scripts/nginx/organizr.sh
+        echo_progress_start "Updating organizr to v2.1"
+        bash /etc/swizzin/scripts/nginx/organizr.sh
+        echo_progress_done "Organizr nginx config updated"
+
+        echo_progress_start "Pulling down new organizr source code"
+        git -C "/srv/organizr" pull >> "${log:?}"
+        echo_progress_done "Retrieved new organizr source code"
+    fi
+
+fi
