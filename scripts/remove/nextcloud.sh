b2145bc0789486f8eebc3b698063f0c11ac6917a
Flying Sausages <flying_sausages@protonmail.com>
2020-12-15 03:33:18 +0100

    fix remove script

--- scripts/remove/nextcloud.sh
+++ scripts/remove/nextcloud.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # nextcloud uninstaller
-host=$(mysql --execute="select host from mysql.user where user = 'nextcloud';" | grep -E "localhost|127.0.0.1")
-if ! mysql --execute="DROP DATABASE nextcloud;" && mysql --execute="DROP USER nextcloud@$host;"; then
+# host=$(mysql --execute="select host from mysql.user where user = 'nextcloud';" | grep -E "localhost|127.0.0.1")
+host='localhost'
+if ! mysql --execute="DROP DATABASE nextcloud;"; then
     echo_error "MySQL Drop failed. Please try again or investigate"
-    exit 1
+    # exit 1
 fi
+mysql --execute="DROP USER nextcloud@$host;"
+
 crontab -l -u www-data | grep -v nextcloud > /tmp/.lol
 crontab -u www-data /tmp/.lol
 rm -rf /srv/nextcloud
