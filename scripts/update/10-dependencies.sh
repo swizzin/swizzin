4c86595141ecb5a2b6a2d1cbc812c316201b66e0
Flying Sausages <flying_sausages@protonmail.com>
2020-12-16 22:14:31 +0100

    make cracklib a required dependency

--- scripts/update/10-dependencies.sh
+++ scripts/update/10-dependencies.sh
@@ -2,7 +2,7 @@
 # Ensures that dependencies are installed and corrects them if that is not the case.
 
 #space-separated list of required GLOBAL SWIZZIN dependencies (NOT application specific ones)
-dependencies="whiptail git sudo curl wget lsof fail2ban apache2-utils vnstat tcl tcl-dev build-essential dirmngr apt-transport-https bc uuid-runtime jq net-tools fortune gnupg2"
+dependencies="whiptail git sudo curl wget lsof fail2ban apache2-utils vnstat tcl tcl-dev build-essential dirmngr apt-transport-https bc uuid-runtime jq net-tools fortune gnupg2 libcrack2-dev"
 
 missing=()
 for dep in $dependencies; do
