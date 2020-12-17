acec6abe05c56f3dfa0b94126bb8265061bcb088
liaralabs <liara@swizzin.ltd>
2020-12-16 18:08:19 -0800

    Rebuild venv instead of moving it and ensure new venv is removed

--- scripts/remove/panel.sh
+++ scripts/remove/panel.sh
@@ -3,6 +3,7 @@
 systemctl disable --now -q panel
 rm -rf /srv/panel > /dev/null 2>&1
 rm -rf /opt/swizzin
+rm -rf /opt/.venv/swizzin
 rm -f /etc/nginx/apps/panel.conf
 rm -f /etc/sudoers.d/panel
 rm /etc/cron.d/set_interface > /dev/null 2>&1
