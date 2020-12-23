72e9b2c5bea399d1c4cfab6b0b152a5612f71c27
Flying Sausages <flying_sausages@protonmail.com>
2020-12-23 03:06:21 +0100

    Move git logic to an update script and add environment variables for branch

---
+++ scripts/update/10-git.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [[ -L /etc/swizzin ]]; then
+    echo_warn "Not updating /etc/swizzin as it is a symlink. Please consult your provider/maintainers in case you believe this is an error."
+elif [[ -e /etc/swizzin/.dev.lock ]]; then
+    echo_warn "Not updating /etc/swizzin as a dev lockfile is present."
+elif [[ $1 == "--dev" ]]; then
+    echo_warn "Not updating from git due to --dev flag"
+else
+    echo_progress_start "Updating swizzin local repository"
+    cd /etc/swizzin || exit
+    git checkout "${SWIZ_GIT_CHECKOUT:-master}"
+    git fetch origin "${SWIZ_GIT_CHECKOUT:-master}"
+    git reset --hard origin/"${SWIZ_GIT_CHECKOUT:-master}"
+    echo_info "HEAD is now set to $(git log --pretty=format:'%h' -n1)"
+    echo_progress_done "Local repository updated from ${SWIZ_GIT_CHECKOUT:-master}"
+fi
