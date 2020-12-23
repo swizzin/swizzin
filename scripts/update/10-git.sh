16f3a9a958199dce3f76ab276edb06cdb5c32946
Flying Sausages <flying_sausages@protonmail.com>
2020-12-23 04:39:03 +0100

    Add tag fetching and force checkouts

--- scripts/update/10-git.sh
+++ scripts/update/10-git.sh
@@ -9,8 +9,9 @@ elif [[ $1 == "--dev" ]]; then
 else
     echo_progress_start "Updating swizzin local repository"
     cd /etc/swizzin || exit
-    git checkout "${SWIZ_GIT_CHECKOUT:-master}"
+    git fetch --all --tags --prune
     git fetch origin "${SWIZ_GIT_CHECKOUT:-master}"
+    git checkout -f "${SWIZ_GIT_CHECKOUT:-master}"
     git reset --hard origin/"${SWIZ_GIT_CHECKOUT:-master}"
     echo_info "HEAD is now set to $(git log --pretty=format:'%h' -n1)"
     echo_progress_done "Local repository updated from ${SWIZ_GIT_CHECKOUT:-master}"
