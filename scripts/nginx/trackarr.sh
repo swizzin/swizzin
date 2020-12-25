480c350603c8a08474f9032e4f0d71f652183977
Flying Sausages <flying_sausages@protonmail.com>
2020-12-25 05:37:47 +0100

    remove extra protecting from nginx because nginx

--- scripts/nginx/trackarr.sh
+++ scripts/nginx/trackarr.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 
+. /etc/swizzin/sources/functions/utils
+user=$(_get_master_username)
 cat > /etc/nginx/apps/trackarr.conf << EOF
 location /trackarr/ {
     proxy_pass              http://127.0.0.1:7337/trackarr;
     proxy_set_header        X-Forwarded-Host        \$http_host;
+    auth_basic "What's the password?";
+    auth_basic_user_file /etc/htpasswd.d/htpasswd.${user};
 }
 EOF
 
 sed -i "s|baseurl: /$|baseurl: /trackarr|" /opt/trackarr/config.yaml
 sed -i "s|host: 0.0.0.0|host: 127.0.0.1|" /opt/trackarr/config.yaml
-
-if ! grep -q "user:" /opt/trackarr/config.yaml; then
-    . /etc/swizzin/sources/functions/utils
-    master=$(_get_master_username)
-    pass=$(_get_user_password "$master")
-    sed -i "/^server:*/a \ \ user: $master" /opt/trackarr/config.yaml
-    sed -i "/^server:*/a \ \ pass: $pass" /opt/trackarr/config.yaml
-fi
