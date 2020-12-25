732010ceac887a0ab4c893c308d56068c6db5c01
Flying Sausages <flying_sausages@protonmail.com>
2020-12-25 05:20:25 +0100

    change host bind

--- scripts/nginx/trackarr.sh
+++ scripts/nginx/trackarr.sh
@@ -10,7 +10,8 @@ EOF
 . /etc/swizzin/sources/functions/utils
 master=$(_get_master_username)
 pass=$(_get_user_password "$master")
-sed -i "s|baseurl: /$|baseurl: /trackarr |" /opt/trackarr/config.yaml
+sed -i "s|baseurl: /$|baseurl: /trackarr|" /opt/trackarr/config.yaml
+sed -i "s|host: 0.0.0.0|host: 127.0.0.1|" /opt/trackarr/config.yaml
 
 if ! grep -q "user:" /opt/trackarr/config.yaml; then
     sed -i "/^server:*/a \ \ user: $master" /opt/trackarr/config.yaml
