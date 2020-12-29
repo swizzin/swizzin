fd804201f481c6f7acd1a0bcc2600eeb969a4794
Flying Sausages <flying_sausages@protonmail.com>
2020-12-29 16:20:37 +0100

    Offer to remove the DB install as well, off by default

--- scripts/remove/nextcloud.sh
+++ scripts/remove/nextcloud.sh
@@ -5,7 +5,15 @@ read -s 'password'
 rm -rf /srv/nextcloud
 rm /etc/nginx/apps/nextcloud.conf
 systemctl reload nginx
-host=$(mysql -u root --password="$password" --execute="select host from mysql.user where user = 'nextcloud';" | grep -E "localhost|127.0.0.1")
-mysql --user="root" --password="$password" --execute="DROP DATABASE nextcloud;"
-mysql --user="root" --password="$password" --execute="DROP USER nextcloud@$host;"
+
+if ask "Would you like to also remove the mysql/mariadb server?" N; then
+    apt_remove mariadb-server --purge
+else
+    echo_progress_start "Dropping nextcloud tables and user"
+    host=$(mysql -u root --password="$password" --execute="select host from mysql.user where user = 'nextcloud';" | grep -E "localhost|127.0.0.1")
+    mysql --user="root" --password="$password" --execute="DROP DATABASE nextcloud;"
+    mysql --user="root" --password="$password" --execute="DROP USER nextcloud@$host;"
+    echo_progress_done "Nextcloud dropped from database"
+fi
+
 rm /install/.nextcloud.lock
