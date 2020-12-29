cd1246598d4ec8092bebae991d52af70358ee74d
Flying Sausages <flying_sausages@protonmail.com>
2020-12-29 02:56:18 +0100

    Handle multi-arch for ombi

--- scripts/install/ombi.sh
+++ scripts/install/ombi.sh
@@ -4,8 +4,22 @@
 
 function _sources() {
     echo_progress_start "Installing ombi apt sources"
-    echo "deb http://repo.ombi.turd.me/stable/ jessie main" > /etc/apt/sources.list.d/ombi.list
-    wget -qO - https://repo.ombi.turd.me/pubkey.txt | apt-key add - >> "$log" 2>&1
+
+    case "$(_os_arch)" in
+        "arm64")
+            echo_info "Installing v4 as v3 only supports amd64"
+            curl -sSL https://roxedus.github.io/apt-test/pub.key | apt-key add - >> "$log" 2>&1
+            echo "deb https://roxedus.github.io/apt-test/develop jessie main" > /etc/apt/sources.list.d/ombi.list
+            ;;
+        "armhf" | "amd64")
+            echo "deb http://repo.ombi.turd.me/stable/ jessie main" > /etc/apt/sources.list.d/ombi.list
+            wget -qO - https://repo.ombi.turd.me/pubkey.txt | apt-key add - >> "$log" 2>&1
+            ;;
+        *)
+            echo_error "Unsupported arch"
+            exit 1
+            ;;
+    esac
     echo_progress_done "Sources installed"
     apt_update
 }
