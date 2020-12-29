5a867b31b04d670aa567b0289a53f89ad86c9a5a
Flying Sausages <flying_sausages@protonmail.com>
2020-12-30 00:41:07 +0100

    fix nginx reload

--- scripts/remove/trackarr.sh
+++ scripts/remove/trackarr.sh
@@ -7,7 +7,7 @@ userdel -rf trackarr
 
 if [[ -f /install/.nginx.lock ]]; then
     rm /etc/nginx/apps/trackarr.conf
-    systemctl nginx reload
+    systemctl reload nginx
 fi
 
 rm /install/.trackarr.lock
