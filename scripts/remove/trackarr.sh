a5ab9da7f0e2704a14cc299bc886dcae5ac959f4
Flying Sausages <flying_sausages@protonmail.com>
2020-12-30 02:26:19 +0100

    claen rm echos

--- scripts/remove/trackarr.sh
+++ scripts/remove/trackarr.sh
@@ -2,8 +2,8 @@
 
 systemctl disable --now -q trackarr
 
-rm -rf /opt/trackarr
-userdel -rf trackarr
+rm -rf /opt/trackarr >> "$log" 2>&1
+userdel -rf trackarr >> "$log" 2>&1
 
 if [[ -f /install/.nginx.lock ]]; then
     rm /etc/nginx/apps/trackarr.conf
