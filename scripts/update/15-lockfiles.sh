9afe274136efc2f5932601186550972b3e0a9be0
Flying Sausages <flying_sausages@protonmail.com>
2020-12-30 22:46:42 +0100

    add migration script

---
+++ scripts/update/15-lockfiles.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Added 31 Dec 2020
+# Move lockfiles from old directory to whatever the new one ends up being lol
+# should run once and only once
+if [[ -d /install/ ]]; then
+    readarray -t list_installed < <(find /install -type f -name ".*.lock" | awk -F. '{print $2}')
+    echo_log_only "Moving ${list_installed[*]} to  "
+    for app in "${list_installed[@]}"; do
+        lock "$app"
+    done
+    rm -rf /install/ # byyeeeee
+fi
