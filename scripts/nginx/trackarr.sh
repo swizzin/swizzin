b1f61b1071349c0b8e7828afc0b8b3aea025fa8c
Flying Sausages <flying_sausages@protonmail.com>
2020-12-30 01:58:33 +0100

    Revert "use trackarr auth instead"
    This reverts commit c6f76f7c75d900467bf53f4b82866ca9a960e3df.

--- scripts/nginx/trackarr.sh
+++ scripts/nginx/trackarr.sh
@@ -7,8 +7,14 @@ location /trackarr/ {
     proxy_pass              http://127.0.0.1:7337/trackarr/;
     proxy_set_header        Host                    \$proxy_host;
     proxy_set_header        X-Forwarded-Host        \$http_host;
+    auth_basic              "What's the password?";
+    auth_basic_user_file    /etc/htpasswd.d/htpasswd.${user};
 }
 EOF
+
+# Prevent double-basic-auth req if nginx was installed after trackarr
+sed -i '/user:/d' /opt/trackarr/config.yaml
+sed -i '/pass:/d' /opt/trackarr/config.yaml
 
 sed -i "s|baseurl: /$|baseurl: /trackarr|" /opt/trackarr/config.yaml
 sed -i "s|host: 0.0.0.0|host: 127.0.0.1|" /opt/trackarr/config.yaml
