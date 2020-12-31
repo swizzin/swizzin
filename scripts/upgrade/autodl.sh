e65759a3f079cb956c67c4ea008894e7521e965b
Flying Sausages <flying_sausages@protonmail.com>
2020-12-31 16:02:46 +0100

    get rid of `cd` because it is relative-ass cancer, add echos

--- scripts/upgrade/autodl.sh
+++ scripts/upgrade/autodl.sh
@@ -4,21 +4,27 @@
 . /etc/swizzin/sources/functions/utils
 readarray -t users < <(_get_user_list)
 
+echo_progress_start "Downloading autodl-irssi latest release"
 wget -q "$(curl -sL http://git.io/vlcND | jq .assets[0].browser_download_url -r)" -O /tmp/autodl-irssi.zip >> $log 2>&1 || {
     echo_error "Autodl download failed, please check the log"
     exit 1
 }
+echo_progress_done "Release downloaded"
 
+echo_progress_start "Extracting release for each user"
 for u in "${users[@]}"; do
-    cd "/home/${u}/.irssi/scripts/"
-    rm -rf AutodlIrssi
-    rm -f autodl-irssi.pl
-    rm -f autorun/autodl-irssi.pl
-    cp /tmp/autodl-irssi.zip .
-    unzip -o autodl-irssi.zip >> "${log}" 2>&1
-    rm autodl-irssi.zip
-    cp autodl-irssi.pl autorun/
-    chown -R $u: /home/${u}/.irssi/
+    echo_log_only "Extracting for user $u"
+    irssipath="/home/${u}/.irssi/scripts/"
+    # cd "/home/${u}/.irssi/scripts/"
+    rm -rf "$irssipath"/AutodlIrssi
+    rm -f "$irssipath"/autodl-irssi.pl
+    rm -f "$irssipath"/autorun/autodl-irssi.pl
+    # cp /tmp/autodl-irssi.zip .
+    unzip -o tmp/autodl-irssi.zip -d "$irssipath" >> "${log}" 2>&1
+    # rm "$irssipath"/autodl-irssi.zip
+    cp "$irssipath"/autodl-irssi.pl "$irssipath"/autorun/
+    chown -R "$u": /home/"${u}"/.irssi/
 done
+echo_progress_done "Release installed for all users"
 
 rm /tmp/autodl-irssi.zip
