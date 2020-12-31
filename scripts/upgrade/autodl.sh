f0a0022753e9dfeab9de57b91ec2efa6110a9a93
Flying Sausages <flying_sausages@protonmail.com>
2020-12-31 15:55:00 +0100

    dirty fix for autodl

--- scripts/upgrade/autodl.sh
+++ scripts/upgrade/autodl.sh
@@ -2,14 +2,21 @@
 
 users=($(cut -d: -f1 < /etc/htpasswd))
 
+wget -q $(curl -sL http://git.io/vlcND | jq .assets[0].browser_download_url -r) -O /tmp/autodl-irssi.zip >> $log 2>&1 || {
+    echo_error "Autodl download failed, please check the log"
+    exit 1
+}
+
 for u in "${users[@]}"; do
     cd "/home/${u}/.irssi/scripts/"
     rm -rf AutodlIrssi
     rm -f autodl-irssi.pl
     rm -f autorun/autodl-irssi.pl
-    curl -sL http://git.io/vlcND | grep -Po '(?<="browser_download_url": ")(.*-v[\d.]+.zip)' | xargs wget --quiet -O autodl-irssi.zip
+    cp /tmp/autodl-irssi.zip .
     unzip -o autodl-irssi.zip >> "${log}" 2>&1
     rm autodl-irssi.zip
     cp autodl-irssi.pl autorun/
     chown -R $u: /home/${u}/.irssi/
 done
+
+rm /tmp/autodl-irssi.zip
