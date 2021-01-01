b3160a7c900b54efafbb1326c54fd04d7bb1f637
Flying Sausages <flying_sausages@protonmail.com>
2021-01-01 16:43:51 +0100

    split things into functions, remove sqlite shit

--- scripts/install/overseerr.sh
+++ scripts/install/overseerr.sh
@@ -2,50 +2,50 @@
 
 # Overseer installer by flying sausages 2020 GPLv3
 
-#shellcheck source=sources/functions/npm
-. /etc/swizzin/sources/functions/npm
+_dependencies() {
+    apt_install libsqlite3-dev sqlite3
 
-apt_install libsqlite3-dev sqlite3
+    #shellcheck source=sources/functions/npm
+    . /etc/swizzin/sources/functions/npm
+    npm_install #Install node 12 LTS and npm if they're not present or outdated
 
-npm_install #Install node 12 LTS and npm if they're not present or outdated
-
-echo_progress_start "Installing yarn"
-npm install -g yarn || {
-    echo_error "Yarn failed to install"
-    exit 1
+    echo_progress_start "Installing yarn"
+    npm install -g yarn || {
+        echo_error "Yarn failed to install"
+        exit 1
+    }
+    echo_progress_done "Yarn installed"
 }
-echo_progress_done "Yarn installed"
 
-echo_progress_start "Downloading and extracting source code"
-dlurl="$(curl https://api.github.com/repos/sct/overseerr/releases/latest | jq .tarball_url -r)"
-wget "$dlurl" -O /tmp/overseerr.tar.gz || {
-    echo_error "Download failed"
-    exit 1
+_install() {
+    echo_progress_start "Downloading and extracting source code"
+    dlurl="$(curl https://api.github.com/repos/sct/overseerr/releases/latest | jq .tarball_url -r)"
+    wget "$dlurl" -O /tmp/overseerr.tar.gz || {
+        echo_error "Download failed"
+        exit 1
+    }
+    mkdir -p /opt/overseerr
+    tar --strip-components=1 -C /opt/overseerr -xzvf /tmp/overseerr.tar.gz
+    echo_progress_done "Code extracted"
+
+    echo_progress_start "Installing via yarn"
+    # Ensure sqlite can build right in case it needs to use python
+    if ! which python > /dev/null; then #TODO make this a more specific check as this could interfere with other things possibly
+        npm config set python "$(which python3)"
+    fi
+    yarn install --cwd /opt/overseerr || {
+        echo_error "Failed to install dependencies"
+        exit 1
+    }
+    yarn --cwd /opt/overseerr build || {
+        echo_error "Failed to build overseerr sqlite"
+        exit 1
+    }
+    # yarn cache clean
+    echo_progress_done "Dependencies installed"
 }
-mkdir -p /opt/overseerr
-tar --strip-components=1 -C /opt/overseerr -xzvf /tmp/overseerr.tar.gz
-echo_progress_done "Code extracted"
 
-echo_progress_start "Installing via yarn"
-# npm install -g sqlite3 --build-from-source --sqlite=/usr/bin || {
-#     echo_error "Failed to install sqlite"
-#     exit 1
-# }
-
-# Ensure sqlite can build right in case it needs to use python
-if ! which python > /dev/null; then #TODO make this a more specific check as this could interfere with other things possibly
-    npm config set python "$(which python3)"
-fi
-
-yarn install --cwd /opt/overseerr || {
-    echo_error "Failed to install dependencies"
-    exit 1
-}
-yarn --cwd /opt/overseerr build || {
-    echo_error "Failed to build overseerr sqlite"
-    exit 1
-}
-# yarn cache clean
-echo_progress_done "Dependencies installed"
+_dependencies
+_install
 
 touch /install/.overseerr.lock
