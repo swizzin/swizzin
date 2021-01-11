acf58fc6f4b363db7c2bed3b64c903bab742a4a5
Flying Sausages <flying_sausages@protonmail.com>
2021-01-11 04:20:50 +0100

    remover notes

--- scripts/remove/calibre.sh
+++ scripts/remove/calibre.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#TODO shut up and make
+# Some systems could be apt-installed, some will have script/built setup
 if [[ -e /usr/bin/calibre-uninstall ]]; then
+    #TODO shut up and make this go unattended
     /usr/bin/calibre-uninstall
 else
     apt_remove calibre
