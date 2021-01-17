4040f24edd39a658e85e1db9831ab23b739f20b3
flying-sausages <flying_sausages@protonmail.com>
2021-01-17 19:45:31 +0100

    Swizzin file-based "quasi-DB" (#565)
    * first attempt at "swizzin db"
    
    * fix exports, fix regex, add to globals
    
    * change separator
    
    * fix logs, switch from cut to awk
    
    * switch from one file to multiple
    
    * remove separator
    
    * Snippets
    
    * refactor entire thing around path-getting method pathswizdb
    
    * pathswizdb snippets
    
    * clean comments
    
    * Add wrapper function, rename old to internals
    
    * Fix snippets
    
    * fix bad value variable
    
    * ensure value is returned _as well as_ logged
    
    * Make logs more explicit, catch possible exceptions from path function
    
    * add extra log for when file is found

--- sources/globals.sh
+++ sources/globals.sh
@@ -26,3 +26,7 @@ export log="/root/logs/swizzin.log"
 # A script to standardise yes/no dialogues
 #shellcheck source=sources/functions/ask
 . /etc/swizzin/sources/functions/ask
+
+# Tool to manage key-value pairs on filesystem in persistent storage
+#shellcheck source=sources/functions/swizdb
+. /etc/swizzin/sources/functions/swizdb
