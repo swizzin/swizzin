36080bdc8d5704a56bff21e2782c328551221ef1
Flying Sausages <flying_sausages@protonmail.com>
2021-01-18 01:23:32 +0100

    escape dollar in cat

--- scripts/nginx/mango.sh
+++ scripts/nginx/mango.sh
@@ -5,7 +5,7 @@ cat > /etc/nginx/apps/mango.conf << EOF
 location /mango/ {
   proxy_pass http://localhost:9003/;
   proxy_http_version 1.1;
-  proxy_set_header Upgrade $http_upgrade;
+  proxy_set_header Upgrade \$http_upgrade;
   proxy_set_header Connection "upgrade";
 }
 EOF
