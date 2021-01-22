fc5a657421a834cfb7aa7a85ebe334f2a1f90603
Alex Ling <hkalexling@gmail.com>
2021-01-23 06:05:07 +0800

    IMPROVE: Nginx WebSocket support for Mango (#573)
    * Nginx WebSocket support for Mango
    
    * escape dollar in cat
    
    * Updating old nginx configs for websocket support
    
    Co-authored-by: Flying Sausages <flying_sausages@protonmail.com>

--- scripts/nginx/mango.sh
+++ scripts/nginx/mango.sh
@@ -4,6 +4,9 @@
 cat > /etc/nginx/apps/mango.conf << EOF
 location /mango/ {
   proxy_pass http://localhost:9003/;
+  proxy_http_version 1.1;
+  proxy_set_header Upgrade \$http_upgrade;
+  proxy_set_header Connection "upgrade";
 }
 EOF
 
