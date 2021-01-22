87576a32f29f0e1c06400817e0f7b4ade0e39977
liaralabs <liara@swizzin.ltd>
2021-01-21 16:51:33 -0800

    Update sabnzbd

--- scripts/install/sabnzbd.sh
+++ scripts/install/sabnzbd.sh
@@ -10,22 +10,36 @@
 #   including (via compiler) GPL-licensed code must also be made available
 #   under the GPL along with build & install instructions.
 
-user=$(cut -d: -f1 < /root/.master.info)
-password=$(cut -d: -f2 < /root/.master.info)
-distribution=$(lsb_release -is)
-codename=$(lsb_release -cs)
-#latest=$(curl -s https://sabnzbd.org/downloads | grep -m1 Linux | grep download-link-src | grep -oP "href=\"\K[^\"]+")
-latest=$(curl -sL https://api.github.com/repos/sabnzbd/sabnzbd/releases/latest | grep -Po '(?<="browser_download_url":).*?[^\\].tar.gz"' | sed 's/"//g')
-latestversion=$(echo $latest | awk -F "/" '{print $NF}' | cut -d- -f2)
-
 . /etc/swizzin/sources/functions/pyenv
 . /etc/swizzin/sources/functions/utils
 
-LIST='par2 p7zip-full python3-dev python3-setuptools python3-pip python3-venv libffi-dev libssl-dev libglib2.0-dev libdbus-1-dev'
+user=$(_get_master_username)
+password=$(_get_user_password ${user})
+#latest=$(curl -s https://sabnzbd.org/downloads | grep -m1 Linux | grep download-link-src | grep -oP "href=\"\K[^\"]+")
+latest=$(curl -sL https://api.github.com/repos/sabnzbd/sabnzbd/releases/latest | grep -Po '(?<="browser_download_url":).*?[^\\].tar.gz"' | sed 's/"//g')
+latestversion=$(echo $latest | awk -F "/" '{print $NF}' | cut -d- -f2)
+systempy3_ver=$(get_candidate_version python3)
+
+if dpkg --compare-versions ${systempy3_ver} lt 3.6.0 && dpkg --compare-versions ${latestversion} ge 3.2.0; then
+    LIST='par2 p7zip-full libffi-dev libssl-dev libglib2.0-dev libdbus-1-dev'
+    PYENV=True
+else
+    LIST='par2 p7zip-full python3-dev python3-setuptools python3-pip python3-venv libffi-dev libssl-dev libglib2.0-dev libdbus-1-dev'
+fi
 
 apt_install $LIST
 
-python3_venv ${user} sabnzbd
+case ${PYENV} in
+    True)
+        pyenv_install
+        pyenv_install_version 3.7.7
+        pyenv_create_venv 3.7.7 /opt/.venv/sabnzbd
+        chown -R ${user}: /opt/.venv/sabnzbd
+        ;;
+    *)
+        python3_venv ${user} sabnzbd
+        ;;
+esac
 
 install_rar
 
@@ -74,13 +88,24 @@ WantedBy=multi-user.target
 SABSD
 
 systemctl enable -q --now sabnzbd 2>&1 | tee -a $log
-sleep 2
+
+for ((n = 0; n < 5; n++)); do
+    sleep 2
+    if [[ -f /home/${user}/.config/sabnzbd/sabnzbd.ini ]]; then
+        break
+    fi
+    if [[ $n == 4 ]]; then
+        echo_error "sabnzbd.ini does not seem to exist. Cleaning up and exiting."
+        bash /usr/local/bin/swizzin/scripts/remove/sabnzbd.sh
+        exit 1
+    fi
+done
 echo_progress_done "SABnzbd started"
 
 echo_progress_start "Configuring SABnzbd"
 systemctl stop sabnzbd >> ${log} 2>&1
 sed -i "s/host_whitelist = .*/host_whitelist = $(hostname -f), $(hostname)/g" /home/${user}/.config/sabnzbd/sabnzbd.ini
-sed -i "s|^host = .*|host = 0.0.0.0|g" /home/${user}/.config/sabnzbd/sabnzbd.ini
+#sed -i "s|^host = .*|host = 0.0.0.0|g" /home/${user}/.config/sabnzbd/sabnzbd.ini
 sed -i "0,/^port = /s/^port = .*/port = 65080/" /home/${user}/.config/sabnzbd/sabnzbd.ini
 sed -i "s|^download_dir = .*|download_dir = ~/Downloads/incomplete|g" /home/${user}/.config/sabnzbd/sabnzbd.ini
 sed -i "s|^complete_dir = .*|complete_dir = ~/Downloads/complete|g" /home/${user}/.config/sabnzbd/sabnzbd.ini
