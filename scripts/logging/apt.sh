393080883eeb6e9cc40d5fdbac44e8869b8de1cf
flying-sausages <flying_sausages@protonmail.com>
2021-01-24 12:52:21 +0100

    Add all sources to apt

--- scripts/logging/apt.sh
+++ scripts/logging/apt.sh
@@ -2,4 +2,6 @@
 
 paths+=('/var/log/apt/history.log')
 paths+=('/var/log/dpkg.log')
+paths+=('/etc/apt/sources.list')
+paths+=(/etc/apt/sources.list.d/*)
 version+=("$(apt --version | cut -d' ' -f2)")
