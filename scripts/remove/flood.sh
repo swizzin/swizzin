c83b2fa7c315b6ecece867be33897dd6e5fb87dc
Flying Sausages <flying_sausages@protonmail.com>
2021-01-27 00:59:18 +0100

    fix remover and prints

--- scripts/remove/flood.sh
+++ scripts/remove/flood.sh
@@ -4,12 +4,12 @@
 
 systemctl disable flood --now -q
 rm /etc/systemd/system/flood.service
-npm -g remove flood
+npm -g remove flood >> "$log" 2>&1
 
 userdel -rf flood
 
 if [ -f "/etc/nginx/apps/flood.conf" ]; then
-    rm /etc/nginx/apps/flood.config
+    rm /etc/nginx/apps/flood.conf
     systemctl reload nginx
 fi
 
