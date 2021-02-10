2515a499f93bbb2c584a3e3143d6a45862887819
liaralabs <liara@swizzin.ltd>
2021-02-10 13:01:05 -0800

    Reorder repo enable to work around a potential ubuntu issue

--- scripts/update/10-dependencies.sh
+++ scripts/update/10-dependencies.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 # Ensures that dependencies are installed and corrects them if that is not the case.
 
-#space-separated list of required GLOBAL SWIZZIN dependencies (NOT application specific ones)
-dependencies="whiptail git sudo curl wget lsof fail2ban apache2-utils vnstat tcl tcl-dev build-essential dirmngr apt-transport-https bc uuid-runtime jq net-tools fortune gnupg2 cracklib-runtime software-properties-common"
-
-missing=()
-for dep in $dependencies; do
-    if ! check_installed "$dep"; then
-        missing+=("$dep")
-    fi
-done
-
-if [[ ${missing[0]} != "" ]]; then
-    echo_info "Installing missing dependencies"
-    apt_install "${missing[@]}"
-else
-    echo_log_only "No dependencies required to install"
+if ! which add-apt-repository > /dev/null; then
+    apt_install software-properties-common # Ubuntu may require universe/mutliverse enabled for certain packages so we must ensure repos are enabled before deps are attempted to installed
 fi
 
 if [[ $(_os_distro) = "Ubuntu" ]]; then
@@ -24,7 +11,7 @@ if [[ $(_os_distro) = "Ubuntu" ]]; then
         add-apt-repository universe >> ${log} 2>&1
         trigger_apt_update=true
     fi
-    if ! grep -q 'universe' /etc/apt/sources.list; then
+    if ! grep -q 'multiverse' /etc/apt/sources.list; then
         echo_info "Enabling multiverse repo"
         add-apt-repository multiverse >> ${log} 2>&1
         trigger_apt_update=true
@@ -48,4 +35,21 @@ elif [[ $(_os_distro) == "Debian" ]]; then
 fi
 if [[ $trigger_apt_update == "true" ]]; then
     apt_update
+fi
+
+#space-separated list of required GLOBAL SWIZZIN dependencies (NOT application specific ones)
+dependencies="whiptail git sudo curl wget lsof fail2ban apache2-utils vnstat tcl tcl-dev build-essential dirmngr apt-transport-https bc uuid-runtime jq net-tools fortune gnupg2 cracklib-runtime"
+
+missing=()
+for dep in $dependencies; do
+    if ! check_installed "$dep"; then
+        missing+=("$dep")
+    fi
+done
+
+if [[ ${missing[0]} != "" ]]; then
+    echo_info "Installing missing dependencies"
+    apt_install "${missing[@]}"
+else
+    echo_log_only "No dependencies required to install"
 fi
