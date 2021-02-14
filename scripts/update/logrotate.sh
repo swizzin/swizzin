b3805b739c75d79f3b610429571642098d5bf211
Flying Sausages <flying_sausages@protonmail.com>
2021-02-14 19:38:10 +0100

    fix old logs moving to new location

--- scripts/update/logrotate.sh
+++ scripts/update/logrotate.sh
@@ -3,8 +3,10 @@
 #Install logrotate definition for swizzin if it does not exist
 if [ -d /root/logs ]; then
     echo_warn "Please note that swizzin logs are being permanently moved to /var/log/swizzin"
-    mv /root/logs/swizzin.log /root/logs/swizzin.log.1
-    mv /root/logs /var/log/swizzin
+    mv /root/logs/swizzin.log /root/logs/box.log.1
+    mv /root/logs/install.log /root/logs/setup.log
+    mv /root/logs/* /var/log/swizzin
+    rm -rf /root/logs
 fi
 
 if [ ! -e "/etc/logrotate.d/swizzin" ]; then
