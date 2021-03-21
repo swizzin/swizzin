5d8f1a38a3daef56fe665b027ea875be8d8d732a
Brett Petch <brettpetch@icloud.com>
2021-03-21 15:55:20 -0400

    removal script for requestrr

---
+++ scripts/remove/requestrr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+systemctl disable --now -q requestrr
+rm /etc/systemd/system/requestrr.service
+systemctl daemon-reload -q
+rm -rf /opt/requestrr
+
+if [[ -f /install/.nginx.lock ]]; then
+    rm /etc/nginx/apps/requestrr.conf
+    systemctl reload nginx
+fi
+
+rm /install/.requestrr.lock
