afbb9cf6639d1d80a32592fffd48945d975e47a5
Brett Petch <brettpetch@icloud.com>
2021-03-21 15:57:29 -0400

    forgot to delete user for requestrr.

--- scripts/remove/requestrr.sh
+++ scripts/remove/requestrr.sh
@@ -3,6 +3,7 @@ systemctl disable --now -q requestrr
 rm /etc/systemd/system/requestrr.service
 systemctl daemon-reload -q
 rm -rf /opt/requestrr
+userdel requestrr
 
 if [[ -f /install/.nginx.lock ]]; then
     rm /etc/nginx/apps/requestrr.conf
