5651853e47497553ff58cbac30bc05d10e276cef
liaralabs <liara@swizzin.ltd>
2021-03-24 13:51:14 -0700

    Updates to removal flow

--- scripts/remove/qbittorrent.sh
+++ scripts/remove/qbittorrent.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 . /etc/swizzin/sources/functions/utils
+. /etc/swizzin/sources/functions/libtorrent
+
+check_libtorrent_rasterbar_method
 
 users=($(_get_user_list))
 for user in ${users[@]}; do
@@ -10,6 +13,11 @@ rm /etc/nginx/apps/qbittorrent.conf
 rm /etc/nginx/conf.d/*.qbittorrent.conf
 rm /etc/systemd/system/qbittorrent@.service
 dpkg -r qbittorrent-nox > /dev/null 2>&1
+dpkg -r libtorrent-rasterbar > /dev/null 2>&1
+
+if [[ ! -f /install/.deluge.lock ]]; then
+    apt_remove --purge ^libtorrent-rasterbar* python-libtorrent python3-libtorrent
+fi
 
 if dpkg -s qtbase5-swizzin > /dev/null 2>&1; then
     dpkg -r qtbase5-swizzin > /dev/null 2>&1
@@ -20,7 +28,3 @@ fi
 
 systemctl reload nginx
 rm /install/.qbittorrent.lock
-
-if [[ ! -f /install/.deluge.lock ]]; then
-    bash /etc/swizzin/scripts/remove/libtorrent.sh
-fi
