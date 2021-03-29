c3943807af7adc3b21f59e507d464d9f6bd3a13f
Flying Sausages <flying_sausages@protonmail.com>
2021-03-29 20:40:39 +0200

    Move transmission logs out of syslog

---
+++ scripts/update/transmission.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -f /install/.transmission.lock ]; then
+
+    if ! grep -q /home/systemd/system/transmission@.service "--logfile"; then
+        echo_progress_start "Moving transmission's logs to a file instead of syslog"
+        : #switch service to logfile
+        sed "s/ExecStart=.*/ExecStart=/usr/bin/transmission-daemon -f --log-error --logfile /home/%i/.config/transmission-daemon/transmission.log/g" -i
+        systemctl daemon-reload
+        readarray -t users < <(_get_user_list)
+        for user in "${users[@]}"; do
+            systemctl restart transmission@"$user"
+        done
+
+    fi
+fi
