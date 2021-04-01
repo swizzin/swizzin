c1bc3b50ad0624d5f0bca8f5136464a2412cd8f9
flying-sausages <flying_sausages@protonmail.com>
2021-04-01 15:51:02 +0200

    Update scripts/update/transmission.sh
    Co-authored-by: liaralabs <30909703+liaralabs@users.noreply.github.com>

--- scripts/update/transmission.sh
+++ scripts/update/transmission.sh
@@ -4,7 +4,7 @@ if [ -f /install/.transmission.lock ]; then
 
     if ! grep -q "\-\-logfile" /etc/systemd/system/transmission@.service ; then
         echo_progress_start "Moving transmission's logs to a file instead of syslog"
-        sed "s/ExecStart=.*/ExecStart=/usr/bin/transmission-daemon -f --log-error --logfile /home/%i/.config/transmission-daemon/transmission.log/g" -i
+        sed "s|ExecStart=.*|ExecStart=/usr/bin/transmission-daemon -f --log-error --logfile /home/%i/.config/transmission-daemon/transmission.log|g" -i
         systemctl daemon-reload
         readarray -t users < <(_get_user_list)
         for user in "${users[@]}"; do
