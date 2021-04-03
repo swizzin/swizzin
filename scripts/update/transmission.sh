04d61f0bda179987c14a705673305e9dd6ad5a8c
Flying Sausages <flying_sausages@protonmail.com>
2021-04-03 20:41:53 +0200

    fix lint

--- scripts/update/transmission.sh
+++ scripts/update/transmission.sh
@@ -2,7 +2,7 @@
 
 if [ -f /install/.transmission.lock ]; then
 
-    if ! grep -q "\-\-logfile" /etc/systemd/system/transmission@.service ; then
+    if ! grep -q "\-\-logfile" /etc/systemd/system/transmission@.service; then
         echo_progress_start "Moving transmission's logs to a file instead of syslog"
         sed "s|ExecStart=.*|ExecStart=/usr/bin/transmission-daemon -f --log-error --logfile /home/%i/.config/transmission-daemon/transmission.log|g" -i
         systemctl daemon-reload
