cf6307734a674d40a4810ac18cfc9d935084c097
Flying Sausages <flying_sausages@protonmail.com>
2021-04-11 10:55:41 +0200

    feat(lidarr): updater for netcore

--- scripts/update/lidarr.sh
+++ scripts/update/lidarr.sh
@@ -1,22 +1,48 @@
 #!/bin/bash
 if [[ -f /install/.lidarr.lock ]]; then
 
-    #Move old homedirectory installations to opt
+    #Move old homedirectory installations to opt and switch to netcore
     user=$(cut -d: -f1 < /root/.master.info)
     if [[ -d /home/$user/Lidarr ]]; then
-        echo_info "Moving Lidarr instllation to opt"
+        echo_info "Moving Lidarr instllation to opt and switching it to net-core"
         wasActive=$(systemctl is-active lidarr)
         systemctl stop lidarr
 
-        mv /home/$user/Lidarr /opt/Lidarr
-        sudo chown -R $user:$user /opt/Lidarr
+        mv /home/"$user"/Lidarr /opt/Lidarr
 
-        sed -i "/ExecStart/c\ExecStart=/usr/bin/mono /opt/Lidarr/Lidarr.exe -nobrowser" /etc/systemd/system/lidarr.service
+        echo_progress_start "Downloading source files"
+        case "$(_os_arch)" in
+            "amd64") dlurl="https://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64" ;;
+            "armhf") dlurl="https://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=arm" ;;
+            "arm64") dlurl="https://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=arm64" ;;
+            *)
+                echo_error "Arch not supported"
+                exit 1
+                ;;
+        esac
+
+        if ! curl "$dlurl" -L -o /tmp/lidarr.tar.gz >> "$log" 2>&1; then
+            echo_error "Download failed, exiting"
+            exit 1
+        fi
+        echo_progress_done "Source downloaded"
+
+        echo_progress_start "Extracting source"
+        tar xfv /tmp/lidarr.tar.gz --directory /opt/ >> $log 2>&1
+        rm -rf /tmp/lidarr.tar.gz
+        chown -R "${user}": /opt/Lidarr
+        echo_progress_done "Source extracted"
+
+        sudo chown -R "$user":"$user" /opt/Lidarr
+
+        sed -i "/ExecStart/c\ExecStart=/opt/Lidarr/Lidarr.exe -nobrowser" /etc/systemd/system/lidarr.service
         sed -i "/ExecStop/d" /etc/systemd/system/lidarr.service
         systemctl daemon-reload
 
         if [[ $wasActive = "active" ]]; then
             systemctl start lidarr
         fi
+
     fi
+
 fi
