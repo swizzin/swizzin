1fcb0e1ce7226a4532271f46f4c387fd8c19e76c
Flying Sausages <flying_sausages@protonmail.com>
2021-04-11 11:04:29 +0200

    refactor(lidarr): use functions, removing mono deps

--- scripts/install/lidarr.sh
+++ scripts/install/lidarr.sh
@@ -10,42 +10,40 @@
 #   including (via compiler) GPL-licensed code must also be made available
 #   under the GPL along with build & install instructions.
 
-user=$(cut -d: -f1 < /root/.master.info)
-distribution=$(lsb_release -is)
-version=$(lsb_release -cs)
-#shellcheck source=sources/functions/mono
-. /etc/swizzin/sources/functions/mono
-mono_repo_setup
-apt_install libmono-cil-dev libchromaprint-tools
-apt_install curl mediainfo sqlite3 chromaprint
+install() {
+    user=$(_get_master_username)
+    #shellcheck source=sources/functions/mono
+    apt_install curl mediainfo sqlite3 chromaprint
 
-echo_progress_start "Fetching Lidarr"
-echo_progress_start "Downloading source files"
-case "$(_os_arch)" in
-    "amd64") dlurl="https://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64" ;;
-    "armhf") dlurl="https://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=arm" ;;
-    "arm64") dlurl="https://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=arm64" ;;
-    *)
-        echo_error "Arch not supported"
+    echo_progress_start "Downloading source files"
+    case "$(_os_arch)" in
+        "amd64") dlurl="https://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64" ;;
+        "armhf") dlurl="https://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=arm" ;;
+        "arm64") dlurl="https://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=arm64" ;;
+        *)
+            echo_error "Arch not supported"
+            exit 1
+            ;;
+    esac
+
+    if ! curl "$dlurl" -L -o /tmp/lidarr.tar.gz >> "$log" 2>&1; then
+        echo_error "Download failed, exiting"
         exit 1
-        ;;
-esac
+    fi
+    echo_progress_done "Source downloaded"
 
-if ! curl "$dlurl" -L -o /tmp/lidarr.tar.gz >> "$log" 2>&1; then
-    echo_error "Download failed, exiting"
-    exit 1
-fi
-echo_progress_done "Source downloaded"
+    echo_progress_start "Extracting source"
+    tar xfv /tmp/lidarr.tar.gz --directory /opt/ >> $log 2>&1
+    rm -rf /tmp/lidarr.tar.gz
+    chown -R "${user}": /opt/Lidarr
+    echo_progress_done "Source extracted"
+}
 
-echo_progress_start "Extracting source"
-tar xfv /tmp/lidarr.tar.gz --directory /opt/ >> $log 2>&1
-rm -rf /tmp/lidarr.tar.gz
-chown -R "${user}": /opt/Lidarr
-echo_progress_done "Source extracted"
+config() {
 
-echo_progress_start "Creating configuration and service files"
-if [[ ! -d /home/${user}/.config/Lidarr/ ]]; then mkdir -p "/home/${user}/.config/Lidarr/"; fi
-cat > "/home/${user}/.config/Lidarr/config.xml" << LID
+    echo_progress_start "Creating configuration and service files"
+    if [[ ! -d /home/${user}/.config/Lidarr/ ]]; then mkdir -p "/home/${user}/.config/Lidarr/"; fi
+    cat > "/home/${user}/.config/Lidarr/config.xml" << LID
 <Config>
   <Port>8686</Port>
   <UrlBase>lidarr</UrlBase>
@@ -55,8 +53,10 @@ cat > "/home/${user}/.config/Lidarr/config.xml" << LID
   <LaunchBrowser>False</LaunchBrowser>
 </Config>
 LID
-chown -R "${user}": "/home/${user}/.config"
-cat > /etc/systemd/system/lidarr.service << LID
+    chown -R "${user}": "/home/${user}/.config"
+}
+systemd() {
+    cat > /etc/systemd/system/lidarr.service << LID
 [Unit]
 Description=Lidarr
 After=syslog.target network.target
@@ -73,15 +73,23 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.target
 LID
-echo_progress_done "Services configured"
+    echo_progress_done "Services configured"
+}
 
-if [[ -f /install/.nginx.lock ]]; then
-    echo_progress_start "Configuring nginx"
-    sleep 10
-    bash /usr/local/bin/swizzin/nginx/lidarr.sh
-    systemctl reload nginx
-    echo_progress_done "Nginx configured"
-fi
+nginx() {
+    if [[ -f /install/.nginx.lock ]]; then
+        echo_progress_start "Configuring nginx"
+        sleep 10
+        bash /usr/local/bin/swizzin/nginx/lidarr.sh
+        systemctl reload nginx
+        echo_progress_done "Nginx configured"
+    fi
+}
+
+install
+config
+systemd
+nginx
 
 echo_progress_start "Enabling auto-start and executing Lidarr"
 systemctl enable -q --now lidarr
