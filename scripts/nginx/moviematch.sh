684ddb25c734b63c96a39197593f672e389ca8d5
Flying Sausages <flying_sausages@protonmail.com>
2021-04-12 22:13:44 +0200

    fix(mmatch): change to new conf format

--- scripts/nginx/moviematch.sh
+++ scripts/nginx/moviematch.sh
@@ -5,9 +5,9 @@ cat > /etc/nginx/apps/moviematch.conf << NGINX
         proxy_pass http://127.0.0.1:8420/;
         proxy_set_header Upgrade \$http_upgrade;
     }
-
 NGINX
 
-echo "ROOT_PATH=/moviematch" >> /opt/moviematch/.env
-
+echo "host: 0.0.0.0
+basePath: /moviematch" >> /opt/moviematch/config.yaml
+systemctl daemon-reload
 systemctl try-restart moviematch -q
