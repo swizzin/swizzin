3a0d8ec9b42a88e805c0b2bfaa54032bed5e84eb
userdocs <16525024+userdocs@users.noreply.github.com>
2021-04-12 22:49:57 +0100

    Update scripts/update/completion.sh
    Co-authored-by: flying-sausages <flying_sausages@protonmail.com>

--- scripts/update/completion.sh
+++ scripts/update/completion.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #
 # Set the correct permissions for the completion file and then create a symlink to the bash completion file.
-chmod 644 '/etc/swizzin/sources/bash_completion.d/swizzin'
-ln -fs '/etc/swizzin/sources/bash_completion.d/swizzin' '/etc/bash_completion.d/swizzin'
+if [ ! -e /etc/bash_completion.d/swizzin ]; then
+    chmod 644 '/etc/swizzin/sources/bash_completion.d/swizzin'
+    ln -s '/etc/swizzin/sources/bash_completion.d/swizzin' '/etc/bash_completion.d/swizzin'
+    echo_info "Shell completions for the box command have been installed. They will apply when your shell is reloaded, then try pressing <tab> when you type 'box'." 
+fi
