9d9ebb2d80bd0e296e0973fb204c3a92a36474c2
userdocs <16525024+userdocs@users.noreply.github.com>
2021-04-12 22:53:08 +0100

    shellcheck

--- scripts/update/completion.sh
+++ scripts/update/completion.sh
@@ -4,5 +4,5 @@
 if [ ! -e /etc/bash_completion.d/swizzin ]; then
     chmod 644 '/etc/swizzin/sources/bash_completion.d/swizzin'
     ln -s '/etc/swizzin/sources/bash_completion.d/swizzin' '/etc/bash_completion.d/swizzin'
-    echo_info "Shell completions for the box command have been installed. They will apply when your shell is reloaded, then try pressing <tab> when you type 'box'." 
+    echo_info "Shell completions for the box command have been installed. They will apply when your shell is reloaded, then try pressing <tab> when you type 'box'."
 fi
