07c1ce7657fc200759247cb7bdc68dd61b60aab1
flying-sausages <flying_sausages@protonmail.com>
2021-04-13 00:39:09 +0200

    Revert "feat: bash completion (#626)" (#628)
    This reverts commit f94a479fe7bd480bf2a41ca57f380d7d71686ad9.

--- scripts/update/completion.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Set the correct permissions for the completion file and then create a symlink to the bash completion file.
-if [ ! -e /etc/bash_completion.d/swizzin ]; then
-    chmod 644 '/etc/swizzin/sources/bash_completion.d/swizzin'
-    ln -s '/etc/swizzin/sources/bash_completion.d/swizzin' '/etc/bash_completion.d/swizzin'
-    echo_info "Shell completions for the box command have been installed. They will apply when your shell is reloaded, then try pressing <tab> when you type 'box'."
-fi
