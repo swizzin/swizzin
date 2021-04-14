9cd4cf1700c3a665b7335565547b5ff917ac14c5
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-04-14 11:05:21 -0500

    capital True

--- scripts/update/radarr.sh
+++ scripts/update/radarr.sh
@@ -56,7 +56,7 @@ if [[ -f /install/.radarr.lock ]]; then
     sslenabled=$(grep -oPm1 "(?<=<EnableSsl>)[^<]+" /home/"$radarrOwner"/.config/Radarr/config.xml)
     if [[ "$sslport" = "8787" ]]; then
         sed -i "s|<SslPort\>port\=8787/|<SslPort\>port\=9898|g" /home/"$radarrOwner"/.config/Radarr/config.xml
-        if [[ "$sslenabled" = "true" ]]; then
+        if [[ "$sslenabled" = "True" ]]; then
             echo "Radarr SSL port changed from 8787 to 9898 due to Readarr conflicts; please ensure to adjust your dependent systems in case they were using this port"
         fi
     fi
