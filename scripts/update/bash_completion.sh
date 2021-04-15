3e96563c0d6a63063219138e75b69740cdad05e8
userdocs <16525024+userdocs@users.noreply.github.com>
2021-04-15 17:08:56 +0100

    use swizdb

--- scripts/update/bash_completion.sh
+++ scripts/update/bash_completion.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 #
-# Create the symlink /etc/bash_completion.d/swizzin if it does not exist and output a helpful echo
-if [[ ! -e /etc/bash_completion.d/swizzin ]]; then
-    ln -s '/etc/swizzin/sources/bash_completion/swizzin' '/etc/bash_completion.d/swizzin'
+# Create the symlink /etc/bash_completion.d/box if it does not exist and output a helpful echo
+if [[ ! -e /etc/bash_completion.d/box ]]; then
+    ln -s '/etc/swizzin/sources/bash_completion' '/etc/bash_completion.d/box'
     echo_info "Shell completions for the box command have been installed. They will be applied the next time you log into a bash shell"
 fi
+
 # Create our apps and upgrade lists for the completion arrays. This is fired everytime this script is run to make sure the lists are current.
-find "/etc/swizzin/scripts/install/" -type f -exec basename {} \; | sort > /etc/swizzin/sources/bash_completion/completion.apps
-find "/etc/swizzin/scripts/upgrade/" -type f -exec basename {} \; | sort > /etc/swizzin/sources/bash_completion/completion.upgrade
-# Add all files in /etc/swizzin/sources/bash_completion to the bash_completion_perms array to process in our for below
-readarray -t bash_completion_perms < <(find "/etc/swizzin/sources/bash_completion" -type f -exec basename {} \;)
-# Check to make sure the directory /etc/swizzin/sources/bash_completion is set to 755 otherwise chmod it to 755
-if [[ $(stat -c '%A' /etc/swizzin/sources/bash_completion) != 'drwxr-xr-x' ]]; then
-    chmod 755 '/etc/swizzin/sources/bash_completion'
+swizdb set bash_completion/apps.list "$(find "/etc/swizzin/scripts/install/" -type f -exec basename {} \; | sort)"
+swizdb set bash_completion/upgrade.list "$(find "/etc/swizzin/scripts/upgrade/" -type f -exec basename {} \; | sort)"
+
+# Check to make sure the completion file /etc/swizzin/sources/bash_completion is set to 644
+if [[ $(stat -c '%A' /etc/swizzin/sources/bash_completion) != '-rw-r--r--' ]]; then
+    chmod 644 '/etc/swizzin/sources/bash_completion'
 fi
-# Using a for and the bash_completion_perms array check through all files in the /etc/swizzin/sources/bash_completion have 644 perms set otherwise do nothing
+
+# Check to make sure the directory /var/lib/swizzin/db/bash_completion is set to 755
+if [[ $(stat -c '%A' /var/lib/swizzin/db/bash_completion) != 'drwxr-xr-x' ]]; then
+    chmod 755 '/var/lib/swizzin/db/bash_completion'
+fi
+
+# Add all files in /var/lib/swizzin/db/bash_completion to the bash_completion_perms array to process in our for below
+readarray -t bash_completion_perms < <(find "/var/lib/swizzin/db/bash_completion" -type f -exec basename {} \;)
+# Using a for and the bash_completion_perms array check through all files in the /var/lib/swizzin/db/bash_completion have 644 perms set otherwise do nothing
 for file_names in "${bash_completion_perms[@]}"; do
-    if [[ "$(stat -c '%A' "/etc/swizzin/sources/bash_completion/${file_names}")" != '-rw-r--r--' ]]; then
-        chmod 644 "/etc/swizzin/sources/bash_completion/${file_names}"
+    if [[ "$(stat -c '%A' "/var/lib/swizzin/db/bash_completion/${file_names}")" != '-rw-r--r--' ]]; then
+        chmod 644 "/var/lib/swizzin/db/bash_completion/${file_names}"
     fi
 done
