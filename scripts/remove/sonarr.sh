6eb78d64d261cea22604063a8a120c3b1a0f8e4f
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-04-14 19:49:39 -0500

    ask if we should purge; don't purge by default

--- scripts/remove/sonarr.sh
+++ scripts/remove/sonarr.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-apt_remove --purge sonarr
-
-rm -rf /var/lib/sonarr
-rm -rf /usr/lib/sonarr
-
+if ask "Would you like to purge the configuration?" Y; then
+    apt_remove --purge sonarr
+    rm -rf /var/lib/sonarr
+    rm -rf /usr/lib/sonarr
+else
+    apt_remove sonarr
+fi
 if [[ -f /install/.nginx.lock ]]; then
     rm /etc/nginx/apps/sonarr.conf
     systemctl reload nginx >> "$log" 2>&1
