0cb39f67aecee293e383f5eee059191b08dd9379
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-04-24 12:11:27 -0500

    Notify v2 is EOL and should be upgraded Fixes #596

--- scripts/update/sonarr.sh
+++ scripts/update/sonarr.sh
@@ -2,6 +2,7 @@
 
 if dpkg -l | grep nzbdrone > /dev/null 2>&1; then
     v2present=true
+    echo_warn "Sonarr v2 is obsolete and end-of-life. Please upgrade your Sonarr to v3 using \`box upgrade sonarr\`."
 fi
 if [[ -f /install/.sonarr.lock ]] && [[ $v2present == "true" ]]; then
     #update lock file
