6f41292a77b14ae3b1629b7e8cb434c4f9b52ec3
Flying Sausages <flying_sausages@protonmail.com>
2021-04-24 20:02:43 +0200

    fix(sonarr): add echos to updater

--- scripts/update/sonarr.sh
+++ scripts/update/sonarr.sh
@@ -5,11 +5,13 @@ if dpkg -l | grep nzbdrone > /dev/null 2>&1; then
     echo_warn "Sonarr v2 is obsolete and end-of-life. Please upgrade your Sonarr to v3 using \`box upgrade sonarr\`."
 fi
 if [[ -f /install/.sonarr.lock ]] && [[ $v2present == "true" ]]; then
+    echo_info "box package sonarr is being renamed to sonarrv2old"
     #update lock file
     rm /install/.sonarr.lock
     touch /install/.sonarrv2old.lock
 fi
 if [[ -f /install/.sonarrv3.lock ]]; then
+    echo_info "box package sonarrv3 is being renamed to sonarr as it has been released as stable"
     #upgrade sonarr v3 lock
     rm /install/.sonarrv3.lock
     touch /install/.sonarr.lock
