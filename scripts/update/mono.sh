6f52f2ac6b7afac308e4a7583713d30415d4d2b4
Flying Sausages <flying_sausages@protonmail.com>
2021-04-24 16:18:04 +0200

    remove lidarr from mono script as it ain't mono no mo'

--- scripts/update/mono.sh
+++ scripts/update/mono.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Have I mentioned I hate mono?
 
-if [[ -f /install/.sonarr.lock ]] || [[ -f /install/.radarr.lock ]] || [[ -f /install/.lidarr.lock ]]; then
+if [[ -f /install/.sonarr.lock ]] || [[ -f /install/.radarr.lock ]]; then
     version=$(lsb_release -cs)
     distro=$(lsb_release -is)
     master=$(cut -d: -f1 < /root/.master.info)
