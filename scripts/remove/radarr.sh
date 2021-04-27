070257bed5c4e39fa7bcbfc592e9b85fdb4c475d
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-04-27 17:03:25 -0500

    add echos and logging to remove script

--- scripts/remove/radarr.sh
+++ scripts/remove/radarr.sh
@@ -5,6 +5,7 @@ servicename="radarr"
 nginxname="radarr"
 lockname="radarr"
 appname_lower="radarr"
+appname_camel="Radarr"
 servicefile="/etc/systemd/system/$servicename.service"
 appdir="/opt/Radarr"
 
@@ -13,25 +14,38 @@ if ! user="$(swizdb get $appname_lower/owner)"; then
 fi
 
 appdatadir="/home/$user/.config/Radarr/"
+# this must be set AFTER we get the user
 
 if ask "Would you like to purge the configuration?" Y; then
     purgeapp="True"
+    echo_info "Application Data Directory to delete & purge detected as $appdatadir"
 else
     purgeapp="False"
 fi
+echo_progress_start "Removing $appname_camel..."
 
+echo_progress_start "Disabling $servicename service and removing file"
 systemctl disable --now -q "$servicename"
 rm "$servicefile"
 systemctl daemon-reload -q
+echo_log_only "Disabled service $servicename and removed service file $servicefile"
+echo_progress_done "Service disabled and removed"
+
+echo_progress_start "Removing application files"
 rm -rf "$appdir"
+echo_progress_done "Application files removed"
 
 if [[ "$purgeapp" = "True" ]]; then
     rm -rf "$appdatadir"
+    echo_info "Application data purged"
 fi
 
 if [[ -f /install/.nginx.lock ]]; then
+    echo_progress_start "Removing nginx conf"
     rm "/etc/nginx/apps/$nginxname.conf"
     systemctl reload nginx >> "$log" 2>&1
+    echo_progress_done "Nginx conf removed"
 fi
 
 rm "/install/.$lockname.lock"
+echo_progress_done "$appname_camel removed sucessfully"
