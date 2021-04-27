c13f58a2afc480253fe20b6b1962823d94c9cee1
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-04-27 15:34:54 -0500

    refactor radarr removal and add variables New: Purge option for Radarr

--- scripts/remove/radarr.sh
+++ scripts/remove/radarr.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-systemctl disable --now -q radarr
-rm /etc/systemd/system/radarr.service
-systemctl daemon-reload -q
-rm -rf /opt/Radarr
 
-if [[ -f /install/.nginx.lock ]]; then
-    rm /etc/nginx/apps/radarr.conf
-    systemctl reload nginx
+servicename="radarr"
+nginxname="radarr"
+lockname="radarr"
+servicefile="/etc/systemd/system/$servicename.service"
+appdir="/opt/Radarr"
+appdatadir="/home/$user/.config/Radarr/"
+
+if ask "Would you like to purge the configuration?" Y; then
+    purgeapp="True"
+else
+    purgeapp="False"
 fi
 
-rm /install/.radarr.lock
+systemctl disable --now -q "$servicename"
+rm "$servicefile"
+systemctl daemon-reload -q
+rm -rf "$appdir"
+
+if [[ "$purgeapp" = "True" ]]; then
+    rm -rf "$appdatadir"
+fi
+
+if [[ -f /install/.nginx.lock ]]; then
+    rm "/etc/nginx/apps/$nginxname.conf"
+    systemctl reload nginx >> "$log" 2>&1
+fi
+
+rm "/install/.$lockname.lock"
