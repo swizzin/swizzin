b313de0eab2708528ae70a4b9dc3fc61c22b9fc7
flying-sausages <flying_sausages@protonmail.com>
2021-04-27 21:06:48 +0200

    Update scripts/update/radarr.sh

--- scripts/update/radarr.sh
+++ scripts/update/radarr.sh
@@ -68,6 +68,7 @@ if [[ -f /install/.radarr.lock ]]; then
     sslport=$(grep -oPm1 "(?<=<SslPort>)[^<]+" /home/"$radarrOwner"/.config/Radarr/config.xml)
     sslenabled=$(grep -oPm1 "(?<=<EnableSsl>)[^<]+" /home/"$radarrOwner"/.config/Radarr/config.xml)
     if [[ "$sslport" = "8787" ]]; then
+        echo_log_only "Changing radarr ssl port"
         sed -i 's|<SslPort>8787</SslPort>|<SslPort>9898</SslPort>|g' "/home/$radarrOwner/.config/Radarr/config.xml"
         if [[ "$sslenabled" = "True" ]]; then
             echo "Radarr SSL port changed from 8787 to 9898 due to Readarr conflicts; please ensure to adjust your dependent systems in case they were using this port"
