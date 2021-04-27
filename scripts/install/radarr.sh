ee759420a8b7b5d07a136c7302b2af30f30a74e9
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-04-27 15:46:50 -0500

    refactor radarr installer
    add swizdb support

--- scripts/install/radarr.sh
+++ scripts/install/radarr.sh
@@ -1,62 +1,86 @@
 #!/bin/bash
-# radarr v3 installer
-# Flying sauasges for swizzin 2020
+# *Arr Installer for Radarr
+# Refactored from existing files by FlyingSausages and others
+# Bakerboy448 2021 for Swizzin
 
 #shellcheck source=sources/functions/utils
 . /etc/swizzin/sources/functions/utils
 
+if [ -z "$RADARR_OWNER" ]; then
+    if ! RADARR_OWNER="$(swizdb get radarr/owner)"; then
+        RADARR_OWNER=$(_get_master_username)
+        echo_info "Setting Radarr owner = $RADARR_OWNER"
+        swizdb set "radarr/owner" "$RADARR_OWNER"
+    fi
+else
+    echo_info "Setting Radarr owner = $RADARR_OWNER"
+    swizdb set "radarr/owner" "$RADARR_OWNER"
+fi
+
 _install_radarr() {
+    user="$RADARR_OWNER"
+    appname_lower="radarr"
+    appname_camel="Radarr"
+    appconfigdir="/home/$user/.config/$appname_camel"
+
     apt_install curl mediainfo sqlite3
 
-    radarrConfDir="/home/$radarrOwner/.config/Radarr"
-    mkdir -p "$radarrConfDir"
-    chown -R "$radarrOwner":"$radarrOwner" /home/$radarrOwner/.config
+    if [ ! -d "$appconfigdir" ]; then
+        mkdir -p "$appconfigdir"
+    fi
+    chown -R "$user":"$user" "$appconfigdir"
 
     echo_progress_start "Downloading release archive"
+
+    urlbase="https://$appname_lower.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore"
     case "$(_os_arch)" in
-        "amd64") dlurl="https://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64" ;;
-        "armhf") dlurl="https://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=arm" ;;
-        "arm64") dlurl="https://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=arm64" ;;
+        "amd64") dlurl="${urlbase}&arch=x64" ;;
+        "armhf") dlurl="${urlbase}&arch=arm" ;;
+        "arm64") dlurl="${urlbase}&arch=arm64" ;;
         *)
             echo_error "Arch not supported"
             exit 1
             ;;
     esac
 
-    if ! curl "$dlurl" -L -o /tmp/Radarr.tar.gz >> "$log" 2>&1; then
+    if ! curl "$dlurl" -L -o "/tmp/$appname_lower.tar.gz" >> "$log" 2>&1; then
         echo_error "Download failed, exiting"
         exit 1
     fi
     echo_progress_done "Archive downloaded"
 
     echo_progress_start "Extracting archive"
-    tar -xvf /tmp/Radarr.tar.gz -C /opt >> "$log" 2>&1
+    tar xfv "/tmp/$appname_lower.tar.gz" --directory /opt/ >> "$log" 2>&1 || {
+        echo_error "Failed to extract"
+        exit 1
+    }
+    rm -rf "/tmp/$appname_lower.tar.gz"
+    appdir="/opt/$appname_camel"
+    chown -R "${user}": "$appdir"
     echo_progress_done "Archive extracted"
 
-    touch /install/.radarr.lock
-
     echo_progress_start "Installing Systemd service"
-    cat > /etc/systemd/system/radarr.service << EOF
+    cat > "/etc/systemd/system/$appname_lower.service" << EOF
 [Unit]
-Description=Radarr Daemon
+Description=$appname_camel Daemon
 After=syslog.target network.target
 
 [Service]
 # Change the user and group variables here.
-User=${radarrOwner}
-Group=${radarrOwner}
+User=${user}
+Group=${user}
 
 Type=simple
 
 # Change the path to Radarr here if it is in a different location for you.
-ExecStart=/opt/Radarr/Radarr -nobrowser -data=/home/$radarrOwner/.config/Radarr/
+ExecStart=$appdir/Radarr -nobrowser -data=$appconfigdir
 TimeoutStopSec=20
 KillMode=process
 Restart=on-failure
 
 # These lines optionally isolate (sandbox) Radarr from the rest of the system.
 # Make sure to add any paths it might use to the list below (space-separated).
-#ReadWritePaths=/opt/Radarr /path/to/movies/folder
+#ReadWritePaths=$appdir /path/to/movies/folder
 #ProtectSystem=strict
 #PrivateDevices=true
 #ProtectHome=true
@@ -64,41 +88,32 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.target
 EOF
-    chown -R "$radarrOwner":"$radarrOwner" /opt/Radarr
-    systemctl -q daemon-reload
-    systemctl enable --now -q radarr
-    sleep 1
-    echo_progress_done "Radarr service installed and enabled"
 
-    if [[ -f $radarrConfDir/update_required ]]; then
-        echo_progress_start "Radarr is installing an internal upgrade..."
-        # echo "You can track the update by running \`systemctl status radarr\`0. in another shell."
-        # echo "In case of errors, please press CTRL+C and run \`box remove radarr\` in this shell and check in with us in the Discord"
-        while [[ -f $radarrConfDir/update_required ]]; do
-            sleep 1
-            echo_log_only "Upgrade file is still here"
-        done
-        echo_progress_done "Upgrade finished"
+    systemctl -q daemon-reload
+    systemctl enable --now -q "$appname_lower"
+    sleep 1
+    echo_progress_done "$appname_camel service installed and enabled"
+
+    echo_progress_start "$appname_camel is installing an internal upgrade..."
+    if ! timeout 30 bash -c -- "while ! curl -sIL http://127.0.0.1:7878 >> \"$log\" 2>&1; do sleep 2; done"; then
+        echo_error "The $appname_camel web server has taken longer than 30 seconds to start."
+        exit 1
     fi
+    echo_progress_done "Internal upgrade finished"
 
 }
 
 _nginx_radarr() {
     if [[ -f /install/.nginx.lock ]]; then
-        echo_progress_start "Installing nginx configuration"
-        #TODO what is this sleep here for? See if this can be fixed by doing a check for whatever it needs to
+        echo_progress_start "Configuring nginx"
         sleep 10
         bash /usr/local/bin/swizzin/nginx/radarr.sh
-        systemctl -q reload nginx
+        systemctl reload nginx
         echo_progress_done "Nginx configured"
     else
-        echo_info "Radarr will be available on port 7878. Secure your installation manually through the web interface."
+        echo_info "$appname_camel will run on port 7878"
     fi
 }
-
-if [[ -z $radarrOwner ]]; then
-    radarrOwner=$(_get_master_username)
-fi
 
 _install_radarr
 _nginx_radarr
@@ -115,4 +130,5 @@ if [[ -f /install/.bazarr.lock ]]; then
     echo_info "Please adjust your Bazarr setup accordingly"
 fi
 
-echo_success "Radarr v3 installed"
+touch "/install/.$appname_lower.lock"
+echo_success "$appname_camel installed"
