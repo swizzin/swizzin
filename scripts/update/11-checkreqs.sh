3bbd9c0bfd0c803317df0e489b9132742e825420
Flying Sausages <flying_sausages@protonmail.com>
2021-04-28 21:56:51 +0200

    feat: abstract repetitive code to function

--- scripts/update/11-checkreqs.sh
+++ scripts/update/11-checkreqs.sh
@@ -5,42 +5,29 @@
 # DO NOT SOURCE GLOBALS HERE, THAT DEFEATS THE WHOLE PURPOSE OF THIS CHECK
 # SOURCING GLOBALS HERE WILL MAKE THESE TESTS PASS BUT THE FUNCTIONS WON'T BE AVAILABLE IN THE SHELL ABOVE
 
-if ! command -v apt_install > /dev/null 2>&1; then
+killage() {
     echo
     echo "Due to internal restructuring please run \`box update\` again. You should only have to do this once."
-    echo "Reason: Apt functions unavailable"
+    echo "Reason: $1"
     kill -13 $(ps --pid $$ -oppid=)
     exit 1
+}
+if ! command -v apt_install > /dev/null 2>&1; then
+    killage "Apt functions unavailable"
 fi
 
 if ! command -v echo_error > /dev/null 2>&1; then
-    echo
-    echo "Due to internal restructuring please run \`box update\` again. You should only have to do this once."
-    echo "Reason: Echo functions unavailable"
-    kill -13 $(ps --pid $$ -oppid=)
-    exit 1
+    killage "Echo functions unavailable"
 fi
 
 if [[ -z $log ]]; then
-    echo
-    echo "Due to internal restructuring please run \`box update\` again. You should only have to do this once."
-    echo "Reason: log not set"
-    kill -13 $(ps --pid $$ -oppid=)
-    exit 1
+    killage "Log variable not set"
 fi
 
 if ! swizdb list > /dev/null 2>&1; then
-    echo
-    echo "Due to internal restructuring please run \`box update\` again. You should only have to do this once."
-    echo "Reason: swizdb list command unavailable"
-    kill -13 $(ps --pid $$ -oppid=)
-    exit 1
+    killage "SwizDB list command unavailable"
 fi
 
 if [[ -z $SWIZ_REPO_SCRIPT_RAN ]]; then
-    echo
-    echo "Due to internal restructuring please run \`box update\` again. You should only have to do this once."
-    echo "Reason: Updating procedure restructured"
-    kill -13 $(ps --pid $$ -oppid=)
-    exit 1
+    killage "Updating procedure restructured"
 fi
