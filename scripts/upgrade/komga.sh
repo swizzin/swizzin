e4fc68d0f383c5e9ad39d4f309536a7b22cab7b5
Flying Sausages <flying_sausages@protonmail.com>
2021-05-01 20:23:23 +0200

    feat(komga): upgrader

---
+++ scripts/upgrade/komga.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ ! -f /install/.komga.install ]; then
+    echo_error "You ain't got no komga, whacha doen"
+    exit 1
+fi
+
+user=$(_get_master_username)
+if [[ $(systemctl is-active komga) == "active" ]]; then
+    wasactive=yes
+    systemctl stop komga
+fi
+echo_progress_start "Downloading komga binary"
+dlurl="$(curl -sNL https://api.github.com/repos/gotson/komga/releases/latest | jq -r '.assets[]?.browser_download_url | select(contains("jar"))')"
+wget "$dlurl" -o /opt/komga/komga.jar
+chown -R "$user":"$user" /opt/komga
+echo_progress_done "Bin downloaded"
+
+if [[ $wasactive == "yes" ]]; then
+    systemctl start komga
+fi
