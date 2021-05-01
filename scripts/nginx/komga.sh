1a12c0a852544a68f98e2e1beaabaffe72527ea5
Flying Sausages <flying_sausages@protonmail.com>
2021-05-01 20:24:44 +0200

    fix: remove proxy conf

--- scripts/nginx/komga.sh
+++ scripts/nginx/komga.sh
@@ -2,7 +2,7 @@
 
 cat > /etc/nginx/apps/komga.conf <<- NGX
 location /komga {
-  include /etc/nginx/snippets/proxy.conf;
+#   include /etc/nginx/snippets/proxy.conf;
   proxy_pass        http://127.0.0.1:6800/komga;
 }
 NGX
