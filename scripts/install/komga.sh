a7e4c9d2c0a17fb345566ba3f4b45e1ce5d7f077
ComputerByte <46009342+ComputerByte@users.noreply.github.com>
2021-05-01 05:34:03 -0700

    Update komga.sh

--- scripts/install/komga.sh
+++ scripts/install/komga.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script by @ComputerByte 
+# Script by @ComputerByte
 # For Komga Installs
 
 user=$(_get_master_username)
@@ -9,11 +9,11 @@ echo_progress_start "Making data directory and owning it to ${user}"
 mkdir -p "/opt/komga"
 chown -R "$user":"$user" /opt/komga
 cd "/opt/komga" || exit
-wget "https://github.com/gotson/komga/releases/download/untagged-6ed960e7d43ebfe31fe8/komga-0.90.0.jar" >> $log 2>&1
+wget "https://github.com/gotson/komga/releases/download/untagged-6ed960e7d43ebfe31fe8/komga-0.90.0.jar" >>$log 2>&1
 echo_progress_done "Data Directory created and owned."
 
 echo_progress_start "Installing systemd service file"
-cat > /etc/systemd/system/komga.service <<- SERV 
+cat >/etc/systemd/system/komga.service <<-SERV
 [[Unit]
 Description=Komga server
 
@@ -34,19 +34,19 @@ echo_progress_done "Komga service installed"
 # This checks if nginx is installed, if it is, then it will install nginx config for komga
 if [[ -f /install/.nginx.lock ]]; then
     echo_progress_start "Installing nginx config"
-    cat > /etc/nginx/apps/komga.conf <<- NGX
+    cat >/etc/nginx/apps/komga.conf <<-NGX
 location /komga {
   include /etc/nginx/snippets/proxy.conf;
   proxy_pass        http://127.0.0.1:8080/komga;
 }
 NGX
-# Reload nginx
-systemctl reload nginx
-else echo_info "Komga will be available on port 8080. Secure your installation manually through the web interface."
-echo_progress_done "Nginx config applied"
+    # Reload nginx
+    systemctl reload nginx
+else
+    echo_info "Komga will be available on port 8080. Secure your installation manually through the web interface."
+    echo_progress_done "Nginx config applied"
 fi
 
-
 touch /install/.komga.lock
-systemctl restart panel   >> $log 2>&1
+systemctl restart panel >>$log 2>&1
 echo_success "Komga installed"
