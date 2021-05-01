147a6217d1aa698684704644cc472c2a77bbe0b5
Flying Sausages <flying_sausages@protonmail.com>
2021-05-01 19:58:25 +0200

    fix(komga): refactor installer to functions

--- scripts/install/komga.sh
+++ scripts/install/komga.sh
@@ -2,18 +2,19 @@
 
 # Script by @ComputerByte
 # For Komga Installs
+install() {
+    user=$(_get_master_username)
+    echo_progress_start "Making data directory and owning it to ${user}"
+    mkdir -p "/opt/komga"
+    chown -R "$user":"$user" /opt/komga
+    cd "/opt/komga" || exit
+    wget "https://github.com/gotson/komga/releases/download/untagged-6ed960e7d43ebfe31fe8/komga-0.90.0.jar" >> $log 2>&1
+    echo_progress_done "Data Directory created and owned."
+}
 
-user=$(_get_master_username)
-
-echo_progress_start "Making data directory and owning it to ${user}"
-mkdir -p "/opt/komga"
-chown -R "$user":"$user" /opt/komga
-cd "/opt/komga" || exit
-wget "https://github.com/gotson/komga/releases/download/untagged-6ed960e7d43ebfe31fe8/komga-0.90.0.jar" >>$log 2>&1
-echo_progress_done "Data Directory created and owned."
-
-echo_progress_start "Installing systemd service file"
-cat >/etc/systemd/system/komga.service <<-SERV
+systemd() {
+    echo_progress_start "Installing systemd service file"
+    cat > /etc/systemd/system/komga.service <<- SERV
 [[Unit]
 Description=Komga server
 
@@ -29,24 +30,23 @@ StandardError=syslog
 [Install]
 WantedBy=multi-user.target
 SERV
-echo_progress_done "Komga service installed"
-
-# This checks if nginx is installed, if it is, then it will install nginx config for komga
-if [[ -f /install/.nginx.lock ]]; then
-    echo_progress_start "Installing nginx config"
-    cat >/etc/nginx/apps/komga.conf <<-NGX
-location /komga {
-  include /etc/nginx/snippets/proxy.conf;
-  proxy_pass        http://127.0.0.1:6800/komga;
+    echo_progress_done "Komga service installed"
 }
-NGX
-    # Reload nginx
-    systemctl reload nginx
-else
-    echo_info "Komga will be available on port 6800. Secure your installation manually through the web interface."
-    echo_progress_done "Nginx config applied"
-fi
+
+nginx() {
+    if [[ -f /install/.nginx.lock ]]; then
+        echo_progress_start "Configuring nginx"
+        bash /etc/swizzin/scripts/nginx/komga.sh
+        systemctl reload nginx
+        echo_progress_done "nginx configured"
+    else
+        echo_info "Komga will be available on port 6800. Secure your installation manually through the web interface."
+    fi
+}
+
+install
+systemd
+nginx
 
 touch /install/.komga.lock
-systemctl restart panel >>$log 2>&1
 echo_success "Komga installed"
