7daeb9ff01ee116be03cfce73b2635bcfab05632
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-05-02 20:28:08 -0500

    feat(radarr) - refactor nginx to form a template

--- scripts/nginx/radarr.sh
+++ scripts/nginx/radarr.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 # Nginx conf for Radarr v3
 # Flying sausages 2020
+# Refactored by Bakerboy448 2021
 master=$(cut -d: -f1 < /root/.master.info)
+app_name="radarr"
+if ! RADARR_OWNER="$(swizdb get $app_name/owner)"; then
+    RADARR_OWNER=$(_get_master_username)
+else
+    RADARR_OWNER="$(swizdb get $app_name/owner)"
+fi
+app_port="7878"
+user="$RADARR_OWNER"
+app_servicename="${app_name}"
+app_configdir="/home/$user/.config/${app_name^}"
+app_baseurl=$app_name
 
-cat > /etc/nginx/apps/radarr.conf << RADARR
-location /radarr {
-  proxy_pass        http://127.0.0.1:7878/radarr;
+cat > /etc/nginx/apps/$app_name.conf << RADARR
+location /$app_name {
+  proxy_pass        http://127.0.0.1:$app_port/$app_baseurl;
   proxy_set_header Host \$proxy_host;
   proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
   proxy_set_header X-Forwarded-Proto \$scheme;
@@ -19,51 +31,51 @@ location /radarr {
 }
 RADARR
 
-isactive=$(systemctl is-active radarr)
+isactive=$(systemctl is-active $app_servicename)
 
 if [[ $isactive == "active" ]]; then
-    echo_log_only "Stopping radarr"
-    systemctl stop radarr
+    echo_log_only "Stopping $app_servicename"
+    systemctl stop $app_servicename
 fi
-user=$(grep User /etc/systemd/system/radarr.service | cut -d= -f2)
+user=$(grep User /etc/systemd/system/$app_servicename.service | cut -d= -f2)
 echo_log_only "Radarr user detected as $user"
-apikey=$(grep -oPm1 "(?<=<ApiKey>)[^<]+" /home/"$user"/.config/Radarr/config.xml)
+apikey=$(grep -oPm1 "(?<=<ApiKey>)[^<]+" "$app_configdir")
 echo_log_only "Apikey = $apikey" >> "$log"
 
-cat > /home/"$user"/.config/Radarr/config.xml << RADARR
+cat > "$app_configdir"/config.xml << RADARR
 <Config>
   <LogLevel>info</LogLevel>
   <UpdateMechanism>BuiltIn</UpdateMechanism>
   <BindAddress>127.0.0.1</BindAddress>
-  <Port>7878</Port>
+  <Port>$app_port</Port>
   <SslPort>9898</SslPort>
   <EnableSsl>False</EnableSsl>
   <LaunchBrowser>False</LaunchBrowser>
   <ApiKey>${apikey}</ApiKey>
   <AuthenticationMethod>None</AuthenticationMethod>
-  <UrlBase>radarr</UrlBase>
+  <UrlBase>$app_baseurl</UrlBase>
 </Config>
 RADARR
 
-chown -R "$user":"$user" /home/"$user"/.config/Radarr
+chown -R "$user":"$user" "$app_configdir"
 
 #shellcheck source=sources/functions/utils
 . /etc/swizzin/sources/functions/utils
-systemctl start radarr -q # Switch radarr on regardless whether it was off before or not as we need to have it online to trigger this cahnge
-if ! timeout 15 bash -c -- "while ! curl -fL \"http://127.0.0.1:7878/api/v3/system/status?apiKey=${apikey}\" >> \"$log\" 2>&1; do sleep 5; done"; then
-    echo_error "Radarr API did not respond as expected. Please make sure Radarr is on v3 and running."
+systemctl start $app_servicename -q # Switch app on regardless whether it was off before or not as we need to have it online to trigger this cahnge
+if ! timeout 15 bash -c -- "while ! curl -fL \"http://127.0.0.1:$app_port/api/v3/system/status?apiKey=${apikey}\" >> \"$log\" 2>&1; do sleep 5; done"; then
+    echo_error "${app_name^} API did not respond as expected. Please make sure ${app_name^} is up to date and running."
     exit 1
 else
-    urlbase="$(curl -sL "http://127.0.0.1:7878/api/v3/config/host?apikey=${apikey}" | jq '.urlBase' | cut -d '"' -f 2)"
-    echo_log_only "Radarr API tested and reachable"
+    urlbase="$(curl -sL "http://127.0.0.1:${app_port}/api/v3/config/host?apikey=${apikey}" | jq '.urlBase' | cut -d '"' -f 2)"
+    echo_log_only "${app_name^} API tested and reachable"
 fi
 
-payload=$(curl -sL "http://127.0.0.1:7878/api/v3/config/host?apikey=${apikey}" | jq ".certificateValidation = \"disabledForLocalAddresses\"")
+payload=$(curl -sL "http://127.0.0.1:${app_port}/api/v3/config/host?apikey=${apikey}" | jq ".certificateValidation = \"disabledForLocalAddresses\"")
 echo_log_only "Payload = \n${payload}"
-echo_log_only "Return from radarr after PUT ="
-curl -s "http://127.0.0.1:7878${urlbase}/api/v3/config/host?apikey=${apikey}" -X PUT -H 'Accept: application/json, text/javascript, */*; q=0.01' --compressed -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' --data-raw "${payload}" >> "$log"
+echo_log_only "Return from ${app_name^} after PUT ="
+curl -s "http://127.0.0.1:$app_port/$urlbase/api/v3/config/host?apikey=${apikey}" -X PUT -H 'Accept: application/json, text/javascript, */*; q=0.01' --compressed -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' --data-raw "${payload}" >> "$log"
 
-# Switch radarr back off if it was dead before
+# Switch app back off if it was dead before
 if [[ $isactive != "active" ]]; then
-    systemctl stop radarr
+    systemctl stop app_servicename
 fi
