edeb8a7e54c8dac96c9ca5e2a74499f4ef965680
ComputerByte <46009342+ComputerByte@users.noreply.github.com>
2021-05-07 03:11:52 -0700

    feat(Komga): Add Komga (#656)
    * Create komga.sh
    
    * Create komga.sh
    
    * Update scripts/install/komga.sh
    
    Co-authored-by: flying-sausages <flying_sausages@protonmail.com>
    
    * Update komga.sh
    
    * Update komga.sh
    
    * Update komga.sh
    
    added port variable to start up to avoid using 8080 and change nginx config to match
    
    * fix(komga): Move nginx to separate script
    
    * fix(komga): refactor installer to functions
    
    * fix: let the version loose
    
    * fix(komga): start the service after install
    
    * fix(komga): reorder echoes and chownd
    
    * feat(komga): upgrader
    
    * fix(komga): bad systemd unit file
    
    * fix: remove proxy conf
    
    * feat: java install utils
    
    * fix(komga): catch bad wgets
    
    * fix(komga): fix wget flag
    
    * fix(komga): wget output to log
    
    * feat(komga): Add java install
    
    * Update komga.sh
    
    Changed java usage, 1g works just fine, no need for 4g, even with a large library. Can't say for multi user though.
    
    Co-authored-by: flying-sausages <flying_sausages@protonmail.com>

---
+++ scripts/upgrade/komga.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [ ! -f /install/.komga.install ]; then
+    echo_error "You ain't got no komga, whacha doen"
+    exit 1
+fi
+
+user=$(_get_master_username)
+if [[ $(systemctl is-active komga) == "active" ]]; then
+    wasactive=yes
+    systemctl stop komga
+fi
+echo_progress_start "Downloading komga binary"
+dlurl="$(curl -sNL https://api.github.com/repos/gotson/komga/releases/latest | jq -r '.assets[]?.browser_download_url | select(contains("jar"))')"
+wget "$dlurl" -O /opt/komga/komga.jar >> "${log}" 2>&1 || {
+    echo_error "Download failed"
+    exit 1
+}
+chown -R "$user":"$user" /opt/komga
+echo_progress_done "Bin downloaded"
+
+if [[ $wasactive == "yes" ]]; then
+    systemctl start komga
+fi
