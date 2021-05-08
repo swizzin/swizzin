296c3b9c2c9257026a66e588295d905e5ce7fc2a
Flying Sausages <flying_sausages@protonmail.com>
2021-05-08 14:01:38 +0200

    fix: komga auth breaking swizzin

--- scripts/nginx/komga.sh
+++ scripts/nginx/komga.sh
@@ -2,7 +2,7 @@
 
 cat > /etc/nginx/apps/komga.conf <<- NGX
 location /komga {
-#   include /etc/nginx/snippets/proxy.conf;
-  proxy_pass        http://127.0.0.1:6800/komga;
+    proxy_pass      http://127.0.0.1:6800/komga;
+    auth_basic      "Realm";
 }
 NGX
