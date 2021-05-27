7c8921ea54d2a8882e4e46554f1cfe553fd3275b
flying-sausages <flying_sausages@protonmail.com>
2021-05-27 23:00:13 +0200

    fix(rutorrent): Ensure /srv is created, get rid of cds, remove cloudscraper and python dep (#670)
    * fix(rutorrent): Ensure /srv is created, get rid of cds
    
    * fix(rutorrent): catch critical failures
    
    * fix(rar): forward exit up just in case???
    
    * fix(rutorrent): just make the folder
    
    * fix(rutorrent): ensure bash exits recurse up
    
    * feat(rutorrent): buhbye cloudflare cloudscraper shit
    
    * refactor: rutorrent filemanager script linebreaks
    
    * fix(rutorrent): upkeep plugin update
    
    * fix(rutorrent): only run updater if rutorrent exists...
    
    * fix(rutorrent): an escaped git without -C
    
    * fix(rutorrent): echos
    
    * fix(nginx): ensure /srv is created just in case
    
    * fix(ruttorent): those Cs gotta come first in git
    
    * Update sources/functions/utils
    
    * Derp
    
    * Only make noise if we actually pull an update
    
    * fix(rutorrent): who
    
    Co-authored-by: liaralabs <liara@swizzin.ltd>

--- scripts/update/rutorrent.sh
+++ scripts/update/rutorrent.sh
@@ -1,48 +1,56 @@
 #!/bin/bash
 
-#Update club-QuickBox with latest changes
-if [[ -d /srv/rutorrent/plugins/theme/themes/club-QuickBox ]]; then
-    cd /srv/rutorrent/plugins/theme/themes/club-QuickBox
-    git fetch
-    if [[ ! $(git rev-parse HEAD) == $(git rev-parse @{u}) ]]; then
-        git reset HEAD --hard
-        git pull
+if [ -f /install/.rutorrent.lock ]; then
+    #Update club-QuickBox with latest changes
+    if [[ -d /srv/rutorrent/plugins/theme/themes/club-QuickBox ]]; then
+        cqbdir="/srv/rutorrent/plugins/theme/themes/club-QuickBox"
+        git -C "$cqbdir" fetch
+        if [[ ! $(git -C "$cqbdir" rev-parse HEAD) == $(git -C "$cqbdir" rev-parse @{u}) ]]; then
+            echo_progress_start "Updating rutorrent theme"
+            git -C "$cqbdir" reset HEAD --hard >> $log 2>&1
+            git -C "$cqbdir" pull >> $log 2>&1
+            echo_progress_done
+        fi
     fi
-fi
 
-if [[ -d /srv/rutorrent/plugins/theme/themes/DarkBetter ]]; then
-    if [[ -z "$(ls -A /srv/rutorrent/plugins/theme/themes/DarkBetter/)" ]]; then
-        cd /srv/rutorrent
-        git submodule update --init --recursive > /dev/null 2>&1
-    fi
-fi
+    if [[ -d /srv/rutorrent/plugins/theme/themes/DarkBetter ]]; then
+        if [[ -z "$(ls -A /srv/rutorrent/plugins/theme/themes/DarkBetter/)" ]]; then
+            echo_progress_start "Updating rutorrent submodules"
+            git submodule update --init --recursive -C /srv/rutorrent >> $log 2>&1
+            echo_progress_done
+        fi
 
-reloadnginx=0
+    fi
 
-if [[ -f /etc/nginx/apps/rutorrent.conf ]]; then
-    #Fix fastcgi path_info being blank with certain circumstances under ruTorrent
-    #Essentially, disabling the distro snippet and ignoring try_files
-    if grep -q '/srv\$fastcgi_script_name' /etc/nginx/apps/rutorrent.conf; then
-        sed -i 's|/srv$fastcgi_script_name|$request_filename|g' /etc/nginx/apps/rutorrent.conf
-        reloadnginx=1
-    fi
-    if grep -q 'alias' /etc/nginx/apps/rutorrent.conf; then
-        sed -i '/alias/d' /etc/nginx/apps/rutorrent.conf
-        reloadnginx=1
-    fi
-    if ! grep -q fastcgi_split_path_info /etc/nginx/apps/rutorrent.conf; then
-        sed -i 's|include snippets/fastcgi-php.conf;|fastcgi_split_path_info ^(.+\\.php)(/.+)$;|g' /etc/nginx/apps/rutorrent.conf
-        sed -i '/SCRIPT_FILENAME/a \ \ \ \ include fastcgi_params;\n    fastcgi_index index.php;' /etc/nginx/apps/rutorrent.conf
-        reloadnginx=1
-    fi
-fi
+    reloadnginx=0
 
-if [[ -f /install/.flood.lock ]]; then
-    users=($(cut -d: -f1 < /etc/htpasswd))
-    for u in ${users[@]}; do
-        if [[ ! -f /etc/nginx/apps/${u}.scgi.conf ]]; then
+    if [[ -f /etc/nginx/apps/rutorrent.conf ]]; then
+        #Fix fastcgi path_info being blank with certain circumstances under ruTorrent
+        #Essentially, disabling the distro snippet and ignoring try_files
+        if grep -q '/srv\$fastcgi_script_name' /etc/nginx/apps/rutorrent.conf; then
+            sed -i 's|/srv$fastcgi_script_name|$request_filename|g' /etc/nginx/apps/rutorrent.conf
+            echo_log_only "rutorrent fastcgi_script_name triggers nginx reload"
             reloadnginx=1
-            cat > /etc/nginx/apps/${u}.scgi.conf << RUC
+        fi
+        if grep -q 'alias' /etc/nginx/apps/rutorrent.conf; then
+            sed -i '/alias/d' /etc/nginx/apps/rutorrent.conf
+            echo_log_only "rutorrent alias triggers nginx reload"
+            reloadnginx=1
+        fi
+        if ! grep -q fastcgi_split_path_info /etc/nginx/apps/rutorrent.conf; then
+            sed -i 's|include snippets/fastcgi-php.conf;|fastcgi_split_path_info ^(.+\\.php)(/.+)$;|g' /etc/nginx/apps/rutorrent.conf
+            sed -i '/SCRIPT_FILENAME/a \ \ \ \ include fastcgi_params;\n    fastcgi_index index.php;' /etc/nginx/apps/rutorrent.conf
+            echo_log_only "rutorrent fastcgi_split_path_info triggers nginx reload"
+            reloadnginx=1
+        fi
+    fi
+
+    if [[ -f /install/.flood.lock ]]; then
+        users=($(cut -d: -f1 < /etc/htpasswd))
+        for u in ${users[@]}; do
+            if [[ ! -f /etc/nginx/apps/${u}.scgi.conf ]]; then
+                reloadnginx=1
+                cat > /etc/nginx/apps/${u}.scgi.conf << RUC
 location /${u} {
 include scgi_params;
 scgi_pass unix:/var/run/${u}/.rtorrent.sock;
@@ -50,16 +58,19 @@ auth_basic "What's the password?";
 auth_basic_user_file /etc/htpasswd.d/htpasswd.${u};
 }
 RUC
-        fi
-    done
-fi
+            fi
+        done
+    fi
 
-if [[ $reloadnginx == 1 ]]; then
-    systemctl reload nginx
-fi
+    if [[ $reloadnginx == 1 ]]; then
+        echo_progress_start "Reloading nginx due to rutorrent config updates"
+        systemctl reload nginx
+        echo_progress_done
+    fi
 
-if [[ -f /install/.quota.lock ]] && { ! grep -q "/usr/bin/quota -wu" /srv/rutorrent/plugins/diskspace/action.php > /dev/null 2>&1 || [[ ! $(grep -c cachedEcho /srv/rutorrent/plugins/diskspace/action.php) == 2 ]]; }; then
-    cat > /srv/rutorrent/plugins/diskspace/action.php << 'DSKSP'
+    if [[ -f /install/.quota.lock ]] && { ! grep -q "/usr/bin/quota -wu" /srv/rutorrent/plugins/diskspace/action.php > /dev/null 2>&1 || [[ ! $(grep -c cachedEcho /srv/rutorrent/plugins/diskspace/action.php) == 2 ]]; }; then
+        echo_progress_start "Fixing quota rutorrent plugin"
+        cat > /srv/rutorrent/plugins/diskspace/action.php << 'DSKSP'
 <?php
 #################################################################################
 ##  [Quick Box - action.php modified for quota systems use]
@@ -83,6 +94,9 @@ if [[ -f /install/.quota.lock ]] && { ! grep -q "/usr/bin/quota -wu" /srv/rutorr
   }
 ?>
 DSKSP
-    . /etc/swizzin/sources/functions/php
-    restart_php_fpm
+        . /etc/swizzin/sources/functions/php
+        restart_php_fpm
+        echo_progress_done
+    fi
+
 fi
