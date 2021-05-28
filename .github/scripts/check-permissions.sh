6ad7f2e2d432386960fad4d85c522a71a531cdd9
Oscar Busk <oscar.busk@gmail.com>
2021-05-28 23:07:28 +0200

    fix(ci): Add double quotes as sensei spellcheck commands.
    https://github.com/koalaman/shellcheck/wiki/SC2086

--- .github/scripts/check-permissions.sh
+++ .github/scripts/check-permissions.sh
@@ -3,7 +3,7 @@ exit_code=0
 
 # Check so that all scripts has --rwxr-xr-x = 755
 for file in ./scripts/**/*; do
-    permissions=$(stat -L -c "%a" $file)
+    permissions=$(stat -L -c "%a" "$file")
     if [[ "$permissions" != "755" ]]; then
         exit_code=1
         echo "'$file' has incorrect permissions '$permissions', should be '755'"
