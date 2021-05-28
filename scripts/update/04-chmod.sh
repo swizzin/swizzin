d91e6ff80b541c8acf9589bacde3dafa91cbf1dc
Oscar Busk <oscar.busk@gmail.com>
2021-05-28 23:58:01 +0200

    chore(update): Less spammy output
    Co-authored-by: flying-sausages <flying_sausages@protonmail.com>

--- scripts/update/04-chmod.sh
+++ scripts/update/04-chmod.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # Ensures all scrips has permissions so that they are executable
 
-echo_info "Setting permissions of scripts"
-# Set permissions of all scripts
-chmod -R 700 /etc/swizzin/scripts
+echo_progress_start "Checking permissions on swizzin files"
 
-echo_info "Setting permissions of sources"
-# Sources are only `.` or `source`d and does not need to be executable
-chmod -R 600 /etc/swizzin/sources
+chmod -R 700 /etc/swizzin/scripts # Set permissions of all scripts
+chmod -R 600 /etc/swizzin/sources # Sources are only `.` or `source`d and does not need to be executable
+
+echo_progress_done "Permissions changed"
