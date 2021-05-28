3fc1fe0f70bc0e0e7e7edbe9095c6076b02a9eb8
Oscar Busk <oscar.busk@gmail.com>
2021-05-28 23:29:49 +0200

    chore(update): Print messages before trying chmod

--- scripts/update/04-chmod.sh
+++ scripts/update/04-chmod.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Ensures all scrips has permissions so that they are executable
 
+echo_info "Setting permissions of scripts"
 # Set permissions of all scripts
 chmod -R 700 /etc/swizzin/scripts
 
+echo_info "Setting permissions of sources"
 # Sources are only `.` or `source`d and does not need to be executable
 chmod -R 600 /etc/swizzin/sources
