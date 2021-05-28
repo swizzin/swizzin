feba538ff0948a45495e0be3c50e14a56a63bd8d
Oscar Busk <oscar.busk@gmail.com>
2021-05-29 00:08:36 +0200

    chore(update): Set chmod 700 on all directories inside `sources/`
    Co-authored-by: flying-sausages <flying_sausages@protonmail.com>

--- scripts/update/04-chmod.sh
+++ scripts/update/04-chmod.sh
@@ -5,6 +5,6 @@ echo_progress_start "Checking permissions on swizzin files"
 
 chmod -R 700 /etc/swizzin/scripts        # Set permissions of all scripts
 chmod -R 600 /etc/swizzin/sources        # Sources are only `.` or `source`d and does not need to be executable
-chmod 700 /etc/swizzin/sources/functions # Best to keep X permission on the folder inside sources
+find /etc/swizzin/sources -type d -exec chmod 700 {} \; # Best to keep X permission on the folder inside sources
 
 echo_progress_done "Permissions changed"
