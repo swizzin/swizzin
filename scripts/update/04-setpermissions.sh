8883afd2e46b1efc0f803a41006fd93849ec4af8
Flying Sausages <flying_sausages@protonmail.com>
2021-05-29 01:06:06 +0200

    fix(perms): move sources back to 600

--- scripts/update/04-setpermissions.sh
+++ scripts/update/04-setpermissions.sh
@@ -2,7 +2,8 @@
 
 echo_progress_start "Setting correct permissions on swizzin files"
 
-find /etc/swizzin/scripts -exec chmod 700 {} \; # Set X on all subfolders of `sources`
-find /etc/swizzin/sources -exec chmod 700 {} \; # Set X on all subfolders of `sources`
+find /etc/swizzin/scripts -exec chmod 700 {} \;
+find /etc/swizzin/sources -type f -exec chmod 600 {} \;
+find /etc/swizzin/sources -type d -exec chmod 700 {} \;
 
 echo_progress_done "Permissions set"
