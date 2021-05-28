ae39c2265af6877271e61b4849c1e6857fa93dd3
Oscar Busk <oscar.busk@gmail.com>
2021-05-29 00:01:06 +0200

    chore(update): +x on folder in sources directory

--- scripts/update/04-chmod.sh
+++ scripts/update/04-chmod.sh
@@ -3,7 +3,8 @@
 
 echo_progress_start "Checking permissions on swizzin files"
 
-chmod -R 700 /etc/swizzin/scripts # Set permissions of all scripts
-chmod -R 600 /etc/swizzin/sources # Sources are only `.` or `source`d and does not need to be executable
+chmod -R 700 /etc/swizzin/scripts        # Set permissions of all scripts
+chmod -R 600 /etc/swizzin/sources        # Sources are only `.` or `source`d and does not need to be executable
+chmod 700 /etc/swizzin/sources/functions # Best to keep X permission on the folder inside sources
 
 echo_progress_done "Permissions changed"
