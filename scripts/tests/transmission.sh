256c2a19a3fceedf42d07563f7905bfc9d422391
Flying Sausages <flying_sausages@protonmail.com>
2021-05-29 18:55:26 +0200

    test: proper test for transmission

--- scripts/tests/transmission.sh
+++ scripts/tests/transmission.sh
@@ -3,9 +3,18 @@
 #shellcheck source=scripts/tests/basetest.sh
 . /etc/swizzin/scripts/tests/basetest.sh
 
-master="$(_get_master_username)"
-check_service "transmission@$master" || bad=true
-check_port "transmission" || bad=true
-check_nginx "transmission" || bad=true
+readarray -t users < <(_get_user_list)
+for user in "${users[@]}"; do
+    check_service "transmission@$user" || bad=true
+
+    confpath="/home/$user/.config/transmission-daemon/settings.json"
+    key="rpc-port"
+    paramrr=".[\"$key\"]"
+    port=$(jq -r "$paramrr" < "$confpath")
+    extra_params="--user $user:$(_get_user_password "$user")"
+
+    check_port "$port" "$extra_params" || bad=true
+    check_nginx "transmission" "$extra_params" || bad=true
+done
 
 evaluate_bad
