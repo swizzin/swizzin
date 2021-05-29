f03fa452101f1a45bc1a1bf789b72ba56d121a0a
Flying Sausages <flying_sausages@protonmail.com>
2021-05-29 14:51:29 +0200

    fix(ci): add sources to the actual shellcheck find

--- .github/scripts/shellcheck-diff-apply.sh
+++ .github/scripts/shellcheck-diff-apply.sh
@@ -2,6 +2,7 @@
 
 echo -e "---\tListing all shellcheck warning"
 find . -type f -name \*.sh -exec bash -c 'shellcheck "$1" -S warning' none {} \;
+find sources/functions -type f -exec bash -c 'shellcheck "$1" -S warning' none {} \;
 
 export EXPLICIT_SHCK_CODES="SC2086,SC2164"
 echo -e "---\tAutofixing all .sh files"
