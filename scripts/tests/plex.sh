9e582d43422466522f51d63baea111c381f8c049
Flying Sausages <flying_sausages@protonmail.com>
2021-05-29 19:16:09 +0200

    test: add plex

---
+++ scripts/tests/plex.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#shellcheck source=sources/functions/tests
+. /etc/swizzin/sources/functions/tests
+
+check_service "plexmediaserver" || bad=true
+check_port "32400" || bad=true
+
+evaluate_bad
