301237d7771f0911b39132897207abd20dacfe55
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-05-28 20:30:42 -0500

    feat: Prowlarr addition (#674)
    * New Application: Prowlarr
    
    * address review
    
    todo: apply to #658
    
    * fixup! address review
    
    * address comments part 2
    
    * change ssl to match app default
    
    - readarr default (pr'd) 7979
    - radarr default 9898
    - sonarr default 8988
    - lidarr default 6868
    - prowlarr default (pr'd) 6969
    
    * reorder functions
    
    * fixup nginx formatting
    
    * var fixups
    
    * reorder vars
    
    * var fixups
    
    remove logging of apikey
    add branch as a var
    
    * fix: ensure scripts can execute
    
    * refactor nginx
    
    - no cert change needed
    
    * Update scripts/nginx/prowlarr.sh
    
    * fixup nginx config
    
    * fixup! fixup nginx config
    
    * ensure $user/config exists and is owned
    
    * fixup! ensure $user/config exists and is owned
    
    Co-authored-by: Flying Sausages <flying_sausages@protonmail.com>

---
+++ scripts/remove/prowlarr.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+app_name="prowlarr"
+
+if ! PROWLARR_OWNER="$(swizdb get $app_name/owner)"; then
+    PROWLARR_OWNER=$(_get_master_username)
+fi
+
+user="$PROWLARR_OWNER"
+app_configdir="/home/$user/.config/${app_name^}"
+app_servicefile="${app_name}.service"
+app_dir="/opt/${app_name^}"
+app_lockname="$app_name"
+
+if ask "Would you like to purge the configuration?" Y; then
+    purgeapp="True"
+else
+    purgeapp="False"
+fi
+
+systemctl disable --now -q "$app_servicefile"
+rm /etc/systemd/system/"$app_servicefile"
+systemctl daemon-reload -q
+rm -rf "$app_dir"
+
+if [[ "$purgeapp" = "True" ]]; then
+    rm -rf "$app_configdir"
+fi
+
+if [[ -f /install/.nginx.lock ]]; then
+    rm "/etc/nginx/apps/$app_name.conf"
+    systemctl reload nginx >> "$log" 2>&1
+fi
+
+swizdb clear "$app_name/owner"
+rm "/install/.$app_lockname.lock"
