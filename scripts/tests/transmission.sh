629c1b4c717a4090a55ed811355ed9a13387d333
Flying Sausages <flying_sausages@protonmail.com>
2021-05-30 01:08:34 +0200

    test: collapse vars in transmission

--- scripts/tests/transmission.sh
+++ scripts/tests/transmission.sh
@@ -7,14 +7,11 @@ readarray -t users < <(_get_user_list)
 for user in "${users[@]}"; do
     check_service "transmission@$user" || bad=true
 
-    confpath="/home/$user/.config/transmission-daemon/settings.json"
-    key="rpc-port"
-    paramrr=".[\"$key\"]"
-    port=$(jq -r "$paramrr" < "$confpath")
     extra_params="--user $user:$(_get_user_password "$user")"
-
-    check_port "$port" "$extra_params" || bad=true
     check_nginx "transmission" "$extra_params" || bad=true
+
+    port=$(jq -r ".[\"rpc-port\"]" < "/home/$user/.config/transmission-daemon/settings.json")
+    check_port "$port" "$extra_params" || bad=true
 done
 
 evaluate_bad
