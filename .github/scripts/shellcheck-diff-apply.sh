353f80e8743c5a41bc36f8ac737742577515bc1c
Flying Sausages <flying_sausages@protonmail.com>
2021-05-29 12:52:19 +0200

    fix(ci): ensure all .sh and functions get checked

--- .github/scripts/shellcheck-diff-apply.sh
+++ .github/scripts/shellcheck-diff-apply.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 export SHELLCHECK_CODES="SC2086"
-find scripts -type f -exec bash -c 'shellcheck "$1" -i $SHELLCHECK_CODES --format=diff | patch "$1"' none {} \;
+echo -e "---\tChecking all .sh files"
+find . -type f -name \*.sh -exec bash -c 'shellcheck "$1" -i $SHELLCHECK_CODES --format=diff | patch "$1"' none {} \;
+
+echo -e "---\tChecking all sources/functions files"
+find sources/functions -type f -exec bash -c 'shellcheck "$1" -i $SHELLCHECK_CODES --format=diff | patch "$1"' none {} \;
