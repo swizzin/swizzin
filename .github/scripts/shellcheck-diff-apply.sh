ac95d4ed1edc0f4e68ac845801b34cabe451ac5b
Flying Sausages <flying_sausages@protonmail.com>
2021-05-29 14:30:03 +0200

    fix(ci): add cd fixing

--- .github/scripts/shellcheck-diff-apply.sh
+++ .github/scripts/shellcheck-diff-apply.sh
@@ -3,7 +3,7 @@
 echo -e "---\tListing all shellcheck warning"
 find . -type f -name \*.sh -exec bash -c 'shellcheck "$1" -S warning' none {} \;
 
-export EXPLICIT_SHCK_CODES="SC2086,SC1091"
+export EXPLICIT_SHCK_CODES="SC2086,SC2164"
 echo -e "---\tAutofixing all .sh files"
 find . -type f -name \*.sh -exec bash -c 'shellcheck "$1" -i $EXPLICIT_SHCK_CODES --format=diff | patch "$1"' none {} \;
 
