12e666a91bda7952dc4f20d054be9943b5ac3be6
Flying Sausages <flying_sausages@protonmail.com>
2021-05-30 22:00:10 +0200

    test: rclone better

--- scripts/tests/rclone.sh
+++ scripts/tests/rclone.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-echo_warn "No tests to run for rclone"
+#shellcheck source=sources/functions/tests
+. /etc/swizzin/sources/functions/tests
+
+readarray -t users < <(_get_user_list)
+for user in "${users[@]}"; do
+    systemctl -q is-enabled "rclone@$user" || {
+        echo_warn "rclone@$user is not enabled, skipping"
+        continue
+    }
+    check_service "rclone@$user" || bad=true
+done
+
+evaluate_bad
