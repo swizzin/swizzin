c7bbaf448a095fbc8756c0b4434bac3243c2e9eb
Flying Sausages <flying_sausages@protonmail.com>
2021-05-30 16:53:44 +0200

    test: add qbittorrent and deluged+deluge-web

---
+++ scripts/tests/deluge.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#shellcheck source=sources/functions/tests
+. /etc/swizzin/sources/functions/tests
+
+#Check nginx only once beause if the config works for one, it will work for all
+check_nginx "deluge" || bad=true
+
+readarray -t users < <(_get_user_list)
+for user in "${users[@]}"; do
+    check_service "deluged@$user" || {
+        bad=true
+        continue
+    }
+
+    d_port=$(jq -r '.["daemon_port"]' < /home/"$user"/.config/deluge/core.conf | grep -e '^[0-9]*$')
+    check_port "$d_port"
+
+    check_service "deluge-web@$user" || {
+        bad=true
+        continue
+    }
+
+    extra_params="--user $user:$(_get_user_password "$user")"
+    web_port=$(jq -r '.["port"]' < /home/"$user"/.config/deluge/web.conf | grep -e '^[0-9]*$')
+    check_port_curl "$web_port" "$extra_params" || bad=true
+done
+
+evaluate_bad
---
+++ scripts/tests/qbittorrent.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#shellcheck source=sources/functions/tests
+. /etc/swizzin/sources/functions/tests
+
+#Check nginx only once beause if the config works for one, it will work for all
+check_nginx "qbittorrent" || bad=true
+
+readarray -t users < <(_get_user_list)
+for user in "${users[@]}"; do
+    check_service "qbittorrent@$user" || {
+        bad=true
+        continue
+    }
+
+    extra_params="--user $user:$(_get_user_password "$user")"
+    confpath="/home/${user}/.config/qBittorrent/qBittorrent.conf"
+    port=$(grep 'WebUI\\Port' "$confpath" | cut -d= -f2)
+    check_port_curl "$port" "$extra_params" || bad=true
+done
+
+evaluate_bad
