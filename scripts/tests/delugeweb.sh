b64fbd32b1ce036652648e83d207cae2e0047494
Flying Sausages <flying_sausages@protonmail.com>
2021-05-31 12:15:16 +0200

    test: separate deluge due to two lockfiles

--- scripts/tests/delugeweb.sh
+++ scripts/tests/delugeweb.sh
@@ -8,18 +8,14 @@ check_nginx "deluge" || BAD=true
 
 readarray -t users < <(_get_user_list)
 for user in "${users[@]}"; do
-    systemctl -q is-enabled "deluged@$user" || {
-        echo_warn "deluged@$user is not enabled, skipping"
-        continue
-    }
-
-    check_service "deluged@$user" || {
+    check_service "deluge-web@$user" || {
         BAD=true
         continue
     }
 
-    d_port=$(jq -r '.["daemon_port"]' < /home/"$user"/.config/deluge/core.conf | grep -e '^[0-9]*$')
-    check_port "$d_port"
+    extra_params="--user $user:$(_get_user_password "$user")"
+    web_port=$(jq -r '.["port"]' < /home/"$user"/.config/deluge/web.conf | grep -e '^[0-9]*$')
+    check_port_curl "$web_port" "$extra_params" || BAD=true
 done
 
 evaluate_bad
