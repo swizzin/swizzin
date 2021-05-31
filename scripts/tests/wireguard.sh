1782f1b2f287f3df89d0e8b534d41fce8e2b2bb8
Flying Sausages <flying_sausages@protonmail.com>
2021-05-31 09:55:10 +0200

    test: wireguard

---
+++ scripts/tests/wireguard.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#shellcheck source=sources/functions/tests
+. /etc/swizzin/sources/functions/tests
+
+readarray -t users < <(_get_user_list)
+for user in "${users[@]}"; do
+    systemctl -q is-enabled "wg-quick@wg$(id -u "$user")" || {
+        echo_warn "wg-quick@wg$(id -u "$user") is not enabled, skipping"
+        continue
+    }
+    check_service "wg-quick@wg$(id -u "$user")" || bad=true
+done
+
+evaluate_bad
