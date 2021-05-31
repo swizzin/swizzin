1a98b295b18216f80529cc200f3702bf1ea21148
Flying Sausages <flying_sausages@protonmail.com>
2021-05-31 12:32:10 +0200

    test: don't test torrent client nginx if no service is running

--- scripts/tests/qbittorrent.sh
+++ scripts/tests/qbittorrent.sh
@@ -3,15 +3,14 @@
 #shellcheck source=sources/functions/tests
 . /etc/swizzin/sources/functions/tests
 
-#Check nginx only once beause if the config works for one, it will work for all
-check_nginx "qbittorrent" || BAD=true
-
 readarray -t users < <(_get_user_list)
 for user in "${users[@]}"; do
     systemctl -q is-enabled "qbittorrent@$user" || {
         echo_warn "qbittorrent@$user is not enabled, skipping"
         continue
     }
+
+    atleastonerunning=true
 
     check_service "qbittorrent@$user" || {
         BAD=true
@@ -23,5 +22,12 @@ for user in "${users[@]}"; do
     port=$(grep 'WebUI\\Port' "$confpath" | cut -d= -f2)
     check_port_curl "$port" "$extra_params" || BAD=true
 done
+
+if [[ "$atleastonerunning" = "true" ]]; then
+    #Check nginx only once beause if the config works for one, it will work for all
+    check_nginx "qbittorrent" || BAD=true
+else
+    echo_warn "No qbittorrent instance was running, skipping nginx check"
+fi
 
 evaluate_bad
--- scripts/tests/transmission.sh
+++ scripts/tests/transmission.sh
@@ -3,15 +3,14 @@
 #shellcheck source=sources/functions/tests
 . /etc/swizzin/sources/functions/tests
 
-#Check nginx only once beause if the config works for one, it will work for all
-check_nginx "transmission" || BAD=true
-
 readarray -t users < <(_get_user_list)
 for user in "${users[@]}"; do
     systemctl -q is-enabled "transmission@$user" || {
         echo_warn "transmission@$user is not enabled, skipping"
         continue
     }
+
+    atleastonerunning=true
 
     check_service "transmission@$user" || {
         BAD=true
@@ -22,5 +21,12 @@ for user in "${users[@]}"; do
     port=$(jq -r ".[\"rpc-port\"]" < "/home/$user/.config/transmission-daemon/settings.json")
     check_port_curl "$port" "$extra_params" || BAD=true
 done
+
+if [[ "$atleastonerunning" = "true" ]]; then
+    #Check nginx only once beause if the config works for one, it will work for all
+    check_nginx "qbittorrent" || BAD=true
+else
+    echo_warn "No qbittorrent instance was running, skipping nginx check"
+fi
 
 evaluate_bad
