92643c8f4c0a23ce87696270b1746b806446613d
Flying Sausages <flying_sausages@protonmail.com>
2021-07-12 23:23:46 +0200

    fix: apply suggestion from #593

--- scripts/remove/netdata.sh
+++ scripts/remove/netdata.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 # Netdata uninstaller for swizzin
 
+"${NETDATA_PREFIX}"/usr/libexec/netdata/netdata-uninstaller.sh --yes --env /etc/netdata/.environment
+
 systemctl disable -q netdata > /dev/null 2>&1
 systemctl stop -q netdata
 rm -rf $(which netdata)
