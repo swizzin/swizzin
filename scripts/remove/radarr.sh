fc078d3717a95afa40264ccf6651932d03c42655
Flying Sausages <flying_sausages@protonmail.com>
2021-07-13 19:00:57 +0200

    fix: purge config explicitly for radarr

--- scripts/remove/radarr.sh
+++ scripts/remove/radarr.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if ask "Would you like to purge the configuration?" Y; then
+    rm -rf "/home/$(swizdb get radarr/owner)/.config/Radarr"
+    swizdb clear "sonarr/owner"
+fi
+
 systemctl disable --now -q radarr
 rm /etc/systemd/system/radarr.service
 systemctl daemon-reload -q
