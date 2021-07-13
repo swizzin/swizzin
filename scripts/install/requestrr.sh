155382a060d0027d27b2bb6dbe40486d63b62fca
Flying Sausages <flying_sausages@protonmail.com>
2021-07-13 12:15:52 +0200

    style: fix echos

--- scripts/install/requestrr.sh
+++ scripts/install/requestrr.sh
@@ -8,12 +8,11 @@
 #   changes/dates in source files. Any modifications to our software
 #   including (via compiler) GPL-licensed code must also be made available
 #   under the GPL along with build & install instructions.
-#shellcheck source=sources/functions/requestrr
-#shellcheck source=sources/functions/users
-. /etc/swizzin/sources/functions/requestrr
 
 user=$(_get_master_username)
 
+#shellcheck source=sources/functions/requestrr
+. /etc/swizzin/sources/functions/requestrr
 # Download Requestrr
 _requestrr_download
 
@@ -25,20 +24,20 @@ mv /opt/requestrr* /opt/requestrr
 echo_progress_done "Archive extracted"
 
 # Create User and set permissions
-echo_progress_start "Creating requestrr user and setting permssions"
+echo_progress_start "Configuring requestrr"
 useradd --system -Md /opt/requestrr/ requestrr
 chown -R requestrr:requestrr /opt/requestrr/
 chmod u+x /opt/requestrr/Requestrr.WebApi
-echo_progress_done "Requestrr user has been created & permissions set."
 
 # Apply Requestrr Config from sources/function/requestrr
-echo_progress_start "Applying Requestrr config..."
 _requestrr_config
-echo_progress_done "Requestrr config applied."
+echo_progress_done "Requestrr configured"
 
 echo_progress_start "Installing Systemd service"
 requestrr_systemd
-echo_progress_done "Installing Systemd service"
+systemctl -q daemon-reload
+systemctl -q enable --now requestrr
+echo_progress_done "Service installed and started"
 
 # Nginx Changes
 if [[ -f /install/.nginx.lock ]]; then
@@ -60,8 +59,6 @@ else
 SET
 fi
 
-systemctl -q daemon-reload
-systemctl -q enable --now requestrr
 touch /install/.requestrr.lock
 
 echo_progress_done "Requestrr service installed and enabled"
