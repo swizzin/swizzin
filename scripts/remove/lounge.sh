60aaf818caa346361563ef20dc713567fb91be64
flying-sausages <flying_sausages@protonmail.com>
2021-07-19 22:32:08 +0200

    Update scripts/remove/lounge.sh

--- scripts/remove/lounge.sh
+++ scripts/remove/lounge.sh
@@ -8,7 +8,7 @@ npm uninstall -g thelounge --save >> /dev/null 2>&1
 deluser lounge --remove-home >> /dev/null 2>&1
 rm -rf /home/lounge # just in case
 
-rm /etc/nginx/apps/lounge.conf
+rm -f /etc/nginx/apps/lounge.conf
 systemctl reload nginx -q
 
 rm -f /etc/systemd/system/lounge.service
