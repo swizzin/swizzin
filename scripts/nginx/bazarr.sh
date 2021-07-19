04935cce30a4240abbe1b7bb754f28e8ea55f7b0
Flying Sausages <flying_sausages@protonmail.com>
2021-07-19 18:00:40 +0200

    fix: update nginx config

--- scripts/nginx/bazarr.sh
+++ scripts/nginx/bazarr.sh
@@ -17,11 +17,25 @@ if [[ $isactive == "active" ]]; then
 fi
 
 cat > /etc/nginx/apps/bazarr.conf << BAZN
-location /bazarr {
-  include /etc/nginx/snippets/proxy.conf;
-  proxy_pass http://127.0.0.1:6767/bazarr;
-  auth_basic "What's the password?";
-  auth_basic_user_file /etc/htpasswd.d/htpasswd.${user};
+location /bazarr/ {
+    proxy_pass              http://127.0.0.1:6767/bazarr/;
+    proxy_set_header        X-Real-IP               \$remote_addr;
+    proxy_set_header        Host                    \$http_host;
+    proxy_set_header        X-Forwarded-For         \$proxy_add_x_forwarded_for;
+    proxy_set_header        X-Forwarded-Proto       \$scheme;
+    proxy_http_version      1.1;
+    proxy_set_header        Upgrade                 \$http_upgrade;
+    proxy_set_header        Connection              "Upgrade";
+    proxy_redirect off;
+
+    auth_basic              "What's the password?";
+    auth_basic_user_file    /etc/htpasswd.d/htpasswd.${user};
+    
+    # Allow the Bazarr API through if you enable Auth on the block above
+    location /bazarr/api {
+        auth_request off;
+        proxy_pass http://127.0.0.1:6767/bazarr/api;
+
 }
 
 BAZN
