1f97ac4d71d7eba9032e6262b83ae0b7ef2290c7
Brett Petch <bpetch@uwo.ca>
2021-07-21 17:55:02 -0400

    fix: app port

--- scripts/tests/readarr.sh
+++ scripts/tests/readarr.sh
@@ -4,7 +4,7 @@
 . /etc/swizzin/sources/functions/tests
 
 check_service "readarr" || BAD=true
-check_port_curl "9696" || BAD=true
+check_port_curl "8787" || BAD=true
 check_nginx "readarr" || BAD=true
 
 evaluate_bad
