ce9efe22c635839cfca8c6c54897889711088995
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-07-24 21:08:05 -0700

    should remove jdownloader for EACH user now

--- scripts/remove/jdownloader.sh
+++ scripts/remove/jdownloader.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-# TODO: Make this check for all swizzin users, and iterate and remove all JDownloader installations.
-# TODO: Do a check to see if they want to purge their JDownloader configurations.
-systemctl disable -q --now jdownloader@$1
-echo_progress_start "Removing JDownloader"
-rm_if_exists -r /home/$1/jd
+# JDownloader remover for swizzin
+# Author: Aethaeran
+
+# Import swizzin utils
+. /etc/swizzin/sources/functions/utils
+
+# this will check for all swizzin users, and iterate and remove all JDownloader installations.
+users=("$(_get_user_list)")
+for user in "${users[@]}"; do
+    echo_progress_start "Removing JDownloader for $user..."
+    systemctl disable -q --now jdownloader@"$user"
+    # TODO: Do a check to see if they want to purge their JDownloader configurations.
+    rm_if_exists -r /home/"$user"/jd
+    echo_progress_done
+done
+echo_progress_start "Removing shared JDownloader files..."
 rm_if_exists -r /etc/systemd/system/jdownloader@.service
 rm_if_exists /install/.jdownloader.lock
 echo_progress_done
