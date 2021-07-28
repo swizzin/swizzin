802878b33ab7b5d640be4757fd10e697a59b5be0
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-07-28 13:14:42 -0700

    disabled a SC2068 that was causing a list to be interpreted as a string

--- scripts/remove/jdownloader.sh
+++ scripts/remove/jdownloader.sh
@@ -7,7 +7,9 @@
 
 # this will check for all swizzin users, and iterate and remove all JDownloader installations.
 users=("$(_get_user_list)")
-for user in "${users[@]}"; do
+# The following line cannot follow SC2068 because it will cause the list to become a string.
+# shellcheck disable=SC2068
+for user in ${users[@]}; do
     echo_progress_start "Removing JDownloader for $user..."
     systemctl disable -q --now jdownloader@"$user"
     # TODO: Do a check to see if they want to purge their JDownloader configurations.
