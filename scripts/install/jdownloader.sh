e46d5395c91a683fa7b15d81c74389872e1ecf45
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-08-03 09:53:54 -0700

    using jdownloader logs instead of making our own tmp log

--- scripts/install/jdownloader.sh
+++ scripts/install/jdownloader.sh
@@ -63,8 +63,6 @@ function install_jdownloader() {
     fi
 
     command="java -jar $JD_HOME/JDownloader.jar -norestart"
-    # TODO: The following line can probably use the most recent JDownloader log instead. /home/$user/jd2/logs/$get_most_recent_dir_in_folder/Log.L.log.0
-    tmp_log="/tmp/jdownloader_install-${user}.log"
 
     # TODO: Currently, we need something here to disable all currently running JDownloader installations, or the MyJD verification logic will cause a loop. Would rather we didn't.
     for each_user in "${users[@]}"; do # disable all instances
