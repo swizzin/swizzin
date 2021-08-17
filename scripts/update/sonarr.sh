15b6443418aa40b9cd744395ae15d416c4db66ca
liaralabs <liara@swizzin.ltd>
2021-08-17 12:13:37 -0700

    Add updater

--- scripts/update/sonarr.sh
+++ scripts/update/sonarr.sh
@@ -42,3 +42,47 @@ if [[ -f /install/.sonarrv3.lock ]]; then
     rm /install/.sonarrv3.lock
     touch /install/.sonarr.lock
 fi
+if [[ -f /install/.sonarr.lock && $(dpkg -l | grep sonarr > /dev/null 2>&1) ]]; then
+    echo_info "Migrating Sonarr away from apt management"
+    cp -a /usr/lib/sonarr/bin /opt/Sonarr
+    cp /usr/lib/systemd/system/sonarr.service /etc/systemd/system
+
+    #Remove comments
+    sed -i 's/^#/d' /etc/systemd/system/sonarr.service
+    #Update binary location
+    sed -i 's|/usr/lib/sonarr/bin|/opt/Sonarr|' /etc/systemd/system/sonarr.service
+
+    #Mark depends as manually installed
+    LIST='mono-runtime
+    ca-certificates-mono
+    libmono-system-net-http4.0-cil
+    libmono-corlib4.5-cil
+    libmono-microsoft-csharp4.0-cil
+    libmono-posix4.0-cil
+    libmono-system-componentmodel-dataannotations4.0-cil
+    libmono-system-configuration-install4.0-cil
+    libmono-system-configuration4.0-cil
+    libmono-system-core4.0-cil
+    libmono-system-data-datasetextensions4.0-cil
+    libmono-system-data4.0-cil
+    libmono-system-identitymodel4.0-cil
+    libmono-system-io-compression4.0-cil
+    libmono-system-numerics4.0-cil
+    libmono-system-runtime-serialization4.0-cil
+    libmono-system-security4.0-cil
+    libmono-system-servicemodel4.0a-cil
+    libmono-system-serviceprocess4.0-cil
+    libmono-system-transactions4.0-cil
+    libmono-system-web4.0-cil
+    libmono-system-xml-linq4.0-cil
+    libmono-system-xml4.0-cil
+    libmono-system4.0-cil
+    sqlite3
+    mediainfo'
+
+    apt_install ${LIST}
+
+    apt_remove sonarr
+    systemctl daemon-reload
+    systemctl try-restart sonarr
+fi
