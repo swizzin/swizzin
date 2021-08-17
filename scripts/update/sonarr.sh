2adc84ca12538c93a22184f4618067cc3bedafe3
liaralabs <liara@swizzin.ltd>
2021-08-16 20:57:24 -0700

    hotfix(sonarr): move mono update functions to sonarr as no other cases exist

--- scripts/update/sonarr.sh
+++ scripts/update/sonarr.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+if [[ -f /install/.sonarr.lock ]]; then
+    #Check if mono needs an update
+    . /etc/swizzin/sources/functions/mono
+    mono_repo_update
+    systemctl try-restart sonarr
+
+    #Ensure Sonarr repo key is up-to-date
+    if ! apt-key adv --list-public-keys 2> /dev/null | grep -q A236C58F409091A18ACA53CBEBFF6B99D9B78493 >> $log 2>&1; then
+        distribution=$(_os_distro)
+        if [[ $distribution == "ubuntu" ]]; then
+            apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493 > /dev/null 2>&1
+        elif [[ $distribution == "debian" ]]; then
+            #buster friendly
+            apt-key --keyring /etc/apt/trusted.gpg.d/nzbdrone.gpg adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493
+        fi
+    fi
+fi
+
 if dpkg -l | grep nzbdrone > /dev/null 2>&1; then
     v2present=true
     echo_warn "Sonarr v2 is obsolete and end-of-life. Please upgrade your Sonarr to v3 using \`box upgrade sonarr\`."
