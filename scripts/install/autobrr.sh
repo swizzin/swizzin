40c774c995b436a265a45a9bd2587c38a7faee58
Flying Sausages <flying_sausages@protonmail.com>
2021-08-19 21:52:00 +0200

    refactor: autobrr installer layout

--- scripts/install/autobrr.sh
+++ scripts/install/autobrr.sh
@@ -7,26 +7,61 @@
 #shellcheck source=sources/functions/autobrr
 . /etc/swizzin/sources/functions/autobrr
 
-# install needed tools
-apt_install curl
-
-users=($(_get_user_list))
-
-_autobrr_user_config() {
-    echo_progress_start "Configuring autobrr"
-
-    # get random available port
-    port=$(port 10000 12000)
-
-    # generate a sessionSecret
-    sessionSecret="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
-
-    if [ ! -d "/home/$user/.config/autobrr/" ]; then
-        mkdir -p "/home/$user/.config/autobrr/"
-        chown -R "$user": "/home/$user/.config"
+_systemd() {
+    type=simple
+    if [[ $(systemctl --version | awk 'NR==1 {print $2}') -ge 240 ]]; then
+        type=exec
     fi
 
-    cat > "/home/$user/.config/autobrr/config.toml" << CFG
+    echo_progress_start "Installing Systemd service"
+    cat > /etc/systemd/system/autobrr@.service << EOF
+[Unit]
+Description=autobrr service for %i
+After=syslog.target network.target
+
+[Service]
+Type=$type
+User=%i
+Group=%i
+ExecStart=/usr/bin/autobrr --config=/home/%i/.config/autobrr/
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    echo_progress_done "Service installed"
+}
+
+_nginx() {
+    if [[ -f /install/.nginx.lock ]]; then
+        echo_progress_start "Configuring nginx"
+        bash /etc/swizzin/scripts/nginx/autobrr.sh
+        systemctl reload nginx >> $log 2>&1
+        echo_progress_done "Nginx configured"
+    fi
+}
+
+# This method will perform all logic related to user accounts. For each user when installed, and for only one user when via box adduser
+_add_users() {
+    for user in "${users[@]}"; do
+        echo_progress_start "Enabling autobrr for $user"
+
+        _get_user_password "$user" | /usr/bin/autobrrctl --config "/home/$user/.config/autobrr" create-user "$user" || {
+            echo_error "Failed to execute autobrrctl command"
+            exit 1
+        }
+
+        # get random available port
+        port=$(port 10000 12000)
+
+        # generate a sessionSecret
+        sessionSecret="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
+
+        if [ ! -d "/home/$user/.config/autobrr/" ]; then
+            mkdir -p "/home/$user/.config/autobrr/"
+            chown -R "$user": "/home/$user/.config"
+        fi
+
+        cat > "/home/$user/.config/autobrr/config.toml" << CFG
 # config.toml
 # Hostname / IP
 #
@@ -63,86 +98,37 @@ logLevel = "DEBUG"
 #
 sessionSecret = "${sessionSecret}"
 CFG
+        chown -R "$user": "/home/$user/.config/autobrr"
 
-    chown -R "$user": "/home/$user/.config/autobrr"
+        # Only run the nginx hook if a new user is being added, otherwise it will be ran for all users at the end of the installer
+        if [[ ${#users[@]} -eq 1 ]]; then
+            if [[ -f /install/.nginx.lock ]]; then
+                bash /etc/swizzin/scripts/nginx/autobrr.sh
+                systemctl reload nginx
+            fi
+        fi
 
-    echo_progress_done
+        systemctl enable -q --now autobrr@"${user}" 2>&1 | tee -a $log
+        echo_progress_done "Started autobrr for $user"
 
+    done
 }
 
-if [[ -n $1 ]]; then
-    user=$1
-    _autobrr_user_config ${user}
-    if [[ -f /install/.nginx.lock ]]; then
-        echo_progress_start "Configuring nginx"
-        bash /etc/swizzin/scripts/nginx/autobrr.sh
-        systemctl reload nginx
-        echo_progress_done
-    fi
-    systemctl enable -q --now autobrr@${user} 2>&1 | tee -a $log
+##############################
 
+# If `box adduser` is running, only do _add_users and then exit. Otherwise, run the whole installer
+if [ -n "$1" ]; then
+    users=("$1")
+    _add_users
     exit 0
 fi
 
-_autobrr_create_users() {
-    # use autobrrctl to add user into database
-    echo_progress_start "Add user"
-
-    for user in "${users[@]}"; do
-        echo_log_only "Adding user $user"
-
-        # the password needs to be created with argon2 so we use autobrrctl to create the user
-        # using sqlite3 directly was not an option
-        _get_user_password "$user" | /usr/bin/autobrrctl --config "/home/$user/.config/autobrr" create-user "$user"
-    done
-}
-
-_systemd_autobrr() {
-    type=simple
-    if [[ $(systemctl --version | awk 'NR==1 {print $2}') -ge 240 ]]; then
-        type=exec
-    fi
-
-    echo_progress_start "Installing Systemd service"
-    cat > /etc/systemd/system/autobrr@.service << EOF
-[Unit]
-Description=autobrr service for %i
-After=syslog.target network.target
-
-[Service]
-Type=$type
-User=%i
-Group=%i
-ExecStart=/usr/bin/autobrr --config=/home/%i/.config/autobrr/
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-    for user in ${users[@]}; do
-        echo_progress_start "Enabling autobrr for $user"
-        _autobrr_user_config ${user}
-        systemctl enable -q --now autobrr@${user} 2>&1 | tee -a $log
-        echo_progress_done "Started autobrr for $user"
-        sleep 3
-    done
-}
-
-_nginx_autobrr() {
-    if [[ -f /install/.nginx.lock ]]; then
-        echo_progress_start "Configuring nginx"
-        bash /etc/swizzin/scripts/nginx/autobrr.sh
-        systemctl reload nginx >> $log 2>&1
-        echo_progress_done "Nginx configured"
-    fi
-}
-
 autobrr_download_latest
+_systemd
 
-_systemd_autobrr
-_autobrr_create_users
-
-_nginx_autobrr
+readarray -t users < <(_get_user_list)
+_add_users
+_nginx
 
 touch "/install/.autobrr.lock"
 echo_success "autobrr installed"
