9d8e226d1ac2f09fe82c679cb4d88986cd5e99f4
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-08-19 14:57:13 -0700

    Update remove/jdownloader.sh

--- scripts/remove/lazylibrarian.sh
+++ scripts/remove/lazylibrarian.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 # LazyLibrarian remove script for swizzin
 # Author: Aethaeran
+app_name=lazylibrarian
+pretty_name=LazyLibrarian
+app_dir="/opt/$app_name"
+config_dir="/home/$master/.config/lazylibrarian"
+master=
+
+rm -rv "$app_dir"
+rm -rv "$config_dir"
+rm -v "/etc/systemd/system/$app_name.service"
+#rm -v "/etc/systemd/system/$app_name@.service"
+rm -v "/etc/nginx/apps/$app_name.conf"
