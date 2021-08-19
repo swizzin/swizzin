4f97338713729eff9a5d1dadc30da2dafcfd0c4a
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-08-19 14:49:36 -0700

    Update lazylibrarian.sh

--- scripts/nginx/lazylibrarian.sh
+++ scripts/nginx/lazylibrarian.sh
@@ -2,5 +2,15 @@
 # LazyLibrarian nginx script for swizzin
 # Author: Aethaeran
 
-    cat >/etc/systemd/system/"$app_name"@.service <<EOF
-EOF
+cat > "/etc/nginx/apps/$app_name.conf" <<NGINXCONF
+location /lazylibrarian {
+        proxy_bind              $server_addr;
+        proxy_pass              http://127.0.0.1:5299;
+        proxy_set_header        Host            $http_host;
+        proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header        X-Scheme        $scheme;
+        proxy_set_header        X-Script-Name   /lazylibrarian;  # IMPORTANT: path has NO trailing slash
+}
+NGINXCONF
+
+# TODO: Verify if "Web Root" needs to be added to LazyLib config
