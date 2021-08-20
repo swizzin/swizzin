9ea9d18d80221bef2b2ac344c3143498e5e2bbd4
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-08-19 21:28:47 -0700

    Update install

--- scripts/install/lazylibrarian.sh
+++ scripts/install/lazylibrarian.sh
@@ -39,35 +39,41 @@ app_dir="/opt/$app_name"
 ##########################################################################
 
 function _dependencies() {
-    # TODO: venv please
+    # TODO: Create a venv for LazyLibrarian
+    # TODO: When run from CLI LazyLibrarian throws the following warnings early on. Add them to venv to ensure LazyLib has full functionality.
     # 19-Aug-2021 17:43:39 - WARNING :: MAIN : __init__.py:initialize:976 : apprise: library missing
     #19-Aug-2021 17:43:39 - WARNING :: MAIN : __init__.py:initialize:976 : pyOpenSSL: module missing
     #19-Aug-2021 17:43:40 - WARNING :: MAIN : LazyLibrarian.py:main:276 : Looking for Apprise library: No module named 'apprise'
-    echo "This is where it would be installing dependencies..."
+    echo_progress_start "Installing dependencies for $pretty_name..."
     #    Install Python 2 v2.6 or higher, or Python 3 v3.5 or higher
     # TODO: https://lazylibrarian.gitlab.io/config_rtorrent/ claims it might require libtorrent
+    echo_progress_done "Dependencies installed."
 }
 
 function _install() {
-    echo "This is where it would be installing..."
+    echo_progress_start "Installing $pretty_name..."
     mkdir -p "$app_dir"
     mkdir -p "$data_dir"
     #    Git clone/extract LL wherever you like
+    # shellcheck disable=2154           # log variable is inherited from box itself.
+    # TODO: Should add some logic in here to verify git cloned successfully. It passed by here without doing so on me once.
     git clone "https://gitlab.com/LazyLibrarian/LazyLibrarian.git" "$app_dir" >>$log 2>&1
     # chown
     chown -R "$master": "$app_dir"
     chown -R "$master": "$data_dir"
+    echo_progress_done "$pretty_name installed."
 }
 
 function _configure() {
     #    Fill in all the config (see docs for full configuration)
-    echo "This is where it would be configuring for $user..."
+    echo_progress_start "Configuring $pretty_name..."
     # TODO: this is gross. Would much rather pass the single config with the api
     # https://lazylibrarian.gitlab.io/api/
     cat >"$app_dir/config.ini" << EOF
 [General]
 http_root = /lazylibrarian
 EOF
+    echo_progress_done "$pretty_name configured."
 }
 
 function _systemd() {
@@ -82,43 +88,45 @@ function _systemd() {
     #--datadir - Path to the datadir used to store database, config, cache - Default is to use program directory if this is unset
     #--config - Path to config.ini - Default is to use datadir
     #    Run "python LazyLibrarian.py -d" to start in daemon mode
+    echo_progress_start "Configuring $pretty_name systemd service..."
     cat >"/etc/systemd/system/$app_name.service" <<EOF
 [Unit]
 Description=LazyLibrarian
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/python3 \
-$app_dir/LazyLibrarian.py
-Type=simple
 User=$master
 Group=$master
+Type=simple
+ExecStart=/usr/bin/python3 \
+$app_dir/LazyLibrarian.py
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
 EOF
+    echo_progress_done "$pretty_name systemd service configured."
 
-    echo_progress_start "Enabling service $app_name"
+    echo_progress_start "Enabling $app_name's systemd service..."
     systemctl enable --quiet --now "$app_name"
-    echo_progress_done
+    echo_progress_done "$pretty_name's systemd service enabled."
 }
 
 function _nginx() {
     if [[ -f /install/.nginx.lock ]]; then
-        echo_progress_start "Installing nginx configuration"
+        echo_progress_start "Installing $app_name's nginx configuration..."
         bash "/usr/local/bin/swizzin/nginx/$app_name.sh"
-        # shellcheck disable=2154 # log variable is inherited from box itself.
+        # shellcheck disable=2154           # log variable is inherited from box itself.
         systemctl reload nginx >>"$log" 2>&1
-        echo_progress_done
+        echo_progress_done "$app_name's nginx configuration installed."
     else
         echo_info "$pretty_name will run on port $default_port"
     fi
 }
 
 function _finishing_touch() {
-    touch "/install/.$app_name.lock"       # Create lock file so that swizzin knows the app is installed.
-    echo_success "$pretty_name installed." # Winner winner. Chicken dinner.
+    touch "/install/.$app_name.lock"        # Create lock file so that swizzin knows the app is installed.
+    echo_success "$pretty_name installed."  # Winner winner. Chicken dinner.
 }
 
 ##########################################################################
