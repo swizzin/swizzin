5c210bf8fc1989865cc23902b09576744e19b6cc
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-08-19 18:12:08 -0700

    Update install

--- scripts/install/lazylibrarian.sh
+++ scripts/install/lazylibrarian.sh
@@ -24,11 +24,25 @@
 . /etc/swizzin/sources/functions/utils
 
 ##########################################################################
+# Variables
+##########################################################################
+
+app_name="lazylibrarian"
+pretty_name="LazyLibrarian"
+default_port="5299"
+master=$(_get_master_username)
+data_dir="/home/$master/.config/lazylibrarian"
+app_dir="/opt/$app_name"
+
+##########################################################################
 # Functions
 ##########################################################################
 
 function _dependencies() {
     # TODO: venv please
+    # 19-Aug-2021 17:43:39 - WARNING :: MAIN : __init__.py:initialize:976 : apprise: library missing
+    #19-Aug-2021 17:43:39 - WARNING :: MAIN : __init__.py:initialize:976 : pyOpenSSL: module missing
+    #19-Aug-2021 17:43:40 - WARNING :: MAIN : LazyLibrarian.py:main:276 : Looking for Apprise library: No module named 'apprise'
     echo "This is where it would be installing dependencies..."
     #    Install Python 2 v2.6 or higher, or Python 3 v3.5 or higher
     # TODO: https://lazylibrarian.gitlab.io/config_rtorrent/ claims it might require libtorrent
@@ -39,7 +53,7 @@ function _install() {
     mkdir -p "$app_dir"
     mkdir -p "$data_dir"
     #    Git clone/extract LL wherever you like
-    git clone "https://gitlab.com/LazyLibrarian/LazyLibrarian.git" "$app_dir"
+    git clone "https://gitlab.com/LazyLibrarian/LazyLibrarian.git" "$app_dir" >>$log 2>&1
     # chown
     chown -R "$master": "$app_dir"
     chown -R "$master": "$data_dir"
@@ -62,20 +76,17 @@ function _systemd() {
     #--datadir - Path to the datadir used to store database, config, cache - Default is to use program directory if this is unset
     #--config - Path to config.ini - Default is to use datadir
     #    Run "python LazyLibrarian.py -d" to start in daemon mode
-    cat >/etc/systemd/system/"$app_name".service <<EOF
+    cat >"/etc/systemd/system/$app_name.service" <<EOF
 [Unit]
 Description=LazyLibrarian
 After=network.target
 
 [Service]
 ExecStart=/usr/bin/python3 \
-$app_dir/LazyLibrarian.py \
---nolaunch \
---daemon \
---datadir=$data_dir
+$app_dir/LazyLibrarian.py
 Type=simple
-User=htpc
-Group=htpc
+User=$master
+Group=$master
 Restart=on-failure
 
 [Install]
@@ -87,12 +98,12 @@ EOF
     echo_progress_done
 }
 
-function _nginx_sonarr() {
+function _nginx() {
     if [[ -f /install/.nginx.lock ]]; then
         echo_progress_start "Installing nginx configuration"
         bash "/usr/local/bin/swizzin/nginx/$app_name.sh"
         # shellcheck disable=2154 # log variable is inherited from box itself.
-        systemctl reload nginx >> "$log" 2>&1
+        systemctl reload nginx >>"$log" 2>&1
         echo_progress_done
     else
         echo_info "$pretty_name will run on port $default_port"
@@ -105,14 +116,8 @@ function _finishing_touch() {
 }
 
 ##########################################################################
-# Script Main
+# Main
 ##########################################################################
-app_name="lazylibrarian"
-pretty_name="LazyLibrarian"
-default_port="5299"
-master=$(_get_master_username)
-data_dir="/home/$master/.config/lazylibrarian"
-app_dir="/opt/$app_name"
 
 _dependencies
 
