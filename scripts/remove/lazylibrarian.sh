67ac363931d20bb350e3b4e37fb1779b04a696c0
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-08-19 18:12:01 -0700

    Update remove

--- scripts/remove/lazylibrarian.sh
+++ scripts/remove/lazylibrarian.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 # LazyLibrarian remove script for swizzin
 # Author: Aethaeran
+
+##########################################################################
+# Import Sources
+##########################################################################
+
+. /etc/swizzin/sources/functions/utils
+
+##########################################################################
+# Variables
+##########################################################################
+
 app_name=lazylibrarian
 pretty_name=LazyLibrarian
 app_dir="/opt/$app_name"
+master=$(_get_master_username)
 config_dir="/home/$master/.config/lazylibrarian"
-master=
 
-rm -rv "$app_dir"
-rm -rv "$config_dir"
-rm -v "/etc/systemd/system/$app_name.service"
-#rm -v "/etc/systemd/system/$app_name@.service"
-rm -v "/etc/nginx/apps/$app_name.conf"
+##########################################################################
+# Main
+##########################################################################
+
+rm -rv "$app_dir" >> $log 2>&1
+
+rm -rv "$config_dir" >> $log 2>&1
+
+systemctl disable --now --quiet $app_name
+rm -v "/etc/systemd/system/$app_name.service" >> $log 2>&1
+#rm -v "/etc/systemd/system/$app_name@.service" >> $log 2>&1
+
+rm -v "/etc/nginx/apps/$app_name.conf" >> $log 2>&1
+
+rm -v "/install/.$app_name.lock" >> $log 2>&1
