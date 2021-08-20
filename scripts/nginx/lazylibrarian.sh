fe3991fb50747c7cb55589d06d822cd14ebad0cd
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-08-20 13:51:15 -0700

    simple logic to verify nginx conf needs creating in nginx/lazylibrarian.sh

--- scripts/nginx/lazylibrarian.sh
+++ scripts/nginx/lazylibrarian.sh
@@ -12,21 +12,25 @@
 # Variables
 ##########################################################################
 
-app_name=lazylibrarian
+app_name="lazylibrarian"
+pretty_name="LazyLibrarian"
+nginx_conf="/etc/nginx/apps/$app_name.conf"
 
 ##########################################################################
 # Main
 ##########################################################################
 
-# TODO: Simple logic to verify this needs creating.
-
-cat > "/etc/nginx/apps/$app_name.conf" << 'EOF'
-location /lazylibrarian {
-        proxy_bind              $server_addr;
-        proxy_pass              http://127.0.0.1:5299;
-        proxy_set_header        Host            $http_host;
-        proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header        X-Scheme        $scheme;
-        proxy_set_header        X-Script-Name   /lazylibrarian;  # IMPORTANT: path has NO trailing slash
-}
+if [[ ! -e "$nginx_conf" ]];then
+    cat > "$nginx_conf" << 'EOF'
+    location /lazylibrarian {
+            proxy_bind              $server_addr;
+            proxy_pass              http://127.0.0.1:5299;
+            proxy_set_header        Host            $http_host;
+            proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header        X-Scheme        $scheme;
+            proxy_set_header        X-Script-Name   /lazylibrarian;  # IMPORTANT: path has NO trailing slash
+    }
 EOF
+else
+    echo_info "$pretty_name's nginx configuration already existed."
+fi
