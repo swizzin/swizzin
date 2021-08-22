847ac421a336629e572a605b0d4532a5f4506f61
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-08-22 15:36:14 -0700

    Revert "fix: test"
    This reverts commit 374ee9a3ba49b77357c6a478b24d7ebfe165c9e2.

--- scripts/tests/lazylibrarian.sh
+++ scripts/tests/lazylibrarian.sh
@@ -1,17 +1,45 @@
 #!/usr/bin/env bash
 # LazyLibrarian test script for swizzin
-# Author: Aethaeran 2021
-# GPLv3
+# Author: Aethaeran
+
+##########################################################################
+# References
+##########################################################################
+
+# https://github.com/swizzin/swizzin/blob/master/scripts/tests/_basetest.sh
+
+##########################################################################
+# Import Sources
+##########################################################################
 
 #shellcheck source=sources/functions/tests
 . /etc/swizzin/sources/functions/tests
 
+##########################################################################
+# Variables
+##########################################################################
+
 app_name="lazylibrarian"
 
-check_port_curl_lazylib() {
+##########################################################################
+# Functions
+##########################################################################
+
+# TODO: Is this change necessary? Maybe I just did the nginx configuration incorrectly.
+
+check_port_curl() {
     echo_progress_start "Checking if port $1 is reachable via curl"
-    # override necessary for baseurl inclusion
-    curl -sSfLk http://127.0.0.1:"$port/$app_name" -o /dev/null || {
+    if [ "$1" -eq "$1" ] 2> /dev/null; then
+        port=$1
+    else
+        port=$(get_port "$1") || {
+            echo_warn "Couldn't guess port"
+            return 1
+        }
+    fi
+    extra_params="$2"
+    # shellcheck disable=SC2086 # We want splitting on the extra params variable. So the warning is void here.
+    curl -sSfLk $extra_params http://127.0.0.1:"$port/$app_name" -o /dev/null || {
         echo_warn "Querying http://127.0.0.1:$port/$app_name failed"
         echo
         return 1
@@ -19,9 +47,15 @@ check_port_curl_lazylib() {
     echo_progress_done
 }
 
+##########################################################################
+# Main
+##########################################################################
+
+# run all functions, if one fails, mark as bad
 check_service "$app_name" || BAD="true"
-check_port 5299 || BAD="true"
-check_port_curl_lazylib 5299 || BAD="true"
+check_port "$app_name" || BAD="true"
+check_port_curl "$app_name" || BAD="true"
+# shellcheck disable=SC2034 # $BAD is used in evaluate_bad. So the warning is void here.
 check_nginx "$app_name" || BAD="true"
 
 evaluate_bad
