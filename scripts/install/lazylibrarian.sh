de05d97d90e2afdcdac00b193efa072d13cb0d17
Flying Sausages <flying_sausages@protonmail.com>
2021-08-22 13:14:08 +0200

    fix: echos

--- scripts/install/lazylibrarian.sh
+++ scripts/install/lazylibrarian.sh
@@ -11,7 +11,6 @@
 # https://www.reddit.com/r/LazyLibrarian/comments/jw5e22/lazylibrarian_calibre_calibreweb_booksonic/
 # https://github.com/lazylibrarian/LazyLibrarian
 # https://gitlab.com/LazyLibrarian/LazyLibrarian
-# https://lazylibrarian.gitlab.io/config_commandline/
 # https://github.com/swizzin/swizzin/pull/743
 # https://lazylibrarian.gitlab.io/config_users/
 # https://lazylibrarian.gitlab.io/config_downloaders/
@@ -56,11 +55,9 @@ pip_reqs='apprise pyOpenSSL'
 ##########################################################################
 
 function _dependencies() {
-    echo_info "Installing dependencies for $pretty_name..."
+    echo_info "Installing dependencies for $pretty_name"
 
-    echo_progress_start "Creating $pretty_name venv"
     python3_venv "$user" "$app_name"
-    echo_progress_done
 
     echo_progress_start "Installing python dependencies to venv"
     # shellcheck disable=2154           # log variable is inherited from box itself.
@@ -73,25 +70,25 @@ function _dependencies() {
 }
 
 function _install() {
-    echo_progress_start "Installing $pretty_name..."
+    echo_progress_start "Cloning $pretty_name source"
     mkdir -p "$app_dir"
     mkdir -p "$data_dir"
     #    Git clone/extract LL wherever you like
     # shellcheck disable=2154           # log variable is inherited from box itself.
     git clone "https://gitlab.com/LazyLibrarian/LazyLibrarian.git" "$app_dir" >> "$log" 2>&1
     if [[ ! -e "$app_dir/LazyLibrarian.py" ]]; then
-        ecoh_error "Git clone unsuccessful. Try running the box install again."
+        echo_error "Git clone unsuccessful. Try running the box install again."
         exit 1
     fi
     chown "$user": "$config_dir" # Ensure correct owner\group on config_dir
     chown -R "$user": "$app_dir" # Change owner\group recursively for new dirs.
     chown -R "$user": "$data_dir"
-    echo_progress_done
+    echo_progress_done "Source cloned"
 }
 
 function _configure() {
     #    Fill in all the config (see docs for full configuration)
-    echo_progress_start "Configuring $pretty_name..."
+    echo_progress_start "Configuring $pretty_name"
     cat > "$data_dir/config.ini" << EOF
 [General]
 http_root = /lazylibrarian
@@ -103,18 +100,8 @@ EOF
 }
 
 function _systemd() {
-    # CommandLine Options¶
-    #-d --daemon - Run the server as a daemon - not available on windows
-    #-q --quiet - Don't log to console
-    #-p --pidfile - Store the process id in this file
-    #--debug - Show debug log messages
-    #--nolaunch - Don't launch browser
-    #--update - Update to latest version from GitHub at start-up (only git installations)
-    #--port - Force webserver to listen on this port
-    #--datadir - Path to the datadir used to store database, config, cache - Default is to use program directory if this is unset
-    #--config - Path to config.ini - Default is to use datadir
-    #    Run "python LazyLibrarian.py -d" to start in daemon mode
-    echo_progress_start "Configuring $pretty_name systemd service..."
+    echo_progress_start "Configuring $pretty_name systemd service"
+    # https://lazylibrarian.gitlab.io/config_commandline/
     cat > "/etc/systemd/system/$app_name.service" << EOF
 [Unit]
 Description=LazyLibrarian
@@ -132,16 +119,14 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.target
 EOF
-    echo_progress_done
 
-    echo_progress_start "Enabling $app_name's systemd service..."
     systemctl enable --quiet --now "$app_name"
     echo_progress_done
 }
 
 function _nginx() {
     if [[ -f /install/.nginx.lock ]]; then
-        echo_progress_start "Installing $app_name's nginx configuration..."
+        echo_progress_start "Installing $app_name's nginx configuration"
         bash "/usr/local/bin/swizzin/nginx/$app_name.sh"
         # shellcheck disable=2154           # log variable is inherited from box itself.
         systemctl reload nginx >> "$log" 2>&1
@@ -154,6 +139,7 @@ function _nginx() {
 function _finishing_touch() {
     touch "/install/.$app_name.lock"       # Create lock file so that swizzin knows the app is installed.
     echo_success "$pretty_name installed." # Winner winner. Chicken dinner.
+    echo_info "Please make sure to finalise the setup in $pretty_name's web interface"
 }
 
 ##########################################################################
