8b5d2b1f8e57bef5f3d9c994a06501d9ed14a356
Flying Sausages <flying_sausages@protonmail.com>
2021-08-22 14:00:30 +0200

    fix: nginx script

--- scripts/nginx/lazylibrarian.sh
+++ scripts/nginx/lazylibrarian.sh
@@ -1,36 +1,15 @@
 #!/bin/bash
 # LazyLibrarian nginx script for swizzin
-# Author: Aethaeran
+# Author: Aethaeran 2021
+# GPLv3
 
-##########################################################################
-# Import Sources
-##########################################################################
-
-. /etc/swizzin/sources/functions/utils
-
-##########################################################################
-# Variables
-##########################################################################
-
-app_name="lazylibrarian"
-pretty_name="LazyLibrarian"
-nginx_conf="/etc/nginx/apps/$app_name.conf"
-
-##########################################################################
-# Main
-##########################################################################
-
-if [[ ! -e "$nginx_conf" ]]; then
-    cat > "$nginx_conf" << 'EOF'
-    location /lazylibrarian {
-            proxy_bind              $server_addr;
-            proxy_pass              http://127.0.0.1:5299;
-            proxy_set_header        Host            $http_host;
-            proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header        X-Scheme        $scheme;
-            proxy_set_header        X-Script-Name   /lazylibrarian;  # IMPORTANT: path has NO trailing slash
-    }
+cat > "/etc/nginx/apps/lazylibrarian.conf" << 'EOF'
+location /lazylibrarian {
+    proxy_bind          $server_addr;
+    proxy_pass          http://127.0.0.1:5299;
+    proxy_set_header    Host            \$http_host;
+    proxy_set_header    X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header    X-Scheme        \$scheme;
+    proxy_set_header    X-Script-Name   /lazylibrarian;  # IMPORTANT: path has NO trailing slash
+}
 EOF
-else
-    echo_info "$pretty_name's nginx configuration already existed."
-fi
