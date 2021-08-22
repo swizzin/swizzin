e84d184cf20218b2c98ca5fe55b7d80079e1cda4
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-08-22 15:37:41 -0700

    Revert "fix: entire remover"
    This reverts commit 6015bacd3c3a58e95778008e74c99ffd209c0249.

--- scripts/remove/lazylibrarian.sh
+++ scripts/remove/lazylibrarian.sh
@@ -1,21 +1,80 @@
 #!/bin/bash
 # LazyLibrarian remove script for swizzin
-# Author: Aethaeran 2021
-# GPLv3
+# Author: Aethaeran
+
+##########################################################################
+# Import Sources
+##########################################################################
+
+. /etc/swizzin/sources/functions/utils
+
+##########################################################################
+# Variables
+##########################################################################
 
 app_name="lazylibrarian"
-user="$(swizdb get $app_name/owner)"
+pretty_name="LazyLibrarian"
+app_dir="/opt/$app_name"
+master=$(_get_master_username)
+config_dir="/home/$master/.config/$app_name"
+venv_dir="/opt/.venv/$app_name"
 
-rm -rf "/opt/.venv/$app_name"
-rm -rf "/opt/$app_name"
-rm -rf "/home/$user/.config/$app_name"
+##########################################################################
+# Main
+##########################################################################
 
-systemctl disable --now "$app_name" --quiet
-rm -rf "/etc/systemd/system/$app_name.service"
-
-if [[ -f /install/.nginx.lock ]]; then
-    rm -rf "/etc/nginx/apps/$app_name.conf"
-    systemctl reload nginx
+if [[ -e "$venv_dir" ]]; then
+    echo_progress_start "Removing $pretty_name venv..."
+    # shellcheck disable=2154 # log variable is inherited from box itself.
+    rm -rv "$venv_dir" >> "$log" 2>&1
+    echo_progress_done
+else
+    echo_info "$pretty_name venv_dir didn't exist."
 fi
 
-rm "/install/.$app_name.lock"
+if [[ -e "$app_dir" ]]; then
+    echo_progress_start "Removing $pretty_name installation..."
+    # shellcheck disable=2154 # log variable is inherited from box itself.
+    rm -rv "$app_dir" >> "$log" 2>&1
+    echo_progress_done
+else
+    echo_info "$pretty_name app_dir didn't exist."
+fi
+
+if [[ -e "$config_dir" ]]; then
+    echo_progress_start "Removing $pretty_name configuration..."
+    rm -rv "$config_dir" >> "$log" 2>&1
+    echo_progress_done
+else
+    echo_info "$pretty_name config_dir didn't exist."
+fi
+
+if [[ -e "/etc/systemd/system/$app_name.service" ]]; then
+    echo_progress_start "Removing $pretty_name systemd service..."
+    if [[ $(systemctl is-active "$app_name") == "active" ]]; then
+        systemctl stop "$app_name" --quiet
+    fi
+    if [[ $(systemctl is-enabled "$app_name") == "enabled" ]]; then
+        systemctl disable "$app_name" --quiet
+    fi
+    rm -v "/etc/systemd/system/$app_name.service" >> "$log" 2>&1
+    echo_progress_done
+else
+    echo_info "$pretty_name systemd service didn't exist."
+fi
+
+if [[ -e "/etc/nginx/apps/$app_name.conf" ]]; then
+    echo_progress_start "Removing $pretty_name nginx configuration..."
+    rm -v "/etc/nginx/apps/$app_name.conf" >> "$log" 2>&1
+    echo_progress_done
+else
+    echo_info "$pretty_name nginx configuration didn't exist."
+fi
+
+if [[ -e "/install/.$app_name.lock" ]]; then
+    echo_progress_start "Removing $pretty_name lock..."
+    rm -v "/install/.$app_name.lock" >> "$log" 2>&1
+    echo_progress_done
+else
+    echo_info "$pretty_name lock didn't exist."
+fi
