a7158894cf64789100df6bb290b2bc784568de06
liaralabs <30909703+liaralabs@users.noreply.github.com>
2021-08-22 17:51:09 -0700

    update(sonarr): use tar ball for installation to prevent unsupported repo issues (#741)
    * Utilize tarball for installation
    
    * Fix config ownership and wget redirect
    
    * Add updater
    
    * Update uninstall
    
    * Formatting
    
    * Remove repo key check
    
    * Fix update condition
    
    * Fix sed syntax
    
    * alternate command
    
    * Fix syntax
    
    * Don't purge, will delete conf dir
    
    * Remove repo files
    
    * Updater systemd logic
    
    * Ensure sonarr.service is removed
    
    * Move config to .config/Sonarr to allow purging of sonarr package
    
    * Use full path when updating systemd config dir
    
    * Use double quotes
    
    * fix: catch bad untars
    
    chance is wget will download a forward or
    some html page or something if the link breaks
    
    * Update isEnabled logic
    
    * Typo
    
    Co-authored-by: Flying Sausages <flying_sausages@protonmail.com>

--- scripts/remove/sonarr.sh
+++ scripts/remove/sonarr.sh
@@ -1,15 +1,48 @@
 #!/bin/bash
 
+user=$(grep User= /etc/systemd/system/sonarr.service | cut -d= -f2)
 if ask "Would you like to purge the configuration?" Y; then
-    apt_remove --purge sonarr
-    rm -rf /var/lib/sonarr
-    rm -rf /usr/lib/sonarr
-else
-    apt_remove sonarr
+    if [[ -d /home/${user}/.config/Sonarr ]]; then
+        rm -rf "/home/${user}/.config/Sonarr"
+    fi
 fi
+rm -rf /opt/Sonarr
 if [[ -f /install/.nginx.lock ]]; then
     rm /etc/nginx/apps/sonarr.conf
     systemctl reload nginx >> "$log" 2>&1
 fi
+
+#Mark mono depends as automatically installed
+LIST='mono-runtime
+    ca-certificates-mono
+    libmono-system-net-http4.0-cil
+    libmono-corlib4.5-cil
+    libmono-microsoft-csharp4.0-cil
+    libmono-posix4.0-cil
+    libmono-system-componentmodel-dataannotations4.0-cil
+    libmono-system-configuration-install4.0-cil
+    libmono-system-configuration4.0-cil
+    libmono-system-core4.0-cil
+    libmono-system-data-datasetextensions4.0-cil
+    libmono-system-data4.0-cil
+    libmono-system-identitymodel4.0-cil
+    libmono-system-io-compression4.0-cil
+    libmono-system-numerics4.0-cil
+    libmono-system-runtime-serialization4.0-cil
+    libmono-system-security4.0-cil
+    libmono-system-servicemodel4.0a-cil
+    libmono-system-serviceprocess4.0-cil
+    libmono-system-transactions4.0-cil
+    libmono-system-web4.0-cil
+    libmono-system-xml-linq4.0-cil
+    libmono-system-xml4.0-cil
+    libmono-system4.0-cil'
+
+apt-mark auto ${LIST} >> ${log} 2>&1
+
+#Remove mono if no longer required
+apt-get autoremove -y >> ${log} 2>&1
+rm -f /etc/apt/sources.list.d/mono-xamarin.list*
+rm -f /etc/systemd/system/sonarr.service
 
 rm /install/.sonarr.lock
