f023f690654e9439ddc70baf5cfe1f673b058570
flying-sausages <flying_sausages@protonmail.com>
2021-08-25 22:56:55 +0200

    Update scripts/upgrade/sonarr.sh

--- scripts/upgrade/sonarr.sh
+++ scripts/upgrade/sonarr.sh
@@ -4,7 +4,7 @@ if [[ -f /install/.sonarrold.lock ]]; then
     box install sonarr
 else
     if [[ -f /install/sonarr.lock ]]; then
-        echo_warn "Please use apt in order to upgrade sonarr to the lastest version"
+        echo_warn "Please upgrade Sonarr from within the UI"
         exit
     fi
     echo_warn "Cannot perform sonarr v2 to v3 migration."
