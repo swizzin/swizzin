0928fa25be98d3537723fc94a09e85d0b2a4d7ee
Flying Sausages <flying_sausages@protonmail.com>
2021-08-30 17:49:44 +0200

    fix: stricter config check

--- scripts/update/prowlarr.sh
+++ scripts/update/prowlarr.sh
@@ -2,7 +2,7 @@
 
 if [[ -f /install/.prowlarr.lock ]]; then
 
-    if [ -f /install/.nginx.lock ] && grep -q '$1/api' /etc/nginx/apps/prowlarr.conf; then
+    if [ -f /install/.nginx.lock ] && grep -q 'http://127.0.0.1:9696/prowlarr/$1/api' /etc/nginx/apps/prowlarr.conf; then
         echo_log_only "Fixing prowlarr nginx config"
         bash /etc/swizzin/scripts/nginx/prowlarr.sh
         systemctl reload nginx
