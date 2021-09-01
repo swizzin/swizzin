702463c3e41a4bc5b45edf331f1033f67592d5b7
Brett Petch <brettpetch@icloud.com>
2021-09-01 17:35:51 -0400

    fix(mylar): nginx

--- scripts/nginx/mylar.sh
+++ scripts/nginx/mylar.sh
@@ -13,17 +13,9 @@ user=$(_get_master_username)
 port=$(awk -F "=" '/http_port/ {print $2}' /home/${user}/.config/mylar/config.ini | tr -d ' ')
 sed -i 's|http_host = 0.0.0.0|http_host = 127.0.0.1|g' /home/${user}/.config/mylar/config.ini
 
-cat > /etc/nginx/apps/panel.conf << EON
-location / {
-  proxy_set_header Host \$host;
-  proxy_set_header X-Real-IP \$remote_addr;
-  proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-  proxy_set_header X-Forwarded-Host \$host;
-  proxy_set_header X-Forwarded-Proto \$scheme;
-  proxy_set_header Origin "";
-  proxy_pass http://127.0.0.1:${port};
-  proxy_http_version 1.1;
-  proxy_set_header Upgrade \$http_upgrade;
-  proxy_set_header Connection "Upgrade";
+cat > /etc/nginx/apps/mylar.conf << EON
+location ^~ /mylar {
+    include /config/nginx/proxy.conf;
+    proxy_pass http://127.0.0.1:${port};
 }
 EON
