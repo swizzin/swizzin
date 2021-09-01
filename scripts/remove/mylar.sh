282b910b9bea0974b8849ed6604c227e3f9d4d23
Brett Petch <brettpetch@icloud.com>
2021-09-01 11:10:30 -0400

    fix(mylar): source globals in uninstaller

--- scripts/remove/mylar.sh
+++ scripts/remove/mylar.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+. /etc/swizzin/sources/globals.sh
 systemctl disable --now mylar
 rm -rf /opt/Mylar
 rm -rf /opt/.venv/mylar
 rm -rf /install/.mylar.lock
 if ask "Would you like to purge the config?"; then
     : rm -rf /home/$(swizdb get Mylar/owner)/.config/Mylar
+    swizdb clear Mylar/owner
 else
     : break
 fi
