e7c67414b83f4bd69ca7a346bbf57051574fb01c
Brett Petch <brettpetch@icloud.com>
2021-09-01 17:39:32 -0400

    fix(mylar): nginx port

--- scripts/nginx/mylar.sh
+++ scripts/nginx/mylar.sh
@@ -12,7 +12,6 @@
 user=$(_get_master_username)
 port=$(awk -F "=" '/http_port/ {print $2}' /home/${user}/.config/mylar/config.ini | tr -d ' ')
 sed -i 's|http_host = 0.0.0.0|http_host = 127.0.0.1|g' /home/${user}/.config/mylar/config.ini
-
 cat > /etc/nginx/apps/mylar.conf << EON
 location ^~ /mylar {
     include /config/nginx/proxy.conf;
