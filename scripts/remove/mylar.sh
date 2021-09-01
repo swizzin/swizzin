d853679417c00be52e692bdebee23bc24e250402
Brett Petch <brettpetch@icloud.com>
2021-09-01 10:58:31 -0400

    fix(mylar): remove lock file on uninstall

--- scripts/remove/mylar.sh
+++ scripts/remove/mylar.sh
@@ -2,6 +2,7 @@
 systemctl disable --now mylar
 rm -rf /opt/Mylar
 rm -rf /opt/.venv/mylar
+rm -rf /install/.mylar.lock
 if ask "Would you like to purge the config?"; then
     : rm -rf /home/$(swizdb get Mylar/owner)/.config/Mylar
 else
