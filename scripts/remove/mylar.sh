74c53c30c8af34c234e8f4e934099b0108f5817a
Brett Petch <brettpetch@icloud.com>
2021-09-01 11:14:14 -0400

    fix (mylar): purge

--- scripts/remove/mylar.sh
+++ scripts/remove/mylar.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+#shellcheck source=sources/globals.sh
 . /etc/swizzin/sources/globals.sh
 systemctl disable --now mylar
 rm -rf /opt/Mylar
 rm -rf /opt/.venv/mylar
 rm -rf /install/.mylar.lock
+
 if ask "Would you like to purge the config?"; then
-    : rm -rf /home/$(swizdb get Mylar/owner)/.config/Mylar
+    :
+    rm -rf /home/$(swizdb get Mylar/owner)/.config/Mylar
     swizdb clear Mylar/owner
 else
-    : break
+    : # no condition
+    exit 0
 fi
