ab420a2c8fb74dba89d668d49ec1147af718424c
ComputerByte <46009342+ComputerByte@users.noreply.github.com>
2021-10-24 18:36:21 -0700

    Create navidrome.sh

---
+++ scripts/nginx/navidrome.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# navidrome nginx conf
+# byte 2021 for Swizzin
+
+#shellcheck source=sources/functions/utils
+. /etc/swizzin/sources/functions/utils
+users=($(_get_user_list))
+cat > /etc/nginx/apps/navidrome.conf <<- NGX
+location /navidrome {
+  proxy_pass        http://127.0.0.1:4533/navidrome;
+  proxy_set_header Host \$proxy_host;
+  proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+  proxy_set_header X-Forwarded-Proto \$scheme;
+  proxy_redirect off;
+  auth_basic "What's the password?";
+  auth_basic_user_file /etc/htpasswd.d/htpasswd.${user};
+}
+NGX
