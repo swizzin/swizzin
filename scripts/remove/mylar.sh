dedca424b2a370c6735671cb99a11b38a3bc2689
Brett Petch <brettpetch@icloud.com>
2021-11-14 12:28:57 -0500

    fix removal and upgrade

--- scripts/remove/mylar.sh
+++ scripts/remove/mylar.sh
@@ -3,13 +3,5 @@ systemctl disable --now mylar
 rm -rf /opt/Mylar
 rm -rf /opt/.venv/mylar
 rm -rf /install/.mylar.lock
-
-if ask "Would you like to purge the config?"; then
-    :
-    rm -rf /home/$(swizdb get mylar/owner)/.config/Mylar
-    swizdb clear mylar/owner
-    swizdb clear mylar/port
-else
-    : # no condition
-    exit 0
-fi
+rm -rf /home/$(swizdb get mylar/owner)/.config/mylar
+swizdb clear mylar/owner
--- scripts/upgrade/mylar.sh
+++ scripts/upgrade/mylar.sh
@@ -6,9 +6,8 @@
 if [[ -f /install/.mylar.lock ]]; then
     systemctl stop mylar
     MYLAR_OWNER=$(swizdb get mylar/owner)
-
     echo_progress_start "Pulling new commits"
-    git -C /opt/mylar checkout origin/master ||
+    git -C /opt/mylar checkout origin/master >> $log 2>&1 ||
         {
             echo_warn "Unclean repo detected, resetting current branch."
             git -C /opt/mylar reset --hard >> $log 2>&1
@@ -24,12 +23,10 @@ if [[ -f /install/.mylar.lock ]]; then
     }
     echo_progress_done
 
-    echo_progress_start "Checking pip for new depends"
     /opt/.venv/mylar/bin/pip install --upgrade pip >> ${log} 2>&1
     /opt/.venv/mylar/bin/pip install -r /opt/mylar/requirements.txt >> ${log} 2>&1
     chown -R $MYLAR_OWNER: /opt/mylar/
     chown -R $MYLAR_OWNER: /opt/.venv/mylar/
-    echo_progress_done "Depends up-to-date"
 
     echo_progress_start "Restarting Mylar"
     systemctl restart -q mylar
