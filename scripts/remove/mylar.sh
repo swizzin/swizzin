de2fe12ff366ec78c122cb7c66f26f1b3e69b6dd
Brett Petch <brettpetch@icloud.com>
2021-11-14 12:41:31 -0500

    fix case issue

--- scripts/remove/mylar.sh
+++ scripts/remove/mylar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 systemctl disable --now mylar
-rm -rf /opt/Mylar
+rm -rf /opt/mylar
 rm -rf /opt/.venv/mylar
 rm -rf /install/.mylar.lock
 rm -rf /home/$(swizdb get mylar/owner)/.config/mylar
