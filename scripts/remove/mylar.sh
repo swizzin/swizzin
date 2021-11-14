7d6411aea0c0d4813acc870495dbfe099594c96c
Brett Petch <bret7600@icloud.com>
2021-11-14 18:07:12 -0500

    Update scripts/remove/mylar.sh
    Co-authored-by: Userdocs <16525024+userdocs@users.noreply.github.com>

--- scripts/remove/mylar.sh
+++ scripts/remove/mylar.sh
@@ -10,5 +10,5 @@ rm_if_exists /etc/systemd/system/mylar.service
 rm_if_exists /opt/mylar
 rm_if_exists /opt/.venv/mylar
 rm_if_exists /install/.mylar.lock
-rm_if_exists /home/$(swizdb get mylar/owner)/.config/mylar
+rm_if_exists "/home/$(swizdb get mylar/owner)/.config/mylar"
 swizdb clear mylar/owner
