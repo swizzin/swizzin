c69d880d48606af68fa1c29bccfa10f95546fff1
Brett Petch <bret7600@icloud.com>
2021-11-14 18:20:39 -0500

    fix
    Co-authored-by: Userdocs <16525024+userdocs@users.noreply.github.com>

--- scripts/nginx/mylar.sh
+++ scripts/nginx/mylar.sh
@@ -3,7 +3,7 @@
 # Author: Brett
 # Copyright (C) 2021 Swizzin
 MYLAR_OWNER="$(swizdb get mylar/owner)"
-port=$(awk -F "=" '/http_port/ {print $2}' /home/${MYLAR_OWNER}/.config/mylar/config.ini | tr -d ' ')
+port=$(sed -rn 's|http_port = (.*)|\1|p' "/home/${MYLAR_OWNER}/.config/mylar/config.ini")
 sed -i 's|http_host = 0.0.0.0|http_host = 127.0.0.1|g' "/home/${MYLAR_OWNER}/.config/mylar/config.ini"
 sed -i 's|http_root = /|http_root = /mylar|g' "/home/${MYLAR_OWNER}/.config/mylar/config.ini"
 
