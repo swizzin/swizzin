88c439d881c882f444ce9965bd68ea410bb282f7
Userdocs <16525024+userdocs@users.noreply.github.com>
2021-11-14 23:05:20 +0000

    Update mylar.sh
    use the remove function instead of the raw commands

--- scripts/remove/mylar.sh
+++ scripts/remove/mylar.sh
@@ -2,10 +2,13 @@
 # Mylar installer for Swizzin
 # Author: Brett
 # Copyright (C) 2021 Swizzin
+#
+. /etc/swizzin/sources/functions/utils
+
 systemctl disable --now mylar
-rm -rf /etc/systemd/system/mylar.service
-rm -rf /opt/mylar
-rm -rf /opt/.venv/mylar
-rm -rf /install/.mylar.lock
-rm -rf /home/$(swizdb get mylar/owner)/.config/mylar
+rm_if_exists /etc/systemd/system/mylar.service
+rm_if_exists /opt/mylar
+rm_if_exists /opt/.venv/mylar
+rm_if_exists /install/.mylar.lock
+rm_if_exists /home/$(swizdb get mylar/owner)/.config/mylar
 swizdb clear mylar/owner
