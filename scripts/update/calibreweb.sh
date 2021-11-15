5a775396265f0bba1d17c3350700d27d44b1d866
liaralabs <liara@swizzin.ltd>
2021-11-15 15:49:59 -0800

    Run check that changes file before potentially restarting

--- scripts/update/calibreweb.sh
+++ scripts/update/calibreweb.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [[ -f /install/.calibreweb.lock ]]; then
-    if ! /opt/.venv/calibreweb/bin/python3 -c "import pkg_resources; pkg_resources.require(open('/opt/calibreweb/requirements.txt',mode='r'))" &> /dev/null; then
-        echo_progress_start "Updating Calibre Web requirements"
-        /opt/.venv/calibreweb/bin/pip install -r /opt/calibreweb/requirements.txt
-        echo_progress_done
-    fi
     if grep -q -- "cps.py -f" /etc/systemd/system/calibreweb.service; then
         echo_info "Updating Calibre Web service file"
         sed -i 's/cps.py -f/cps.py/g' /etc/systemd/system/calibreweb.service
         systemctl daemon-reload
+    fi
+    if ! /opt/.venv/calibreweb/bin/python3 -c "import pkg_resources; pkg_resources.require(open('/opt/calibreweb/requirements.txt',mode='r'))" &> /dev/null; then
+        echo_progress_start "Updating Calibre Web requirements"
+        /opt/.venv/calibreweb/bin/pip install -r /opt/calibreweb/requirements.txt
+        echo_progress_done
     fi
 fi
