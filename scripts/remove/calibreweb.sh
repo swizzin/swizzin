0d2eeeb256e3317921985f1baa9900b8ac9f6b31
userdocs <16525024+userdocs@users.noreply.github.com>
2021-11-15 20:27:16 +0000

    utils

--- scripts/remove/calibreweb.sh
+++ scripts/remove/calibreweb.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#shellcheck source=sources/functions/utils
+. /etc/swizzin/sources/functions/utils
+
 #shellcheck source=sources/functions/kepubify
 . /etc/swizzin/sources/functions/kepubify
 
