aaf42cb1c33b7cf70dfa25ea872d3666e596fe1a
userdocs <16525024+userdocs@users.noreply.github.com>
2021-11-16 23:24:06 +0000

    Update navidrome.sh
    fix the fix

--- scripts/nginx/navidrome.sh
+++ scripts/nginx/navidrome.sh
@@ -9,7 +9,7 @@ user="$(_get_master_username)"
 http_port="$(sed -rn 's|Port = "(.*)"|\1|p' "/home/${user}/.config/navidrome/navidrome.toml")" # default port used by navidrome
 
 [[ -f /install/.navidrome.lock ]] && systemctl stop -q navidrome
-sed -r 's|Address = (.*)|http_host = "127.0.0.1"|g' -i "/home/${user}/.config/navidrome/navidrome.toml"
+sed -r 's|Address = (.*)|Address = "127.0.0.1"|g' -i "/home/${user}/.config/navidrome/navidrome.toml"
 sed -r 's|BaseUrl = (.*)|BaseUrl = "/navidrome"|g' -i "/home/${user}/.config/navidrome/navidrome.toml"
 [[ -f /install/.navidrome.lock ]] && systemctl -q start navidrome
 
