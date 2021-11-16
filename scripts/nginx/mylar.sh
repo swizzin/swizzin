190af4190f6eb63d59d4f132a8ac3c891adef7c0
userdocs <16525024+userdocs@users.noreply.github.com>
2021-11-16 18:50:07 +0000

    Update mylar.sh

--- scripts/nginx/mylar.sh
+++ scripts/nginx/mylar.sh
@@ -6,8 +6,10 @@
 mylar_owner="$(swizdb get mylar/owner)"
 port="$(sed -rn 's|http_port = (.*)|\1|p' "/home/${mylar_owner}/.config/mylar/config.ini")"
 
+[[ -f /install/.mylar.lock ]] && systemctl stop -q mylar
 sed -r 's|http_host = (.*)|http_host = 127.0.0.1|g' -i "/home/${mylar_owner}/.config/mylar/config.ini"
 sed -r 's|http_root = (.*)|http_root = /mylar|g' -i "/home/${mylar_owner}/.config/mylar/config.ini"
+[[ -f /install/.mylar.lock ]] && systemctl -q start mylar
 
 cat > /etc/nginx/apps/mylar.conf << EON
 location ^~ /mylar {
@@ -18,5 +20,3 @@ location ^~ /mylar {
     auth_basic_user_file /etc/htpasswd.d/htpasswd.${mylar_owner};
 }
 EON
-
-systemctl restart -q mylar
