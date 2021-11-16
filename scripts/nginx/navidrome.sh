c9313c667fadc8073b5db66fb522278df3a03c7f
userdocs <16525024+userdocs@users.noreply.github.com>
2021-11-16 22:36:18 +0000

    Update navidrome.sh
    use toml config

--- scripts/nginx/navidrome.sh
+++ scripts/nginx/navidrome.sh
@@ -6,7 +6,12 @@
 . /etc/swizzin/sources/functions/utils
 
 user="$(_get_master_username)"
-http_port="4533" # default port used by navidrome
+http_port="$(sed -rn 's|Port = "(.*)"|\1|p' "/home/${user}/.config/navidrome/navidrome.toml")" # default port used by navidrome
+
+[[ -f /install/.navidrome.lock ]] && systemctl stop -q navidrome
+sed -r 's|Address = (.*)|http_host = 127.0.0.1|g' -i "/home/${user}/.config/navidrome/navidrome.toml"
+sed -r 's|BaseUrl = (.*)|http_root = /navidrome|g' -i "/home/${user}/.config/navidrome/navidrome.toml"
+[[ -f /install/.navidrome.lock ]] && systemctl -q start navidrome
 
 cat > /etc/nginx/apps/navidrome.conf <<- NGX
 location /navidrome {
