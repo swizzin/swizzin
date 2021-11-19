75456242b327811e1115c625c7dbcc01c5ffd782
Userdocs <16525024+userdocs@users.noreply.github.com>
2021-11-19 17:47:14 +0000

    Feat debug (#798)
    * feat: debug
    
    a small debug function to check services using colourised output via ccze
    
    * feat: debug
    
    create symlink on update
    
    * Update debug
    
    add pipe status failure check.
    
    * Update debug
    
    just use pipestatus directly.
    
    * Update debug
    
    pipe status check entire pipe and output more helpful info
    
    * Update debug
    
    * Update debug
    
    * made a file executable
    
    * made a file executable
    
    * extension
    
    extension
    
    * made a file executable

--- scripts/update/10-dependencies.sh
+++ scripts/update/10-dependencies.sh
@@ -39,7 +39,7 @@ if [[ $trigger_apt_update == "true" ]]; then
 fi
 
 #space-separated list of required GLOBAL SWIZZIN dependencies (NOT application specific ones)
-dependencies="whiptail git sudo curl wget lsof fail2ban apache2-utils vnstat tcl tcl-dev build-essential dirmngr apt-transport-https bc uuid-runtime jq net-tools gnupg2 cracklib-runtime unzip"
+dependencies="whiptail git sudo curl wget lsof fail2ban apache2-utils vnstat tcl tcl-dev build-essential dirmngr apt-transport-https bc uuid-runtime jq net-tools gnupg2 cracklib-runtime unzip ccze"
 
 missing=()
 for dep in $dependencies; do
