73ee142212beda7386338dcbd494bcec26e6472f
liaralabs <liara@swizzin.ltd>
2022-01-22 11:29:29 -0800

    Update filemanager and related plugins, remove from ruT initial install since it's currently not stable

--- scripts/nginx/rutorrent.sh
+++ scripts/nginx/rutorrent.sh
@@ -47,33 +47,7 @@ function rutorrent_install() {
         perl -pi -e "s/\$defaultTheme \= \"\"\;/\$defaultTheme \= \"club-QuickBox\"\;/g" /srv/rutorrent/plugins/theme/conf.php
     fi
 
-    if [[ ! -d /srv/rutorrent/plugins/filemanager ]]; then
-        cd /srv/rutorrent/plugins/
-        svn co https://github.com/nelu/rutorrent-thirdparty-plugins/trunk/filemanager >> "$log" 2>&1
-        chown -R www-data: /srv/rutorrent/plugins/filemanager
-        chmod -R +x /srv/rutorrent/plugins/filemanager/scripts
-
-        cat > /srv/rutorrent/plugins/filemanager/conf.php << FMCONF
-<?php
-
-\$fm['tempdir'] = '/tmp';
-\$fm['mkdperm'] = 755;
-
-// set with fullpath to binary or leave empty
-\$pathToExternals['rar'] = '$(which rar)';
-\$pathToExternals['zip'] = '$(which zip)';
-\$pathToExternals['unzip'] = '$(which unzip)';
-\$pathToExternals['tar'] = '$(which tar)';
-
-// archive mangling, see archiver man page before editing
-\$fm['archive']['types'] = array('rar', 'zip', 'tar', 'gzip', 'bzip2');
-\$fm['archive']['compress'][0] = range(0, 5);
-\$fm['archive']['compress'][1] = array('-0', '-1', '-9');
-\$fm['archive']['compress'][2] = \$fm['archive']['compress'][3] = \$fm['archive']['compress'][4] = array(0);
-
-?>
-FMCONF
-    fi
+    # TODO: Add new FileManager when it's more stable with new ruTorrent
 
     if [[ ! -d /srv/rutorrent/plugins/ratiocolor ]]; then
         cd /srv/rutorrent/plugins
