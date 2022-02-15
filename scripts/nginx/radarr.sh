afe45be339d8209db92096e2aee9f45fb7aa9889
Brett Petch <brettpetch@icloud.com>
2022-02-15 00:45:06 -0500

    fix(Radarr): add necessary headers

--- scripts/nginx/radarr.sh
+++ scripts/nginx/radarr.sh
@@ -6,10 +6,12 @@ master=$(cut -d: -f1 < /root/.master.info)
 cat > /etc/nginx/apps/radarr.conf << RADARR
 location /radarr {
   proxy_pass        http://127.0.0.1:7878/radarr;
-  proxy_set_header Host \$proxy_host;
+  proxy_set_header Host \$host;
   proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+  proxy_set_header X-Forwarded-Host \$host;
   proxy_set_header X-Forwarded-Proto \$scheme;
   proxy_redirect off;
+
   auth_basic "What's the password?";
   auth_basic_user_file /etc/htpasswd.d/htpasswd.${master};
 
