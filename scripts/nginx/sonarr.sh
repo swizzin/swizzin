ca65c513885e237d645a740ea3aa904994aff8a0
liaralabs <30909703+liaralabs@users.noreply.github.com>
2022-05-17 18:29:26 -0700

    fix: proxy_host for sonarr

--- scripts/nginx/sonarr.sh
+++ scripts/nginx/sonarr.sh
@@ -20,7 +20,7 @@ app_branch="main"
 cat > /etc/nginx/apps/$app_name.conf << ARRNGINX
 location ^~ /$app_baseurl {
     proxy_pass http://127.0.0.1:$app_port;
-    proxy_set_header Host \$host;
+    proxy_set_header Host \$proxy_host;
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     proxy_set_header X-Forwarded-Host \$host;
     proxy_set_header X-Forwarded-Proto \$scheme;
