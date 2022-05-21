f457dacadcce99a02c77ff6e82b3bf28c8545a13
liaralabs <liara@swizzin.ltd>
2022-05-21 09:37:51 -0700

    fix(emby): arm upgrades

--- scripts/upgrade/emby.sh
+++ scripts/upgrade/emby.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # Simple tool to grab the latest release of emby
 
-current=$(curl -L -s -H 'Accept: application/json' https://github.com/MediaBrowser/Emby.Releases/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-cd /tmp
-wget -q -O emby.dpkg https://github.com/MediaBrowser/Emby.Releases/releases/download/${current}/emby-server-deb_${current}_amd64.deb
-dpkg -i emby.dpkg >> /dev/null 2>&1
+. /etc/swizzin/sources/functions/utils
+current=$(github_latest_version MediaBrowser/Emby.Releases)
+wget -O /tmp/emby.dpkg https://github.com/MediaBrowser/Emby.Releases/releases/download/${current}/emby-server-deb_${current}_$(_os_arch).deb >> $log 2>&1 || {
+    echo_error "Emby failed to download"
+    exit 1
+}
+dpkg -i emby.dpkg >> ${log} 2>&1 || {
+    echo_error "Emby failed to install"
+}
 rm emby.dpkg
