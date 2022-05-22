6fe2652f99479f715f12f3edffcf0876d13f7796
liaralabs <liara@swizzin.ltd>
2022-05-21 20:32:32 -0700

    fix: log output during upgrade

--- scripts/upgrade/lounge.sh
+++ scripts/upgrade/lounge.sh
@@ -12,7 +12,7 @@ function _yarnlounge() {
     yarn --non-interactive global add thelounge >> $log 2>&1
     yarn --non-interactive cache clean >> $log 2>&1
     chown -R lounge: /opt/lounge
-    sudo -u lounge bash -c "thelounge install thelounge-theme-zenburn"
+    sudo -u lounge bash -c "thelounge install thelounge-theme-zenburn" >> ${log} 2>&1
     # Replace with new execstart line
     sed -i "s|ExecStart=.*|ExecStart=$(which thelounge) start|g" /etc/systemd/system/lounge.service
     systemctl daemon-reload
@@ -33,11 +33,11 @@ if [[ $(systemctl is-active lounge) == "active" ]]; then
     echo_progress_done
 fi
 
-if thelounge --version lt 4.3.1; then
+if thelounge --version lt 4.3.1 >> /dev/null 2>&1; then
     _yarnlounge
 fi
 
-if ! /usr/bin/yarn --non-interactive global upgrade thelounge >> "$log"; then
+if ! /usr/bin/yarn --non-interactive global upgrade thelounge >> "$log" 2>&1; then
     echo_error "Lounge failed to update, please investigate the logs"
 fi
 
@@ -46,3 +46,4 @@ if [[ $wasActive = "true" ]]; then
     systemctl start lounge
     echo_progress_done
 fi
+
