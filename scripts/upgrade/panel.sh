90c679564d8e0f6fa85a16b9f9e51746983124b9
liaralabs <30909703+liaralabs@users.noreply.github.com>
2022-05-25 18:50:10 -0700

    fix(panel): botched permissions and hardcode reset paths (#873)
    * fix: botched panel permissions
    
    * fix(panel): hardcode panel reset paths

--- scripts/upgrade/panel.sh
+++ scripts/upgrade/panel.sh
@@ -13,16 +13,17 @@ if [[ -f /install/.panel.lock ]]; then
     if [[ $PANELRESET == 1 ]]; then
         echo_warn "Working around unclean git repo"
         sudo -u swizzin git -C /opt/swizzin fetch origin master >> ${log} 2>&1
-        cp -a core/custom core/custom.tmp
+        cp -a /opt/swizzin/core/custom /opt/swizzin/core/custom.tmp
         sudo -u swizzin git -C /opt/swizzin reset --hard origin/master >> ${log} 2>&1
-        mv core/custom.tmp/* core/custom/ >> ${log} 2>&1
-        rm -rf core/custom.tmp
+        mv /opt/swizzin/core/custom.tmp/* /opt/swizzin/core/custom/ >> ${log} 2>&1
+        rm -rf opt/swizzin/core/custom.tmp
     fi
     echo_progress_done "Commits pulled"
     echo_progress_start "Checking pip for new depends"
     if ! /opt/.venv/swizzin/bin/python /opt/swizzin/tests/test_requirements.py >> ${log} 2>&1; then
         /opt/.venv/swizzin/bin/pip install --upgrade pip wheel >> ${log} 2>&1
         /opt/.venv/swizzin/bin/pip install -r /opt/swizzin/requirements.txt >> ${log} 2>&1
+        chown -R swizzin: /opt/.venv/swizzin
     fi
     echo_progress_done "Depends up-to-date"
     echo_progress_start "Restarting Panel"
