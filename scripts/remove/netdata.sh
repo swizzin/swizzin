ac72a6c58549b90f571568e4c77fad62f2b0c41a
liaralabs <30909703+liaralabs@users.noreply.github.com>
2022-10-15 13:24:18 -0700

    netdata: fix uninstaller (#915)
    * netdata: fix uninstaller
    
    * cleanup
    
    * more cleanup

--- scripts/remove/netdata.sh
+++ scripts/remove/netdata.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 # Netdata uninstaller for swizzin
 
-/usr/libexec/netdata/netdata-uninstaller.sh --yes --env /etc/netdata/.environment -f >> $log 2>&1 || {
-    echo_error "Netdata remover failed!"
-    exit 1
-}
-
+if [[ -f /usr/libexec/netdata/netdata-uninstaller.sh ]]; then
+    /usr/libexec/netdata/netdata-uninstaller.sh --yes --env /etc/netdata/.environment -f >> $log 2>&1 || {
+        echo_error "Netdata remover failed!"
+        exit 1
+    }
+else
+    bash <(curl -Ssf https://my-netdata.io/kickstart.sh 2>> ${log} || { echo "exit 1"; }) --uninstall --non-interactive >> $log 2>&1 || {
+        echo_error "Netdata remover failed!"
+        exit 1
+    }
+fi
+rm /etc/nginx/apps/netdata.conf
+systemctl reload nginx >> ${log} 2>&1
 rm -rf /install/.netdata.lock
