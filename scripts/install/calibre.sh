8898cf7f2b21de9f87be96b996abf7d8e3b1e237
Brett Petch <brettpetch@icloud.com>
2022-10-15 17:01:43 -0400

    calibre: ensure `libOpenGL.so.0` is installed. (#891)
    * fix: ensure `libOpenGL.so.0` is installed.
    
    * calibre: add libopengl0 upgrade check
    
    Co-authored-by: liaralabs <liara@swizzin.ltd>

--- scripts/install/calibre.sh
+++ scripts/install/calibre.sh
@@ -25,7 +25,7 @@ else
 fi
 
 _install() {
-    apt_install xdg-utils wget xz-utils libxcb-xinerama0 libfontconfig libgl1-mesa-glx
+    apt_install xdg-utils wget xz-utils libxcb-xinerama0 libfontconfig libgl1-mesa-glx libopengl0
     echo_progress_start "Installing calibre"
     if [[ $(_os_arch) = "amd64" ]]; then
         wget https://download.calibre-ebook.com/linux-installer.sh -O /tmp/calibre-installer.sh >> $log 2>&1
