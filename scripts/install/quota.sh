e858d3c7a4481a8a4ecd22d3bfee72a8dcd1d4a4
liaralabs <liara@swizzin.ltd>
2023-02-21 17:59:57 -0800

    quota: use rutorrent function for diskspace

--- scripts/install/quota.sh
+++ scripts/install/quota.sh
@@ -105,36 +105,9 @@ function _installquota() {
     fi
 
     if [[ -d /srv/rutorrent ]]; then
-        cat > /srv/rutorrent/plugins/diskspace/action.php << 'DSKSP'
-<?php
-#################################################################################
-##  [Quick Box - action.php modified for quota systems use]
-#################################################################################
-# QUICKLAB REPOS
-# QuickLab _ packages:   https://github.com/QuickBox/quickbox_rutorrent-plugins
-# LOCAL REPOS
-# Local _ packages   :   ~/QuickBox/rtplugins
-# Author             :   QuickBox.IO
-# URL                :   https://plaza.quickbox.io
-#
-#################################################################################
-  require_once( '../../php/util.php' );
-  if (isset($quotaUser) && file_exists('/install/.quota.lock')) {
-    $total = shell_exec("sudo /usr/bin/quota -wu ".$quotaUser."| tail -n 1 | sed -e 's|^[ \t]*||' | awk '{print $3*1024}'");
-    $used = shell_exec("sudo /usr/bin/quota -wu ".$quotaUser."| tail -n 1 | sed -e 's|^[ \t]*||' | awk '{print $2*1024}'");
-    $free = sprintf($total - $used);
-    cachedEcho('{ "total": '.$total.', "free": '.$free.' }',"application/json");
-  } else {
-      cachedEcho('{ "total": '.disk_total_space($topDirectory).', "free": '.disk_free_space($topDirectory).' }',"application/json");
-  }
-?>
-DSKSP
-        if [[ $primaryroot == "root" ]]; then
-            sed -i 's/MOUNT/\//g' /srv/rutorrent/plugins/diskspace/action.php
-        elif [[ $primaryroot == "home" ]]; then
-            sed -i 's/MOUNT/\/home/g' /srv/rutorrent/plugins/diskspace/action.php
-        fi
-
+        #shellcheck source=sources/functions/rutorrent
+        . /etc/swizzin/sources/functions/rutorrent
+        rutorrent_fix_quota
     fi
 }
 
