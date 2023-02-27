5307b1cb74da596e3c9abe307ceb3730aa3f634d
liaralabs <30909703+liaralabs@users.noreply.github.com>
2023-02-27 09:14:32 -0800

    wireguard: fix buster installs, deprecate old platforms (Fixes #925) (#961)
    * wireguard: ensure dkms is used if installing on buster
    
    * Fix additional issues and remove old code

--- scripts/install/wireguard.sh
+++ scripts/install/wireguard.sh
@@ -40,21 +40,18 @@ function _selectiface() {
 }
 
 function _install_wg() {
-    if [[ $distribution == "Debian" ]]; then
-        if [[ ! $codename == "stretch" ]]; then
+    PKGS=(wireguard qrencode iptables)
+
+    case ${codename} in
+        buster)
             check_debian_backports
-        else
-            echo_info "Adding debian unstable repository and limiting packages"
-            echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
-            printf 'Package: *\nPin: release a=unstable\nPin-Priority: 10\n\nPackage: *\nPin: release a=stretch-backports\nPin-Priority: 250' > /etc/apt/preferences.d/limit-unstable
-        fi
-    elif [[ $codename == "bionic" ]]; then
-        #This *should* be enabled by default but you know what they say about assumptions.
-        check_ubuntu_updates
-    fi
+            PKGS+=(wireguard-dkms)
+            ;;
+        *) ;;
+    esac
 
     apt_update
-    apt_install --recommends wireguard qrencode iptables
+    apt_install --recommends ${PKGS[@]}
 
     if [[ ! -d /etc/wireguard ]]; then
         mkdir /etc/wireguard
@@ -164,8 +161,8 @@ EOWGC
 . /etc/swizzin/sources/functions/utils
 . /etc/swizzin/sources/functions/backports
 
-distribution=$(lsb_release -is)
-codename=$(lsb_release -cs)
+distribution=$(_os_distro)
+codename=$(_os_codename)
 ip=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
 if [[ -f /install/.wireguard.lock ]]; then
     wgiface=$(cat /install/.wireguard.lock)
