9cfb8b1e42851f09100413df9ee2898383b8a5f2
flying-sausages <23618693+flying-sausages@users.noreply.github.com>
2023-07-15 18:50:59 +0200

    add nginx conf remove

--- scripts/remove/scrutiny.sh
+++ scripts/remove/scrutiny.sh
@@ -6,6 +6,11 @@ systemctl disable -q --now scrutiny-web
 systemctl disable -q --now scrutiny-collector.timer
 systemctl disable -q --now scrutiny-collector.service
 
+if [[ -f /install/.nginx.lock ]]; then
+    rm "/etc/nginx/apps/scrutiny.conf"
+    systemctl reload nginx >> "$log" 2>&1
+fi
+
 userdel -rf scrutiny
 if ask "Remove InfluxDB as well?"; then
     apt_remove influxdb2
