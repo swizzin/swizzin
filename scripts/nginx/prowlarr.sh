6df2b75002579e69f02032ac36557392b38c113e
Noxillio <me@noxillio.dev>
2024-02-04 01:33:05 -0500

    Update prowlarr.sh

--- scripts/nginx/prowlarr.sh
+++ scripts/nginx/prowlarr.sh
@@ -32,7 +32,14 @@ location ^~ /$app_baseurl {
     auth_basic "What's the password?";
     auth_basic_user_file /etc/htpasswd.d/htpasswd.${master};
 }
+
 # Allow the API/Indexer External Access via NGINX
+location ^~ /$app_baseurl(/[0-9]+)?/api {
+    auth_request off;
+    proxy_pass http://127.0.0.1:$app_port;
+}
+
+# Allow application API access
 location ^~ /$app_baseurl/api {
     auth_request off;
     proxy_pass http://127.0.0.1:$app_port;
