bce5332f98ed8bbc6fbf7a185f77522c7ccabc7a
liaralabs <30909703+liaralabs@users.noreply.github.com>
2024-02-17 08:52:40 -0800

    nginx: fix fancyindex installation (#1136)
    * svn-depreciation
    
    * nginx: fix fancyindex installation
    
    * cleanup
    
    ---------
    
    Co-authored-by: Brett Petch <brettpetch@icloud.com>

--- scripts/install/nginx.sh
+++ scripts/install/nginx.sh
@@ -198,7 +198,10 @@ PROX
 echo_progress_done "Config installed"
 
 echo_progress_start "Installing fancyindex"
-svn export https://github.com/Naereen/Nginx-Fancyindex-Theme/trunk/Nginx-Fancyindex-Theme-dark /srv/fancyindex >> $log 2>&1
+git clone https://github.com/Naereen/Nginx-Fancyindex-Theme/ /tmp/fancyindex >> $log 2>&1
+mv /tmp/fancyindex/Nginx-Fancyindex-Theme-dark /srv/fancyindex >> $log 2>&1
+rm -rf /tmp/fancyindex
+
 cat > /etc/nginx/snippets/fancyindex.conf << FIC
 fancyindex on;
 fancyindex_localtime on;
