5ee91ae76ea6dd7979a9b1e1a40fd00213bd93f7
s0up4200 <s0up4200@pm.me>
2025-02-14 13:58:07 +0100

    feat: Netronome

---
+++ scripts/install/netronome.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# netronome installer
+# soup 2025 for Swizzin
+
+#shellcheck source=sources/functions/utils
+. /etc/swizzin/sources/functions/utils
+#shellcheck source=sources/functions/netronome
+. /etc/swizzin/sources/functions/netronome
+
+_systemd() {
+    type=simple
+    if [[ $(systemctl --version | awk 'NR==1 {print $2}') -ge 240 ]]; then
+        type=exec
+    fi
+
+    echo_progress_start "Installing Systemd service"
+    cat > /etc/systemd/system/netronome@.service << EOF
+[Unit]
+Description=netronome service for %i
+After=syslog.target network.target
+
+[Service]
+Type=$type
+User=%i
+Group=%i
+ExecStart=/usr/bin/netronome --config=/home/%i/.config/netronome/
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    echo_progress_done "Service installed"
+}
+
+_nginx() {
+    if [[ -f /install/.nginx.lock ]]; then
+        echo_progress_start "Configuring nginx for netronome"
+        bash /etc/swizzin/scripts/nginx/netronome.sh
+        systemctl reload nginx >> $log 2>&1
+        echo_progress_done "Nginx configured for netronome"
+    fi
+}
+
+# This method will perform all logic related to user accounts. For each user when installed, and for only one user when via box adduser
+_add_users() {
+    for user in "${users[@]}"; do
+        echo_progress_start "Enabling netronome for $user"
+
+        # get random available port
+        port=$(port 10000 12000)
+
+        # generate a sessionSecret
+        sessionSecret="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
+
+        mkdir -p "/home/$user/.config/netronome/"
+        chown "$user": "/home/$user/.config"
+        chown -R "$user": "/home/$user/.config/netronome"
+
+        cat > "/home/$user/.config/netronome/config.toml" << CFG
+# Netronome Configuration
+
+[database]
+type = "sqlite"
+path = "netronome.db"
+
+[server]
+host = "0.0.0.0"
+port = ${port}
+
+[logging]
+level = "trace"  # trace, debug, info, warn, error, fatal, panic
+
+[speedtest]
+timeout = 30
+
+[speedtest.iperf]
+test_duration = 10
+parallel_conns = 4
+CFG
+}
+
+##############################
+
+# If `box adduser` is running, only do _add_users and then exit. Otherwise, run the whole installer
+if [ -n "$1" ]; then
+    users=("$1")
+    _add_users
+    _nginx
+    exit 0
+fi
+
+netronome_download_latest
+_systemd
+
+readarray -t users < <(_get_user_list)
+_add_users
+_nginx
+
+touch "/install/.netronome.lock"
+echo_success "netronome installed"
