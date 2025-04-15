210a54129fb35194131ca2938a91bfbd3321d7ac
imabee <imabee@riseup.net>
2025-04-15 09:23:53 -0400

    cleanup(tailscale): move `sed` to correct locations

--- scripts/install/tailscale.sh
+++ scripts/install/tailscale.sh
@@ -22,7 +22,6 @@ fi
 
 if [[ -f /install/.nginx.lock ]]; then
     if ask "Would you like to bind your NGINX server to your Tailscale IP / hostname ${ts_hostname}?"; then
-
         echo_progress_start "Binding default server to Tailscale IP"
         tailscale_ipv4=$(tailscale ip -4 | head -n1)
         tailscale_ipv6=$(tailscale ip -6 | head -n1)
@@ -30,21 +29,30 @@ if [[ -f /install/.nginx.lock ]]; then
         sed -i "s|listen \[[^]]*\]:443.*|listen [${tailscale_ipv6}]:443 ssl http2;|g" /etc/nginx/sites-enabled/default
         sed -i "s|listen \([0-9.]*:\)\?80.*|listen ${tailscale_ipv4}:80 default_server;|g" /etc/nginx/sites-enabled/default
         sed -i "s|listen \([0-9.]*:\)\?443.*|listen ${tailscale_ipv4}:443 ssl http2;|g" /etc/nginx/sites-enabled/default
-        sed -i "s|server_name .*;|server_name ${ts_hostname};|g" /etc/nginx/sites-enabled/default
+        systemctl reload nginx >> $log 2>&1
         echo_progress_done "Default server bound to ${tailscale_ipv4} and ${tailscale_ipv6}"
 
         if ask "Would you like Tailscale to generate TLS certificates for your device?"; then
             echo_progress_start "Generating TLS certificates for Tailscale IP"
-            mkdir -p "/etc/nginx/ssl/${ts_hostname}"
-            tailscale cert --cert-file "/etc/nginx/ssl/${ts_hostname}/cert.pem" --key-file "/etc/nginx/ssl/${ts_hostname}/key.pem"
+            ts_renewal
             chmod 700 /etc/nginx/ssl/
             chown -R www-data:www-data "/etc/nginx/ssl/${ts_hostname}"
+            sed -i "s|server_name .*;|server_name ${ts_hostname};|g" /etc/nginx/sites-enabled/default
             sed -i "s|ssl_certificate .*;|ssl_certificate /etc/nginx/ssl/${ts_hostname}/cert.pem;|g" /etc/nginx/sites-enabled/default
             sed -i "s|ssl_certificate_key .*;|ssl_certificate_key /etc/nginx/ssl/${ts_hostname}/key.pem;|g" /etc/nginx/sites-enabled/default
             echo_progress_done
             echo_progress_start "Installing renewal job"
-            ts_znc_hook
-            ts_vsftpd_hook
+            ts_cron="23 2 */45 * * bash -c \"source /etc/swizzin/sources/globals.sh && source /etc/swizzin/sources/functions/letsencrypt && ts_renewal\""
+            crontab -l 2> /dev/null | grep -F "$ts_cron" > /dev/null
+            if [ $? -ne 0 ]; then
+                (
+                    crontab -l 2> /dev/null
+                    echo "$ts_cron"
+                ) | crontab -
+                echo_progress_done
+            else
+                echo_error "Cron job already exists. Skipping."
+            fi
         fi
         echo_progress_start "Applying your changes to nginx"
         systemctl reload nginx >> $log 2>&1
