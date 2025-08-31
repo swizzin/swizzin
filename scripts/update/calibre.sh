88e8deab296b3b88ed3525ffbca200be4494bf5e
flying-sausages <23618693+flying-sausages@users.noreply.github.com>
2025-08-31 17:46:37 +0200

    fixup! add update path

--- scripts/update/calibre.sh
+++ scripts/update/calibre.sh
@@ -3,7 +3,7 @@ if [[ -f /install/.calibre.lock ]]; then
     # check if libxcb-cursor0 is installed
     if ! check_installed libxcb-cursor0; then
         echo_info "Installing new calibre dependencies"
-        apt_install -y libxcb-cursor0
+        apt_install libxcb-cursor0
     fi
 fi
 
