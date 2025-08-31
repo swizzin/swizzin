196d01ecb227ecf99c8ea37628bd025270dafe2f
flying-sausages <23618693+flying-sausages@users.noreply.github.com>
2025-08-31 14:35:51 +0200

    Add new dependency for calibre

---
+++ scripts/update/calibre.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [[ -f /install/.calibre.lock ]]; then
+    # check if libxcb-cursor0 is installed
+    if ! check_installed libxcb-cursor0; then
+        echo_info "Installing new calibre dependencies"
+        apt_install -y libxcb-cursor0
+    fi
+fi
