c5e8faa94e30e50359c1612e4cb83153fbe487eb
flying-sausages <23618693+flying-sausages@users.noreply.github.com>
2025-08-31 16:08:37 +0200

    add update path

--- scripts/update/calibre.sh
+++ scripts/update/calibre.sh
@@ -6,3 +6,19 @@ if [[ -f /install/.calibre.lock ]]; then
         apt_install -y libxcb-cursor0
     fi
 fi
+
+if check_installed calibre; then
+    echo_info "Moving calibre to the online installer instead of the apt install"
+
+    echo_progress_start "Installing calibre"
+    wget https://download.calibre-ebook.com/linux-installer.sh -O /tmp/calibre-installer.sh >> $log 2>&1
+    if ! bash /tmp/calibre-installer.sh install_dir=/opt >> $log 2>&1; then
+        echo_error "Failed to install calibre from web installer, please investigate and try again"
+        exit 1
+    fi
+    echo_progress_done
+
+    echo_progress_start "Removing apt-based calibre binaries"
+    apt_remove calibre
+    echo_progress_done
+fi
