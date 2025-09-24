7632a49796303a5f1c0ae2ad144d1ec032190467
Motophan <42502676+Motophan@users.noreply.github.com>
2025-09-23 20:54:04 -0800

    Update sabnzbd.sh
    my brother in Christ the /sabnzbd/api url endpoint shouldnt go through basic auth.

--- scripts/nginx/sabnzbd.sh
+++ scripts/nginx/sabnzbd.sh
@@ -22,6 +22,8 @@ location /sabnzbd {
   proxy_pass        http://127.0.0.1:65080/sabnzbd;
   auth_basic "What's the password?";
   auth_basic_user_file /etc/htpasswd.d/htpasswd.${user};
+  allow 127.0.0.0/16;       # localhost (covers 127.0.0.1)
+  deny  all;                # everything else must use basic auth
 }
 SAB
 fi
