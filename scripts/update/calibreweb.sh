ee71262221302e13e8261452d1b545606ab8fb48
flying-sausages <23618693+flying-sausages@users.noreply.github.com>
2026-02-12 22:23:34 +0100

    Check with a pip dry run

--- scripts/update/calibreweb.sh
+++ scripts/update/calibreweb.sh
@@ -6,7 +6,7 @@ if [[ -f /install/.calibreweb.lock ]]; then
         sed -i 's/cps.py -f/cps.py/g' /etc/systemd/system/calibreweb.service
         systemctl daemon-reload
     fi
-    if ! /opt/.venv/calibreweb/bin/python3 -c "import pkg_resources; pkg_resources.require(open('/opt/calibreweb/requirements.txt',mode='r'))" &> /dev/null; then
+    if ! /opt/.venv/calibreweb/bin/pip install -r /opt/calibreweb/requirements.txt --dry-run | grep -q -i would; then
         echo_progress_start "Updating Calibre Web requirements"
         /opt/.venv/calibreweb/bin/pip install -r /opt/calibreweb/requirements.txt >> ${log} 2>&1
         if systemctl is-enabled calibreweb > /dev/null 2>&1 &&
