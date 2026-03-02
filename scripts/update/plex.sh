06bbb539d4dc2568954e087836b2156323942b42
flying-sausages <23618693+flying-sausages@users.noreply.github.com>
2026-03-02 16:55:24 +0100

    it's either one, two, or both

--- scripts/update/plex.sh
+++ scripts/update/plex.sh
@@ -3,7 +3,10 @@
 if [[ -f /etc/apt/sources.list.d/plexmediaserver.list ]]; then
     echo_info "Updating plex apt repo endpoint"
     rm /etc/apt/sources.list.d/plexmediaserver.list
-    rm /usr/share/keyrings/plex-archive-keyring.gpg
+    # shellcheck source=sources/functions/utils
+    . /etc/swizzin/sources/functions/utils
+    rm_if_exists /usr/share/keyrings/plex-archive-keyring.gpg
+    rm_if_exists /usr/share/keyrings/plexmediaserver.gpg
     curl -sL https://downloads.plex.tv/plex-keys/PlexSign.v2.key | gpg --yes --dearmor -o /usr/share/keyrings/plexmediaserver.v2.gpg
     echo "deb [signed-by=/usr/share/keyrings/plexmediaserver.v2.gpg] https://repo.plex.tv/deb/ public main" > /etc/apt/sources.list.d/plex.list >> ${log} 2>&1
     apt_update
